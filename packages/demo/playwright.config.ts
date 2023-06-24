b502f9ddd5ed035b3dde1ed0b85ccab7e35bb8c0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-24 20:42:53 +0900

    test: setup e2e (#9)
    * test: setup e2e
    
    * ci: fix playwright
    
    * ci: tweak playwright install
    
    * ci: more tweak

---
+++ packages/demo/playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "@playwright/test";
+
+const PORT = 4456;
+
+export default defineConfig({
+  testDir: "e2e",
+  use: {
+    baseURL: `http://localhost:${PORT}`,
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        browserName: "chromium",
+        // adapt viewport size to browser window size specified below. otherwise viewport will get clipped.
+        // https://github.com/microsoft/playwright/issues/1086#issuecomment-592227413
+        viewport: null,
+        launchOptions: {
+          args: ["--window-size=800,800"],
+        },
+      },
+    },
+  ],
+  webServer: {
+    command: `pnpm dev:vite --port ${PORT} >> dev-e2e.log 2>&1`,
+    port: PORT,
+    reuseExistingServer: true,
+  },
+  forbidOnly: Boolean(process.env["CI"]),
+});
