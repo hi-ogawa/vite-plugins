094bee8cd9c2439a072bd281aed59456a71153e7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-24 16:39:48 +0900

    chore: add theme select

--- packages/demo/src/server/index.ts
+++ packages/demo/src/server/index.ts
@@ -1,7 +1,18 @@
 import { compose } from "@hattip/compose";
+import THEME_SCRIPT from "@hiogawa/utils-experimental/dist/theme-script.global.js?raw";
 import { globApiRoutes } from "@hiogawa/vite-glob-routes/dist/hattip";
 import { indexHtmlMiddleware } from "@hiogawa/vite-index-html-middleware/dist/hattip";
 
 export function createHattipApp() {
-  return compose(globApiRoutes(), indexHtmlMiddleware());
+  return compose(globApiRoutes(), indexHtmlMiddleware({ injectToHead }));
+}
+
+function injectToHead() {
+  return `
+    <script>
+      globalThis.__themeStorageKey = "vite-plugins:theme";
+      globalThis.__themeDefault = "dark";
+      ${THEME_SCRIPT}
+    </script>
+  `;
 }
