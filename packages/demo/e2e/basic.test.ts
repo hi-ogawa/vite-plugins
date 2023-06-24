57e4c991d0981e8ffbc754ec261223e4128a0af3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-24 20:33:38 +0900

    test: setup e2e

---
+++ packages/demo/e2e/basic.test.ts
@@ -0,0 +1,29 @@
+import { test } from "@playwright/test";
+
+test("basic", async ({ page }) => {
+  await page.goto("/");
+
+  await page.getByRole("button", { name: "Fetch API" }).click();
+  await page.getByText('{ "env": ').click();
+
+  await page.getByRole("link", { name: "/other", exact: true }).click();
+  await page.waitForURL("/other");
+  await page.getByText("Other page").click();
+
+  await page.getByRole("link", { name: "/some-dynamic-id" }).click();
+  await page.waitForURL("/some-dynamic-id");
+  await page.getByText('params = { "dynamic": "some-dynamic-id" }').click();
+
+  await page.getByRole("link", { name: "/subdir", exact: true }).click();
+  await page.waitForURL("/subdir");
+  await page.getByText("Sub directory (index)").click();
+
+  await page.getByRole("link", { name: "/subdir/other" }).click();
+  await page.waitForURL("/subdir/other");
+  await page.getByText("Sub directory (other)").click();
+
+  // SPA can load non root url
+  await page.goto("/some-dynamic-id");
+  await page.waitForURL("/some-dynamic-id");
+  await page.getByText('params = { "dynamic": "some-dynamic-id" }').click();
+});
