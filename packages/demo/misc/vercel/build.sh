e090d5350dee95185f68c1eee021cbd2bae51b81
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-24 10:35:26 +0900

    build: setup deploy script

---
+++ packages/demo/misc/vercel/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eu -o pipefail
+
+# https://vercel.com/docs/build-output-api/v3/primitives#edge-functions
+
+# .vercel/
+#   project.json
+#   output/
+#     config.json
+#     static/              = dist/client
+#     functions/
+#       index.func/
+#         .vc-config.json
+#         index.js         = dist/server/index.js (bundled)
+
+# clean
+rm -rf .vercel/output
+mkdir -p .vercel/output/functions/index.func
+
+# config.json
+cp misc/vercel/config.json .vercel/output/config.json
+
+# static
+cp -r dist/client .vercel/output/static
+
+# serverless
+npx esbuild dist/server/index.mjs --outfile=.vercel/output/functions/index.func/index.js --bundle --platform=neutral
+cp misc/vercel/.vc-config.json .vercel/output/functions/index.func/.vc-config.json
