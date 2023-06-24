5ff70544e4aadeb4d8e45fb4278ff51a73877ec5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-24 10:37:04 +0900

    build: setup deploy script (#1)
    * build: setup deploy script
    
    * chore: tweak title

--- packages/demo/src/routes/dev/debug.api.ts
+++ packages/demo/src/routes/dev/debug.api.ts
@@ -1,8 +1,20 @@
 import { RequestContext } from "@hattip/compose";
 import { tinyassert } from "@hiogawa/utils";
 
+// global "process.env" in vercel edge
+// https://vercel.com/docs/concepts/functions/edge-functions/edge-runtime#environment-variables
+declare let process: {
+  env: unknown;
+};
+
 export function get(ctx: RequestContext) {
-  const data = { ok: true, ctx: { url: { href: ctx.url.href } } };
+  const headers: [string, string][] = [];
+  ctx.request.headers.forEach((v, k) => headers.push([k, v]));
+
+  const data = {
+    env: process.env,
+    headers,
+  };
   return new Response(JSON.stringify(data), {
     headers: [["content-type", "application/json"]],
   });
