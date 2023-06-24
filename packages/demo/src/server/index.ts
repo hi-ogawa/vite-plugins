ba837b641cfc806b361c7ba5c53f85609066dff8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-24 16:55:21 +0900

    feat: setup request logger

--- packages/demo/src/server/index.ts
+++ packages/demo/src/server/index.ts
@@ -1,10 +1,16 @@
-import { compose } from "@hattip/compose";
+import { type RequestHandler, compose } from "@hattip/compose";
 import THEME_SCRIPT from "@hiogawa/utils-experimental/dist/theme-script.global.js?raw";
 import { globApiRoutes } from "@hiogawa/vite-glob-routes/dist/hattip";
 import { indexHtmlMiddleware } from "@hiogawa/vite-index-html-middleware/dist/hattip";
+import type { Context, MiddlewareHandler } from "hono";
+import { logger } from "hono/logger";
 
 export function createHattipApp() {
-  return compose(globApiRoutes(), indexHtmlMiddleware({ injectToHead }));
+  return compose(
+    hattipHonoCompat(logger()),
+    globApiRoutes(),
+    indexHtmlMiddleware({ injectToHead })
+  );
 }
 
 function injectToHead() {
@@ -15,4 +21,24 @@ function injectToHead() {
       ${THEME_SCRIPT}
     </script>
   `;
+}
+
+// minimal compatibility to use hono's logger in hattip
+// https://github.com/honojs/hono/blob/0ffd795ec6cfb67d38ab902197bb5461a4740b8f/src/middleware/logger/index.ts
+function hattipHonoCompat(hono: MiddlewareHandler): RequestHandler {
+  return async (ctx) => {
+    let res!: Response;
+    await hono(
+      {
+        req: { method: ctx.method, raw: ctx.request },
+        get res() {
+          return res;
+        },
+      } as Context,
+      async () => {
+        res = await ctx.next();
+      }
+    );
+    return res;
+  };
 }
