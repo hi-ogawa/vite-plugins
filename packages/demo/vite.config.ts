75280681f1abdbe34165dcbfd2e2292f8bb3d03f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-24 17:10:13 +0900

    feat: previewServerPlugin (wip)

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -1,11 +1,10 @@
+import process from "node:process";
 import globRoutesPlugin from "@hiogawa/vite-glob-routes";
 import indexHtmlMiddlewarePlugin from "@hiogawa/vite-index-html-middleware";
 import vaviteConnect from "@vavite/connect";
 import react from "@vitejs/plugin-react";
 import unocss from "unocss/vite";
-import { defineConfig } from "vite";
-
-// TODO: how to support preview?
+import { Plugin, defineConfig } from "vite";
 
 export default defineConfig((ctx) => ({
   plugins: [
@@ -17,10 +16,9 @@ export default defineConfig((ctx) => ({
       standalone: false,
       serveClientAssetsInDev: true,
       handlerEntry:
-        ctx.command === "build"
-          ? "./src/server/adapter-vercel-edge.ts"
-          : "./src/server/adapter-connect.ts",
+        process.env["SERVER_ENTRY"] ?? "./src/server/adapter-connect.ts",
     }),
+    previewServerPlugin(),
   ],
   build: {
     outDir: ctx.ssrBuild ? "dist/server" : "dist/client",
@@ -28,3 +26,13 @@ export default defineConfig((ctx) => ({
   },
   clearScreen: false,
 }));
+
+function previewServerPlugin(): Plugin {
+  return {
+    name: "local:" + previewServerPlugin.name,
+    async configurePreviewServer(server) {
+      const index = await import("./dist/server/index.js");
+      server.middlewares.use(index.default);
+    },
+  };
+}
