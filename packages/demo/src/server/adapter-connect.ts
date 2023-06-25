61ab65ad1756df54e77b7311edfee322629b30d2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-25 12:45:37 +0900

    fix: fix request path log on dev (#16)

--- packages/demo/src/server/adapter-connect.ts
+++ packages/demo/src/server/adapter-connect.ts
@@ -1,4 +1,6 @@
 import { createMiddleware } from "@hattip/adapter-node";
 import { createHattipApp } from ".";
 
-export default createMiddleware(createHattipApp(), { trustProxy: true });
+export default createMiddleware(createHattipApp(), {
+  trustProxy: !import.meta.env.DEV,
+});
