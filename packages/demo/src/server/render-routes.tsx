8548ee63424c10f0d45ec90b6d7836e4a932ccf3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-25 15:52:55 +0900

    feat: integrate react-router loader on SSR

--- packages/demo/src/server/render-routes.tsx
+++ packages/demo/src/server/render-routes.tsx
@@ -1,3 +1,4 @@
+import type { RequestContext } from "@hattip/compose";
 import type { QueryClient } from "@tanstack/react-query";
 import React from "react";
 import { renderToString } from "react-dom/server";
@@ -12,16 +13,15 @@ import { ReactQueryWrapper } from "../utils/react-query-utils";
 // cf. https://reactrouter.com/en/main/routers/static-router-provider
 
 export async function renderRoutes(
-  request: Request,
+  hattipContext: RequestContext,
   routes: RouteObject[],
   queryClient: QueryClient
 ): Promise<string | Response> {
   const handler = createStaticHandler(routes);
 
-  // since we don't have any loader side-effect, this context construction should be simple.
-  // still this probably handles "not found" error.
-  // https://github.com/remix-run/react-router/blob/bc2552840147206716544e5cdcdb54f649f9193f/packages/router/router.ts#L2608
-  const context = await handler.query(request);
+  const context = await handler.query(hattipContext.request, {
+    requestContext: hattipContext,
+  });
   if (context instanceof Response) {
     return context;
   }
