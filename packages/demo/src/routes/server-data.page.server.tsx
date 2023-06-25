3e1c7f929e658a0657f7f3d7ed4ae95e62b38a7a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-25 18:15:51 +0900

    chore: comment

--- packages/demo/src/routes/server-data.page.server.tsx
+++ packages/demo/src/routes/server-data.page.server.tsx
@@ -1,9 +1,9 @@
 import type { LoaderFunction } from "react-router-dom";
 import { pokomenQueryOptionSSR } from "./server-data-utils";
 
-// for the ease (i.e. no need) of tree-shake, employ explicit "page.server.ts" convention.
-// for the ease of integration, loader is used only during initial SSR and "loader data" is not passed to client.
-// currently it only allowed to mutate `queryClient` to implicitly pass data during SSR and hydration.
+// for the ease of tree-shake, employ explicit "page.server.ts" convention.
+// for the ease of react-router integration, loader is used only during initial SSR and "loader data" is not passed to client.
+// this very simplified architecture, however, allows mutating `queryClient` to implicitly pass data during SSR and hydration.
 export const loader: LoaderFunction = async ({ context }) => {
   const { queryKey, queryFn } = pokomenQueryOptionSSR();
   await context.locals.queryClient.prefetchQuery(queryKey, queryFn);
