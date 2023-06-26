77eed4fdc9b652effa627c04d905f9484ee33a04
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-26 17:57:37 +0900

    chore: ui (wip)

--- packages/demo/src/routes/server-data.page.tsx
+++ packages/demo/src/routes/server-data.page.tsx
@@ -1,15 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { pokomenQueryOption } from "./server-data-utils";
+import { trpcRQ } from "../trpc/react-query";
 
 export function Component() {
   const query = useQuery(pokomenQueryOption());
+
+  const counterQuery = useQuery(trpcRQ.getCounter.queryOptions());
+  const updateMutation = useMutation(trpcRQ.updateCounter.mutationOptions());
 
   return (
     <div className="flex flex-col items-center">
       <div className="w-full p-6">
         <div className="flex flex-col gap-4">
           <h1>Server data</h1>
-          {query.isLoading && (
+          {counterQuery.isLoading && (
             <div className="mt-4 mx-auto antd-spin w-10 h-10"></div>
           )}
           {query.isSuccess && (
