0cdec8e17f12cc9423f231a17a8b652dcf6c5ebc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-27 20:15:30 +0900

    chore: remove unused

--- packages/demo/src/utils/misc.ts
+++ packages/demo/src/utils/misc.ts
@@ -5,16 +5,3 @@ export function cls(...args: unknown[]): string {
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(() => resolve(null), ms));
 }
-
-export function createGetterProxy(
-  propHandler: (prop: string | symbol) => unknown
-): unknown {
-  return new Proxy(
-    {},
-    {
-      get(_target, prop, _receiver) {
-        return propHandler(prop);
-      },
-    }
-  );
-}
