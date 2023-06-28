fba087c297535a60ec8c96579e09ec8263b412a9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-28 19:58:17 +0900

    chore: remove unused

--- packages/demo/src/server/request-context.d.ts
+++ packages/demo/src/server/request-context.d.ts
@@ -6,8 +6,6 @@ import "react-router-dom";
 declare module "@hattip/compose" {
   interface RequestContextExtensions {
     queryClient: import("@tanstack/react-query").QueryClient;
-    trpcCaller: import("../trpc/caller").TrpcCaller;
-    trpcCallerQ: import("../trpc/caller-react-query").TrpcCallerQ;
   }
 }
 
