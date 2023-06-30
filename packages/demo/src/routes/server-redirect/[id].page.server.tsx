0fffa4ffa2db09ed315210fec20a9817ed298e8a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-30 09:17:53 +0900

    chore: revert demo

--- packages/demo/src/routes/server-redirect/[id].page.server.tsx
+++ packages/demo/src/routes/server-redirect/[id].page.server.tsx
@@ -1,9 +1,7 @@
-import crypto from "node:crypto";
 import { type LoaderFunction } from "react-router-dom";
 import { serverRedirectCheckQueryOptions } from "./check.api";
 
 export const loader: LoaderFunction = async ({ params, context }) => {
-  console.log(crypto.randomBytes(8).toString("hex"));
   const id = params["id"] ?? "";
   // note that we don't use `prefetchQuery` since it would swallow "throw redirect inside `queryFn`.
   await context.queryClient.fetchQuery(serverRedirectCheckQueryOptions(id));
