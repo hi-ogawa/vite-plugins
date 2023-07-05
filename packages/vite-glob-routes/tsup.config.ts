89e0db39af01623ddd74f0bd37ff2fb3272c9da3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-05 09:31:04 +0900

    chore: release

--- packages/vite-glob-routes/tsup.config.ts
+++ packages/vite-glob-routes/tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  clean: true,
   entry: ["src/index.ts", "src/react-router.ts", "src/hattip.ts"],
   format: ["esm", "cjs"],
   dts: true,
--- packages/vite-import-index-html/tsup.config.ts
+++ packages/vite-import-index-html/tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  clean: true,
   entry: ["src/index.ts", "src/runtime.ts"],
   format: ["esm", "cjs"],
   dts: true,
