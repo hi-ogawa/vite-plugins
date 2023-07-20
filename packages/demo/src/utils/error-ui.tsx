d1dcf424d9a404efe4f0be46e2f9d8d561168292
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-20 13:01:36 +0900

    comment

--- packages/demo/src/utils/error-ui.tsx
+++ packages/demo/src/utils/error-ui.tsx
@@ -1,1 +1,3 @@
 // https://github.com/vitejs/vite/pull/6274
+// https://github.com/vitejs/vite/blob/5a56941a895fd0ffdbdbf0094336fb7f0f4099c1/packages/vite/src/client/overlay.ts#L141
+// https://github.com/vercel/next.js/blob/4994a428ae5beeeb5e0ece1d791458a5ab1c41b2/packages/react-dev-overlay/src/internal/ReactDevOverlay.tsx#L122-L130
