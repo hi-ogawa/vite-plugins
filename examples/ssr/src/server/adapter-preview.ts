a484103dcfb9122b69b4c94c484d6a47530a425b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-23 16:39:27 +0900

    fix: fix root assets

--- examples/ssr/src/server/adapter-preview.ts
+++ examples/ssr/src/server/adapter-preview.ts
@@ -1,11 +1,11 @@
 import process from "node:process";
 import express from "express";
-import middleware from "./adapter-node";
+import adapterNode from "./adapter-node";
 
 function main() {
   const app = express();
-  app.use("/assets", express.static("./dist/client/assets"));
-  app.use(middleware);
+  app.use("/", express.static("./dist/client", { index: false }));
+  app.use(adapterNode);
 
   const port = process.env["PORT"] ?? 3000;
   app.listen(port, () => {
