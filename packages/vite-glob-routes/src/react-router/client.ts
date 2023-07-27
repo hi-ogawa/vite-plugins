240ac1b5494308190569ad888bf65051398d35d6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-27 10:17:59 +0900

    fix(glob-routes): split server/client entries

---
+++ packages/vite-glob-routes/src/react-router/client.ts
@@ -0,0 +1,18 @@
+import virtualPageRoutesClient from "virtual:@hiogawa/vite-glob-routes/internal/pageRoutesClient";
+import vurtialPageRoutesClientLazy from "virtual:@hiogawa/vite-glob-routes/internal/pageRoutesClientLazy";
+import { createGlobPageRoutes } from "./route-utils";
+
+export function globPageRoutesClient() {
+  return createGlobPageRoutes(virtualPageRoutesClient);
+}
+
+export function globPageRoutesClientLazy() {
+  return createGlobPageRoutes(vurtialPageRoutesClientLazy);
+}
+
+export {
+  initializeClientRoutes,
+  getClientGlobal,
+  getPagePrefetchLinks,
+  type ClientGlobal,
+} from "./cilent-helper";
--- packages/vite-glob-routes/tsup.config.ts
+++ packages/vite-glob-routes/tsup.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   // TODO: clean only on release build
   // clean: true,
-  entry: ["src/index.ts", "src/react-router.ts", "src/hattip.ts"],
+  entry: [
+    "src/index.ts",
+    "src/react-router/client.ts",
+    "src/react-router/server.ts",
+    "src/hattip.ts",
+  ],
+  // TODO: probably no point in providing cjs build
   format: ["esm", "cjs"],
   dts: true,
   external: [
