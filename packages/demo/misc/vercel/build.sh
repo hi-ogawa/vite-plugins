16ca2c6a8e9b4c446267d55e13fe4e9e8a51f0b1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-30 16:24:20 +0900

    chore: cleanup build.sh

--- packages/demo/misc/vercel/build.sh
+++ packages/demo/misc/vercel/build.sh
@@ -27,5 +27,9 @@ cp -r dist/client/assets .vercel/output/static/assets
 
 # serverless
 mkdir -p .vercel/output/functions/index.func
-npx esbuild dist/server/index.js --outfile=.vercel/output/functions/index.func/index.js --bundle --minify --format=esm --platform=browser --metafile=dist/server/esbuild-metafile.json
+npx esbuild dist/server/index.js \
+  --outfile=.vercel/output/functions/index.func/index.js \
+  --bundle --minify --format=esm --platform=browser \
+  --metafile=dist/server/esbuild-metafile.json \
+  --external:node:async_hooks
 cp misc/vercel/.vc-config.json .vercel/output/functions/index.func/.vc-config.json
