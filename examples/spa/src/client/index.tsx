c203189d1fd475639ad183d17e21ee02edd43969
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-12 11:38:03 +0900

    fix: replace hydrateRoot

--- examples/spa/src/client/index.tsx
+++ examples/spa/src/client/index.tsx
@@ -1,24 +1,22 @@
 import { tinyassert } from "@hiogawa/utils";
-import { globPageRoutesClient } from "@hiogawa/vite-glob-routes/dist/react-router/client";
+import { globPageRoutesClientLazy } from "@hiogawa/vite-glob-routes/dist/react-router/client";
 import React from "react";
-import { hydrateRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 async function main() {
   const el = document.getElementById("root");
   tinyassert(el);
 
-  const { routes } = globPageRoutesClient();
+  const { routes } = globPageRoutesClientLazy();
   const router = createBrowserRouter(routes);
   const root = (
     <React.StrictMode>
       <RouterProvider router={router} />
     </React.StrictMode>
   );
-  React.startTransition(() => {
-    hydrateRoot(el, root);
-    el.classList.add("hydrated"); // for spinner and e2e
-  });
+  const reactRoot = createRoot(el);
+  reactRoot.render(root);
 }
 
 main();
