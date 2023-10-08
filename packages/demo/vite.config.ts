f08084f4c6a2609c38957502af2196e92d277730
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-08 17:54:45 +0900

    feat: add `vite-emptify-module`

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -4,7 +4,7 @@ import { importDevServerPlugin } from "@hiogawa/vite-import-dev-server";
 import vaviteConnect from "@vavite/connect";
 import react from "@vitejs/plugin-react";
 import unocss from "unocss/vite";
-import { defineConfig } from "vite";
+import { FilterPattern, type Plugin, createFilter, defineConfig } from "vite";
 
 export default defineConfig((ctx) => ({
   plugins: [
@@ -18,6 +18,7 @@ export default defineConfig((ctx) => ({
       handlerEntry:
         process.env["SERVER_ENTRY"] ?? "./src/server/adapter-connect.ts",
     }),
+    viteEmptifyModulePlugin(),
   ],
   build: {
     outDir: ctx.ssrBuild ? "dist/server" : "dist/client",
@@ -32,3 +33,38 @@ export default defineConfig((ctx) => ({
     : undefined,
   clearScreen: false,
 }));
+
+// similar to https://github.com/remix-run/remix/blob/80c6842f547b7e83b58f1963894b07ad18c2dfe2/packages/remix-dev/compiler/plugins/emptyModules.ts#L10
+// but for vite, we needs to fake esm exports so we use es-module-lexer to extract export names.
+function viteEmptifyModulePlugin(pluginOptions?: {
+  clientOnly?: FilterPattern;
+  serverOnly?: FilterPattern;
+  exclude?: FilterPattern;
+}): Plugin {
+  const exclude = pluginOptions?.exclude ?? ["**/node_modules/**"];
+  const serverOnly = createFilter(
+    pluginOptions?.serverOnly ?? ["**/server/**", "**/*.server.*"],
+    exclude
+  );
+  const clientOnly = createFilter(
+    pluginOptions?.clientOnly ?? ["**/client/**", "**/*.client.*"],
+    exclude
+  );
+  return {
+    name: viteEmptifyModulePlugin.name,
+    enforce: "pre",
+    async transform(code, id, options) {
+      if (options?.ssr ? clientOnly(id) : serverOnly(id)) {
+        const lib = await import("es-module-lexer");
+        await lib.init;
+        const [_import, exports] = lib.parse(code);
+        return exports
+          .map((e) =>
+            e.n === "default" ? `export default {}` : `export var ${e.n} = {}`
+          )
+          .join("\n");
+      }
+      return undefined;
+    },
+  };
+}
