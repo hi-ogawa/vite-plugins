7f0bd41cbc6c70f0898fafe4af2ccbce41c81154
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-03 14:46:53 +0900

    chore: comment

--- examples/spa/vite-plugin-skip-refresh.ts
+++ examples/spa/vite-plugin-skip-refresh.ts
@@ -4,6 +4,13 @@ import { type FilterPattern, type Plugin, createFilter } from "vite";
 // https://github.com/facebook/react/blob/4e3618ae41669c95a3377ae615c727f74f89d141/packages/react-refresh/src/ReactFreshRuntime.js#L713-L715
 // https://github.com/vitejs/vite-plugin-react/blob/4bebe5bd7c0267f6b088005293870cf69953b73a/packages/plugin-react/src/refreshUtils.js#L38
 
+// TODO
+// This only works for function exports. So, this approach won't work when non-function object is used for the "handle" convention.
+// https://reactrouter.com/en/main/route/route#handle
+// https://remix.run/docs/en/main/route/handle
+
+// TODO: HDR?
+
 export function vitePluginSkipRefreshPlugin(pluginOpts: {
   include: FilterPattern;
 }): Plugin {
