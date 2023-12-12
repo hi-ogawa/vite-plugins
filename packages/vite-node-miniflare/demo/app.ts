2f1785c4e0fa4d24b579b775ede6d5c9436d8e8a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-13 00:27:41 +0900

    chore: more demo

--- packages/vite-node-miniflare/demo/app.ts
+++ packages/vite-node-miniflare/demo/app.ts
@@ -1,7 +1,26 @@
-import { h } from "@hiogawa/tiny-react";
+import { h, useState } from "@hiogawa/tiny-react";
 
 console.log("@@ importing app.ts");
 
 export function App(props: { url: string }) {
-  return h.div({}, "hello", h.pre({}, JSON.stringify(props)));
+  const [counter, setCounter] = useState(0);
+
+  return h.div(
+    {
+      style:
+        "display: flex; flex-direction: column; gap: 0.5rem; max-width: 300px",
+    },
+    h.h4({}, "Vite Node Miniflare Demo"),
+    "props",
+    h.pre({}, JSON.stringify(props)),
+    "input",
+    h.input({}),
+    "state",
+    h.pre({}, `counter = ${counter}`),
+    h.div(
+      { style: "display: flex; gap: 0.5rem;" },
+      h.button({ onclick: () => setCounter(counter - 1) }, "-1"),
+      h.button({ onclick: () => setCounter(counter + 1) }, "+1")
+    )
+  );
 }
