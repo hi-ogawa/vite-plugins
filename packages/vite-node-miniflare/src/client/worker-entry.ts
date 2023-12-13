c81b6e0d0cc0ff4f1769b217c0858e0606964b78
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-13 09:36:19 +0900

    chore: typing

--- packages/vite-node-miniflare/src/client/worker-entry.ts
+++ packages/vite-node-miniflare/src/client/worker-entry.ts
@@ -3,8 +3,15 @@ import { createViteNodeRunner } from "./vite-node";
 
 let viteNodeRunner: ViteNodeRunner;
 
+interface Env {
+  __UNSAFE_EVAL: any;
+  __VITE_NODE_SERVER_RPC_URL: string;
+  __VITE_NODE_RUNNER_OPTIONS: any;
+  __WORKER_ENTRY: string;
+}
+
 export default {
-  async fetch(request: Request, env: any) {
+  async fetch(request: Request, env: Env) {
     viteNodeRunner ??= createViteNodeRunner({
       unsafeEval: env.__UNSAFE_EVAL,
       serverRpcUrl: env.__VITE_NODE_SERVER_RPC_URL,
