012352e06f0eeb112d8baee214fc0be3d23ef578
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-13 14:40:40 +0900

    chore: typo

--- packages/vite-node-miniflare/src/client/polyfills/node-fs.ts
+++ packages/vite-node-miniflare/src/client/polyfills/node-fs.ts
@@ -2,7 +2,7 @@ import type fs from "node:fs";
 import { createUsageChecker } from "./usage-checker";
 
 export const existsSync: typeof fs.existsSync = (_path) => {
-  // TODO: how does vite-node/client uses this?
+  // TODO: how does vite-node/client use this?
   // console.log("[existsSync]", _path);
   return false;
 };
