abbd37b2a44a6621af25caa9eb99b026afd8e837
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-14 17:44:02 +0900

    wip: debug 1

--- packages/vite-node-miniflare/demo-remix/app/worker-entry-wrapper.ts
+++ packages/vite-node-miniflare/demo-remix/app/worker-entry-wrapper.ts
@@ -1,5 +1,25 @@
 // delay worker entry import to patch globals
 Object.assign(globalThis, { process: { env: {} } });
 
-const original = await import("./worker-entry");
-export default original.default;
+export default {
+  fetch: async () => {
+    try {
+      // this virtual module cannot even resolve...?
+      // await import("virtual:remix/server-build")
+
+      // @ts-ignore
+      await import("/@id/__x00__virtual:remix/server-build");
+
+      return new Response("ok");
+    } catch (e) {
+      console.error("@@@@");
+      console.error(e);
+      console.error("@@@@");
+      return new Response("error");
+    }
+  },
+};
+
+// TODO
+// const original = await import("./worker-entry");
+// export default original.default;
