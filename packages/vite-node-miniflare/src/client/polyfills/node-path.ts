ac205a7b5ba7d12aaa8cbbe9ffb40ba32a75abc6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-14 19:04:45 +0900

    chore: comment

--- packages/vite-node-miniflare/src/client/polyfills/node-path.ts
+++ packages/vite-node-miniflare/src/client/polyfills/node-path.ts
@@ -1,4 +1,7 @@
 import { createUsageChecker } from "./usage-checker";
 
+// TODO don't have to polyfill?
+// https://developers.cloudflare.com/workers/runtime-apis/nodejs/path/
+
 export { dirname } from "pathe";
 export default createUsageChecker("node:path");
