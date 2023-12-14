dfba853b99a6712962eca8bdc4c400d5398a5c5b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-14 17:02:38 +0900

    chore: copy remix template

---
+++ packages/vite-node-miniflare/demo-remix/app/env.d.ts
@@ -0,0 +1,4 @@
+// copied from https://github.com/rakkasjs/rakkasjs/blob/65481844280a936601c3eb73a00dbb1c8362ea14/packages/rakkasjs/src/types.d.ts#L12-L15
+declare module "react-dom/server.browser" {
+  export * from "react-dom/server";
+}
---
+++ packages/vite-node-miniflare/demo-remix/app/root.tsx
@@ -0,0 +1,28 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {/* @ts-ignore TODO: react-route-dom typing mismatch? */}
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
