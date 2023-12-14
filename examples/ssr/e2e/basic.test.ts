61a8cb2e09a5381630fb94961ce8c139e0fa456b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-14 15:52:24 +0900

    test: e2e

--- examples/ssr/e2e/basic.test.ts
+++ examples/ssr/e2e/basic.test.ts
@@ -5,14 +5,30 @@ test("basic", async ({ page }) => {
   await page.locator("#root.hydrated").waitFor({ state: "attached" });
 
   await page.getByText("Index page").click();
+
+  // loader client navigation
   await page.getByRole("link", { name: "Loader Data" }).click();
   await page.getByText('loaderData = { "message": "hello loader" }').click();
+
+  // GET api
   await page.getByRole("link", { name: "GET API" }).click();
   await page.getByText('{"message":"hello api"}').click();
 
+  // loader SSR
   const res = await page.goto("/loader-data");
   expect(res?.status()).toBe(200);
   expect(await res?.text()).toContain(
     "&quot;message&quot;: &quot;hello loader&quot"
   );
+});
+
+test("POST API", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole("textbox").fill("hello");
+  await page.getByRole("button", { name: "POST API" }).click();
+  await page
+    .getByText(
+      '{"method":"POST","message":"hello formData","data":{"input":"hello"}}'
+    )
+    .click();
 });
