164dc90f52ca92e439b53f37eef4cbb9f9bb1af8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-15 10:18:00 +0900

    refactor(vite-node-miniflare): move examples to separate workspaces

---
+++ packages/vite-node-miniflare/examples/basic/src/app.tsx
@@ -0,0 +1,27 @@
+import { useState } from "@hiogawa/tiny-react";
+import { TestComponent } from "./component";
+
+export function App(props: { url: string }) {
+  const [input, setInput] = useState("");
+  const [counter, setCounter] = useState(0);
+  return (
+    <div style="display: flex; flex-direction: column; gap: 0.5rem; max-width: 300px">
+      <h4>Vite Node Miniflare Demo</h4>
+      <div>Props</div>
+      <pre>{JSON.stringify(props)}</pre>
+      <div>{`Input: ${input}`}</div>
+      <input
+        value={input}
+        oninput={(e) => {
+          setInput(e.currentTarget.value);
+        }}
+      />
+      <div>{`Counter: ${counter}`}</div>
+      <div style="display: flex; gap: 0.5rem;">
+        <button onclick={() => setCounter(counter - 1)}>-1</button>
+        <button onclick={() => setCounter(counter + 1)}>+1</button>
+      </div>
+      <TestComponent />
+    </div>
+  );
+}
---
+++ packages/vite-node-miniflare/examples/basic/vite.config.ts
@@ -0,0 +1,23 @@
+import { tinyReactVitePlugin } from "@hiogawa/tiny-react/dist/plugins/vite";
+import { Log } from "miniflare";
+import { defineConfig } from "vite";
+import { vitePluginViteNodeMiniflare } from "@hiogawa/vite-node-miniflare";
+
+export default defineConfig({
+  clearScreen: false,
+  appType: "custom",
+  ssr: {
+    noExternal: true,
+  },
+  plugins: [
+    vitePluginViteNodeMiniflare({
+      debug: true,
+      // TODO: must be a relative url for invalidation to work?
+      entry: "./src/worker-entry.ts",
+      miniflareOptions(options) {
+        options.log = new Log();
+      },
+    }),
+    tinyReactVitePlugin(),
+  ],
+});
