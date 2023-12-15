f92504249effae799fcc97d1e285065a36796b39
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-15 12:06:34 +0900

    refactor: simplify fetch entry

--- packages/vite-node-miniflare/examples/remix/app/worker-entry.ts
+++ packages/vite-node-miniflare/examples/remix/app/worker-entry.ts
@@ -1,5 +1,4 @@
 import * as build from "virtual:remix/server-build";
-import { compose } from "@hattip/compose";
 import { createRequestHandler } from "@remix-run/server-runtime";
 
 export default {
@@ -9,16 +8,5 @@ export default {
 function createFetchHandler() {
   const mode = import.meta.env.DEV ? "development" : "production";
   const remixHandler = createRequestHandler(build, mode);
-  const hattipHandler = compose((ctx) => remixHandler(ctx.request));
-
-  return (request: Request, env: any, ctx: any) => {
-    // cf. https://github.com/hattipjs/hattip/blob/37f824115c85ef96f6e2b28cfe289f882f225c73/packages/adapter/adapter-cloudflare-workers/src/index.ts
-    return hattipHandler({
-      request,
-      ip: "127.0.0.1",
-      waitUntil: ctx.waitUntil.bind(ctx),
-      passThrough() {},
-      platform: { env },
-    });
-  };
+  return remixHandler;
 }
