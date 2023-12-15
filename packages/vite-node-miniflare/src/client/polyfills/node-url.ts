81d719947e5ce5249e1e2e51e74f3291c98e9a9f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-15 12:40:33 +0900

    fix: workaround remix virtual module

--- packages/vite-node-miniflare/src/client/polyfills/node-url.ts
+++ packages/vite-node-miniflare/src/client/polyfills/node-url.ts
@@ -3,11 +3,18 @@ import { tinyassert } from "@hiogawa/utils";
 
 export const fileURLToPath: typeof url.fileURLToPath = (url) => {
   const s = url.toString();
+  // console.log("@@@ fileURLToPath", s);
   tinyassert(s.startsWith("file://"), s);
   return s.slice("file://".length);
 };
 
 export const pathToFileURL: typeof url.pathToFileURL = (path) => {
-  // console.log("@@@ pathToFileURL", path);
+  // TODO
+  // Somehow Remix's virtual module resolveId ends up here "\0virtual:remix/server-build".
+  // Note that this doesn't happen on my own vite-glob-routes virtual modules,
+  // so it's unlikely that this is a vite-node bug.
+  // For now, stripping off "\0" convention seems to work.
+  // console.log("@@@ pathToFileURL", path.replaceAll("\0", "[#]"));
+  path = path.replaceAll("\0", "");
   return new URL("file://" + path);
 };
