8130a26ccb86fa3a0fc6975810aee843f499ccb2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-21 18:21:35 +0900

    fix: fix preview

--- packages/demo/misc/nitro.sh
+++ packages/demo/misc/nitro.sh
@@ -5,9 +5,10 @@ rm -rf dist/nitro
 mkdir -p dist/nitro/routes
 
 cp -r dist/client dist/nitro/public
+rm dist/nitro/public/{index.html,.vite}
 
 cat > dist/nitro/routes/[...].ts <<EOF
-export * from "../../server/index.js";
+export { default } from "../../server/index.js";
 EOF
 
 # TODO: cache config for immutable 'public/assets'?
