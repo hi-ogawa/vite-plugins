f0ada32f332b6507847c64a098c2acafc3b84bc0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-21 19:01:35 +0900

    chore: comment

--- packages/demo/src/server/adapter-h3.ts
+++ packages/demo/src/server/adapter-h3.ts
@@ -10,15 +10,18 @@ if (typeof globalThis.crypto === "undefined") {
 const hattipHandler = createHattipApp();
 
 const h3Handler = defineEventHandler((event) => {
+  event.context;
   return hattipHandler({
     request: toWebRequest(event),
     ip: getRequestIP(event) ?? "127.0.0.1",
-    // TODO: h3 context for worker env etc...?
-    platform: {},
+    // https://github.com/unjs/nitro/blob/5ead36f78eb54ab94b01c48f6da6e0ecf20a79fa/src/runtime/entries/cloudflare-module.ts#L59-L63
+    platform: event.context,
     passThrough() {},
-    // no API to expose `waitUntil`?
-    // https://github.com/unjs/h3/issues/73#issuecomment-1131429799
-    waitUntil(_promise) {},
+    waitUntil(_promise) {
+      // TODO: preset dependent e.g.
+      event.context["cloudflare"];
+      event.context["vercel"];
+    },
   });
 });
 
