3a126cac834daa6ae357fa49969563a045c53b76
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-21 18:55:47 +0900

    refactor: minor

--- packages/demo/misc/nitro/nitro.config.ts
+++ packages/demo/misc/nitro/nitro.config.ts
@@ -4,6 +4,12 @@ export default defineNitroConfig({
   esbuild: {
     options: {
       target: "esnext",
-    }
-  }
-})
+    },
+  },
+  publicAssets: [
+    {
+      dir: "public/assets",
+      maxAge: 30 * 24 * 60 * 60,
+    },
+  ],
+});
