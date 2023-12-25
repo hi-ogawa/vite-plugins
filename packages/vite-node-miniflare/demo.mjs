009cd6d2bc50312ec541753ac8e65a8b8be24b8f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-25 17:47:03 +0900

    chore: minor

--- packages/vite-node-miniflare/demo.mjs
+++ packages/vite-node-miniflare/demo.mjs
@@ -7,7 +7,10 @@ async function main() {
   // TODO: how to cusomize "unsafeEvalBinding" and "bindings" to miniflare?
   // TODO: console.log proxy?
 
-  const dev = await wrangler.unstable_dev("./demo-entry.mjs", {});
+  const dev = await wrangler.unstable_dev("./demo-entry.mjs", {
+    // TODO: cannot specify port...?
+    // port: 8888
+  });
   console.log(`:: starting at http://localhost:${dev.port}`);
   dev.fetch;
   dev.stop;
