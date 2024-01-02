5a524e2c88c5e5b3bcf1faec29954b570f96b64b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-02 18:48:35 +0900

    test: refactor

--- packages/demo/e2e/preload.test.ts
+++ packages/demo/e2e/preload.test.ts
@@ -1,22 +1,32 @@
 import process from "node:process";
 import { tinyassert } from "@hiogawa/utils";
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { isPageReady } from "./helper";
 
 // TODO: test production build
 const isPreview = Boolean(process.env["E2E_COMMAND"]?.includes("preview"));
 
-test("modulepreload", async ({ page }) => {
+test("ssr", async ({ request }) => {
+  test.skip(isPreview);
+
+  const res = await request.get("/loader-data");
+  tinyassert(res.ok);
+
+  const resText = await res.text();
+  const links = [
+    ...resText.matchAll(/<link rel="modulepreload" href="(.*?)" \/\>/g),
+  ].map((m) => m[0]);
+  expect(links).toEqual([
+    '<link rel="modulepreload" href="/src/routes/layout.tsx" />',
+    '<link rel="modulepreload" href="/src/routes/loader-data.page.tsx" />',
+  ]);
+});
+
+test("client", async ({ page }) => {
   test.skip(isPreview);
 
   await page.goto("/loader-data");
   await isPageReady(page);
-
-  // server rendered preload for matching initial routes
-  tinyassert(await findPreloadLink("/src/routes/layout.tsx"));
-  tinyassert(await findPreloadLink("/src/routes/loader-data.page.tsx"));
-  tinyassert(!(await findPreloadLink("/src/routes/subdir/layout.tsx")));
-  tinyassert(!(await findPreloadLink("/src/routes/subdir/other.page.tsx")));
 
   // preload after mouseover
   const preloadRequests = Promise.all([
@@ -27,11 +37,4 @@ test("modulepreload", async ({ page }) => {
     .getByRole("link", { name: "/subdir/other" })
     .dispatchEvent("mouseover");
   await preloadRequests;
-
-  function findPreloadLink(href: string) {
-    return page.evaluate(
-      (href) => Boolean(document.querySelector(`link[href="${href}"]`)),
-      href
-    );
-  }
 });
