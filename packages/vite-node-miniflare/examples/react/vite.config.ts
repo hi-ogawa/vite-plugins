696621a476e14f988edc55a5c7427169f6abc18c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-09 15:23:27 +0900

    fix(vite-node-miniflare): use `ssr.optimizeDeps` for vite-node

--- packages/vite-node-miniflare/examples/react/vite.config.ts
+++ packages/vite-node-miniflare/examples/react/vite.config.ts
@@ -6,7 +6,16 @@ import { defineConfig } from "vite";
 export default defineConfig({
   clearScreen: false,
   appType: "custom",
+  optimizeDeps: {
+    // for debugging
+    force: true,
+  },
   ssr: {
+    optimizeDeps: {
+      noDiscovery: true,
+      disabled: false,
+      include: ["react", "react/jsx-dev-runtime", "react-dom/server"],
+    },
     noExternal: true,
   },
   plugins: [
@@ -16,11 +25,23 @@ export default defineConfig({
       miniflareOptions(options) {
         options.log = new Log();
       },
-      preBundle: {
-        include: ["react", "react/jsx-dev-runtime", "react-dom/server"],
-        force: true,
-      },
+      // preBundle: {
+      //   include: ["react", "react/jsx-dev-runtime", "react-dom/server"],
+      //   force: true,
+      // },
     }),
     react(),
+    {
+      name: "force-ssr-optimize-deps",
+      configureServer(server) {
+        return async () => {
+          // trigger ssr pre-bundling by dummy ssrLoadModule
+          // https://github.com/vitejs/vite/blob/8ccf7222e9ffaa5e97bd0797de101c8bc6ca8d41/packages/vite/src/node/server/index.ts#L472-L475
+          console.log("[trigger ssrLoadModule]");
+          const mod = await server.ssrLoadModule("/package.json");
+          console.log("[mod]", mod);
+        };
+      },
+    },
   ],
 });
