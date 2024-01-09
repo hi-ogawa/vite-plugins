8b74dd43b33bb8e0d08778b570e113b7ecaf7e51
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-09 18:33:09 +0900

    fix: fix pre-bundle externalization + fix "require" esbuild banner

--- packages/vite-node-miniflare/examples/react/vite.config.ts
+++ packages/vite-node-miniflare/examples/react/vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
     force: true,
   },
   ssr: {
+    // Vite injects "require" banner if `target: "node"`
+    // https://github.com/hi-ogawa/vite/blob/a3008671de5b44ced2952f796219c0c4576125ac/packages/vite/src/node/optimizer/index.ts#L824-L830
+    target: "webworker",
     noExternal: true,
     optimizeDeps: {
       noDiscovery: true,
@@ -24,6 +27,13 @@ export default defineConfig({
       entry: "./src/worker-entry.tsx",
       miniflareOptions(options) {
         options.log = new Log();
+      },
+      viteNodeServerOptions(options) {
+        // vite-node tries to externalize pre-bundled deps by default.
+        // by putting a random cacheDir, it can disable this heuristics.
+        // https://github.com/vitest-dev/vitest/blob/043b78f3257b266302cdd68849a76b8ed343bba1/packages/vite-node/src/externalize.ts#L104-L106
+        options.deps ??= {};
+        options.deps.cacheDir = "__disable_externalizing_vite_deps";
       },
       // preBundle: {
       //   include: ["react", "react/jsx-dev-runtime", "react-dom/server"],
