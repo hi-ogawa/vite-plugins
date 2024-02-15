cedf33656c4d9a7421bb36cf97663e07a8d8e234
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-15 09:30:51 +0900

    chore: comment

--- packages/vite-node-miniflare/src/client/worker-entry.ts
+++ packages/vite-node-miniflare/src/client/worker-entry.ts
@@ -105,8 +105,8 @@ function createViteNodeClient(options: {
       },
 
       runExternalModule(filepath) {
-        console.error("[runExternalModule]", filepath);
-        throw new Error(`[runExternalModule] ${filepath}`);
+        console.error("[vite-node-miniflare] runExternalModule:", filepath);
+        throw new Error(`[vite-node-miniflare] runExternalModule: ${filepath}`);
       },
     }
   );
@@ -134,7 +134,7 @@ class SimpleHMRConnection implements HMRRuntimeConnection {
     const payloads = await this.options.rpc.getHMRPayloads();
     for (const payload of payloads) {
       if (this.options.debug) {
-        console.log("[HMRPayload]", payload);
+        console.log("[vite-node-miniflare] HMRPayload:", payload);
       }
       // use simple module tree invalidation for non-hmr mode
       if (!this.options.hmr && payload.type === "update") {
