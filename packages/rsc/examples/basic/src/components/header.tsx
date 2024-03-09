811385176dc07364e4481dfe3fd73993bc6a006c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-09 18:19:55 +0900

    chore: tweak title

--- packages/rsc/examples/basic/src/components/header.tsx
+++ packages/rsc/examples/basic/src/components/header.tsx
@@ -2,8 +2,8 @@
 export function Header() {
   return (
     <div>
-      <h4 style={{ display: "flex", gap: "1rem" }}>
-        Hello RSC
+      <h4 style={{ display: "flex", gap: "0.5rem" }}>
+        RSC Experiment
         <a
           href="https://github.com/hi-ogawa/vite-plugins/pull/172"
           target="_blank"
