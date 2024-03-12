736cb9b637bd66b937be5a5f5c039eda7fdb0003
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-12 10:25:07 +0900

    test: e2e rsc hmr

--- packages/rsc/examples/basic/e2e/basic.test.ts
+++ packages/rsc/examples/basic/e2e/basic.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/test";
+import { type Page, expect, test } from "@playwright/test";
 import { checkNoError, editFile } from "./helper";
 
 test("basic", async ({ page }) => {
@@ -14,15 +14,13 @@ test("navigation", async ({ page }) => {
   await page.goto("/test");
   await page.getByText("hydrated: true").click();
 
-  // setup client state
-  await page.getByPlaceholder("test-input").fill("hello");
+  const checkClientState = await setupCheckClientState(page);
 
   await page.getByRole("link", { name: "/test/other" }).click();
   await page.getByText("Other Page").click();
   await page.waitForURL("/test/other");
 
-  // verify client state is preserved
-  await expect(page.getByPlaceholder("test-input")).toHaveValue("hello");
+  await checkClientState();
 });
 
 test("404", async ({ page }) => {
@@ -33,18 +31,22 @@ test("404", async ({ page }) => {
   await page.getByText("Not Found: /test/not-found").click();
 });
 
-test("@dev rsc reload", async ({ page }) => {
+test("@dev rsc hmr", async ({ page }) => {
   checkNoError(page);
 
   await page.goto("/test");
   await page.getByRole("heading", { name: "RSC Experiment" }).click();
   await page.getByText("hydrated: true").click();
 
+  const checkClientState = await setupCheckClientState(page);
+
   await editFile("./src/components/header.tsx", (s) =>
     s.replace("RSC Experiment", "RSC (EDIT) Experiment")
   );
   await page.getByRole("heading", { name: "RSC (EDIT) Experiment" }).click();
   await page.getByText("hydrated: true").click();
+
+  await checkClientState();
 });
 
 test("@dev client hmr", async ({ page }) => {
@@ -53,8 +55,7 @@ test("@dev client hmr", async ({ page }) => {
   await page.goto("/test");
   await page.getByText("hydrated: true").click();
 
-  // setup client state
-  await page.getByPlaceholder("test-input").fill("hello");
+  const checkClientState = await setupCheckClientState(page);
 
   // modify client comopnent
   await page.getByText("test-hmr-div").click();
@@ -63,10 +64,27 @@ test("@dev client hmr", async ({ page }) => {
   );
   await page.getByText("test-hmr-edit-div").click();
 
-  // verify client state is preserved
-  await expect(page.getByPlaceholder("test-input")).toHaveValue("hello");
+  await checkClientState();
 
   // SSR should also use a fresh module
   const res = await page.request.get("/test");
   expect(await res.text()).toContain("<div>test-hmr-edit-div</div>");
 });
+
+test.skip("css", async ({ page }) => {
+  page;
+});
+
+test.skip("@dev css hmr", async ({ page }) => {
+  page;
+});
+
+async function setupCheckClientState(page: Page) {
+  // setup client state
+  await page.getByPlaceholder("test-input").fill("hello");
+
+  return async () => {
+    // verify client state is preserved
+    await expect(page.getByPlaceholder("test-input")).toHaveValue("hello");
+  };
+}
