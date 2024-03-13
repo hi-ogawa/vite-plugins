29feda96c5e183cc11fed9410b0f46612895e1fc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-14 08:01:58 +0900

    chore: readme

--- packages/react-server/examples/starter/src/use-client.tsx
+++ packages/react-server/examples/starter/src/use-client.tsx
@@ -1,4 +1,4 @@
 "use client";
 
-// TODO: for now "use client" files in node_modules needs to be manually re-exported
+// TODO: currently "use client" files in node_modules needs to be manually re-exported
 export { Link } from "@hiogawa/react-server/client";
