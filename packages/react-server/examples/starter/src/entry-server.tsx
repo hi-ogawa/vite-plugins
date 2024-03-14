bbfb74a4ec81452febb8ce90653ba3d75790a95e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-14 12:49:18 +0900

    chore: starter

--- packages/react-server/examples/starter/src/entry-server.tsx
+++ packages/react-server/examples/starter/src/entry-server.tsx
@@ -1,1 +1,12 @@
-export { handler } from "@hiogawa/react-server/entry-server";
+import { handler as baseHandler } from "@hiogawa/react-server/entry-server";
+
+// for example, wrap with a simple logger
+export const handler: typeof baseHandler = async (req) => {
+  const url = new URL(req.url);
+  console.log("-->", req.method, url.pathname);
+
+  const res = await baseHandler(req);
+
+  console.log("<--", req.method, url.pathname, res.status);
+  return res;
+};
