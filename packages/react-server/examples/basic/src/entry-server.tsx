b2833be90c46eb2b59b5933e0b214fe9fa272b79
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-14 12:46:58 +0900

    chore: simple http logger

--- packages/react-server/examples/basic/src/entry-server.tsx
+++ packages/react-server/examples/basic/src/entry-server.tsx
@@ -1,1 +1,11 @@
-export { handler } from "@hiogawa/react-server/entry-server";
+import { handler as baseHandler } from "@hiogawa/react-server/entry-server";
+
+export const handler: typeof baseHandler = async (req) => {
+  const url = new URL(req.url);
+  console.log("-->", req.method, url.pathname);
+
+  const res = await baseHandler(req);
+
+  console.log("<--", req.method, url.pathname, res.status);
+  return res;
+};
