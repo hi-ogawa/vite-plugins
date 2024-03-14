ede361453d8818c25ce88697abe547a516c6373f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-14 13:06:28 +0900

    chore(react-server): cleanup debug log (#181)

---
+++ packages/react-server/src/lib/debug.ts
@@ -0,0 +1,19 @@
+// minimal https://github.com/debug-js/debug/
+
+// cli
+//   DEBUG=react-server:* pnpm dev
+// browser devtool
+//   __DEBUG = "react-server:*"
+
+const base = "react-server";
+
+export function createDebug(tag: string) {
+  const name = `${base}:${tag}`;
+  return (...args: unknown[]) => {
+    const flag =
+      globalThis.process?.env?.["DEBUG"] ?? (globalThis as any).__DEBUG ?? "";
+    if ([`${base}:*`, name].some((s) => flag.includes(s))) {
+      console.log(`⊳⊳ ${name}`, ...args);
+    }
+  };
+}
