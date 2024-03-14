91cc5b3a2dc9e04651a10ddf732ff17ce0fd4290
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-14 13:04:42 +0900

    chore: add httpLoggerPlugin

--- packages/react-server/examples/basic/vite.config.ts
+++ packages/react-server/examples/basic/vite.config.ts
@@ -2,7 +2,7 @@ import path from "node:path";
 import { vitePluginReactServer } from "@hiogawa/react-server/plugin";
 import { vitePluginSsrMiddleware } from "@hiogawa/vite-plugin-ssr-middleware";
 import react from "@vitejs/plugin-react";
-import { type Plugin, defineConfig } from "vite";
+import { Connect, type Plugin, defineConfig } from "vite";
 
 export default defineConfig({
   clearScreen: false,
@@ -11,6 +11,7 @@ export default defineConfig({
     vitePluginReactServer({
       plugins: [testVitePluginVirtual()],
     }),
+    httpLoggerPlugin(),
     vitePluginSsrMiddleware({
       entry: process.env["SSR_ENTRY"] || "/src/adapters/node.ts",
       preview: path.resolve("./dist/server/index.js"),
@@ -40,4 +41,39 @@ function testVitePluginVirtual(): Plugin {
       return;
     },
   };
+}
+
+// TODO: move to vitePluginSsrMiddleware?
+function httpLoggerPlugin(): Plugin {
+  return {
+    name: "http-logger",
+    configureServer(server) {
+      return () => server.middlewares.use(httpLogger());
+    },
+    configurePreviewServer(server) {
+      return () => server.middlewares.use(httpLogger());
+    },
+  };
+}
+
+function httpLogger(): Connect.NextHandleFunction {
+  return (req, res, next) => {
+    const url = new URL(req.originalUrl!, "https://test.local");
+    console.log("-->", req.method, url.pathname);
+    const startTime = Date.now();
+    res.once("close", () => {
+      console.log(
+        "<--",
+        req.method,
+        url.pathname,
+        res.statusCode,
+        formatDuration(Date.now() - startTime)
+      );
+    });
+    next();
+  };
+}
+
+function formatDuration(ms: number) {
+  return ms < 1000 ? `${Math.floor(ms)}ms` : `${(ms / 1000).toFixed(1)}s`;
 }
