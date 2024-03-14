7c1231c47e84eb04ce5d724d91ccd115aea013e7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-14 13:20:18 +0900

    chore: bump latest

--- packages/react-server/examples/starter/src/entry-server.tsx
+++ packages/react-server/examples/starter/src/entry-server.tsx
@@ -1,12 +1,1 @@
-import { handler as baseHandler } from "@hiogawa/react-server/entry-server";
-
-// for example, wrap with a simple logger
-export const handler: typeof baseHandler = async (req) => {
-  const url = new URL(req.url);
-  console.log("-->", req.method, url.pathname);
-
-  const res = await baseHandler(req);
-
-  console.log("<--", req.method, url.pathname, res.status);
-  return res;
-};
+export { handler } from "@hiogawa/react-server/entry-server";
--- packages/react-server/examples/starter/vite.config.ts
+++ packages/react-server/examples/starter/vite.config.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import { vitePluginReactServer } from "@hiogawa/react-server/plugin";
-import { vitePluginSsrMiddleware } from "@hiogawa/vite-plugin-ssr-middleware";
+import { vitePluginSsrMiddleware, vitePluginLogger } from "@hiogawa/vite-plugin-ssr-middleware";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -9,6 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     vitePluginReactServer(),
+    vitePluginLogger(),
     vitePluginSsrMiddleware({
       entry: process.env["SSR_ENTRY"] || "/src/adapters/node.ts",
       preview: path.resolve("./dist/server/index.js"),
