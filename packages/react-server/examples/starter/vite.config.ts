1cfe121deb468732120a6f3ca10903614b8f5710
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-14 13:23:06 +0900

    feat(ssr-middleware): add `vitePluginLogger` (#182)

--- packages/react-server/examples/starter/vite.config.ts
+++ packages/react-server/examples/starter/vite.config.ts
@@ -1,6 +1,9 @@
 import path from "node:path";
 import { vitePluginReactServer } from "@hiogawa/react-server/plugin";
-import { vitePluginSsrMiddleware } from "@hiogawa/vite-plugin-ssr-middleware";
+import {
+  vitePluginLogger,
+  vitePluginSsrMiddleware,
+} from "@hiogawa/vite-plugin-ssr-middleware";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -9,6 +12,7 @@ export default defineConfig({
   plugins: [
     react(),
     vitePluginReactServer(),
+    vitePluginLogger(),
     vitePluginSsrMiddleware({
       entry: process.env["SSR_ENTRY"] || "/src/adapters/node.ts",
       preview: path.resolve("./dist/server/index.js"),
