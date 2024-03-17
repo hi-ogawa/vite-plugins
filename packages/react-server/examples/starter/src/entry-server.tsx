677f347a343a119f4ff53f5b3a9b3bb5a345bde5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-17 11:15:30 +0900

    chore: revert

--- packages/react-server/examples/starter/src/entry-server.tsx
+++ packages/react-server/examples/starter/src/entry-server.tsx
@@ -1,9 +1,1 @@
 export { handler } from "@hiogawa/react-server/entry-server";
-
-// import { importReactServer, renderHtml } from "@hiogawa/react-server/entry-server";
-// const reactServer = await importReactServer()
-// reactServer.handler(request)
-
-// export function handler() {
-//   //
-// }
