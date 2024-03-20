9641b44ad8c7cb18b80e6b15d1c3e83808fc1b60
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-20 14:33:55 +0900

    chore: unused

--- packages/react-server/src/lib/global.ts
+++ packages/react-server/src/lib/global.ts
@@ -1,11 +1,9 @@
-import type { RouterHistory } from "@tanstack/history";
 import type { ViteDevServer } from "vite";
 import type { CallServerCallback } from "./types";
 
 // centeralize quick global escape hatch...
 
 export const __global: {
-  history: RouterHistory;
   callServer: CallServerCallback;
   dev: {
     server: ViteDevServer;
