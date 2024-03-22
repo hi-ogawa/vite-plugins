b0bdd699ab0c498f747e993d65d831df3c1b937c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-22 12:09:33 +0900

    chore: minor

--- packages/react-server/examples/basic/src/routes/demo/waku_02/_utils.tsx
+++ packages/react-server/examples/basic/src/routes/demo/waku_02/_utils.tsx
@@ -3,7 +3,7 @@ import { sortBy } from "@hiogawa/utils";
 const POKEMON_URL =
   "https://raw.githubusercontent.com/dai-shi/waku/bdcb7b26ef8c69d95eae44cd9f46841fd4a82631/examples/02_demo/private/pokemon.json";
 
-export async function fetchPokemons(): Promise<PokemonEntry[]> {
+async function fetchPokemons(): Promise<PokemonEntry[]> {
   const res = await fetch(POKEMON_URL);
   return res.json();
 }
