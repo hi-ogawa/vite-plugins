e3f7d77c65a1d56869076876a7fc4f8fe3b51de0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-22 10:38:41 +0900

    chore: revert

--- packages/react-server/examples/basic/e2e/basic.test.ts
+++ packages/react-server/examples/basic/e2e/basic.test.ts
@@ -25,12 +25,32 @@ test("navigation", async ({ page }) => {
   await checkClientState();
 });
 
-test("404", async ({ page }) => {
-  checkNoError(page);
-
+test("error", async ({ page }) => {
   const res = await page.goto("/test/not-found");
   expect(res?.status()).toBe(404);
-  await page.getByText("Not Found: /test/not-found").click();
+
+  await page.getByText("hydrated: true").click();
+  await page.getByText(`server error: {"status":404}`).click();
+
+  const checkClientState = await setupCheckClientState(page);
+
+  await page.getByRole("link", { name: "/test/error" }).click();
+  await page.getByRole("link", { name: "Server" }).click();
+  await page.getByText('server error: {"status":500}').click();
+  await page.getByRole("link", { name: "/test/error" }).click();
+  await page.getByRole("link", { name: "Browser" }).click();
+  await page.getByText("server error: (N/A)").click();
+
+  await page.getByRole("link", { name: "/test/other" }).click();
+  await page.getByRole("heading", { name: "Other Page" }).click();
+
+  await checkClientState();
+});
+
+test("DefaultRootErrorPage", async ({ page }) => {
+  const res = await page.goto("/not-found");
+  expect(res?.status()).toBe(404);
+  await page.getByText("404 Not Found").click();
 });
 
 test("rsc hmr @dev", async ({ page }) => {
@@ -94,6 +114,8 @@ test("client hmr @dev", async ({ page }) => {
 });
 
 test("unocss", async ({ page, browser }) => {
+  checkNoError(page);
+
   await page.goto("/test");
   await expect(page.getByRole("heading", { name: "RSC Experiment" })).toHaveCSS(
     "font-weight",
@@ -108,6 +130,8 @@ test("unocss", async ({ page, browser }) => {
 });
 
 test("unocss hmr @dev", async ({ page, browser }) => {
+  checkNoError(page);
+
   await page.goto("/test");
   await page.getByText("hydrated: true").click();
 
@@ -136,6 +160,8 @@ test("unocss hmr @dev", async ({ page, browser }) => {
 });
 
 test("react-server css", async ({ page }) => {
+  checkNoError(page);
+
   await page.goto("/test/css");
   await expect(page.getByText("css normal")).toHaveCSS(
     "background-color",
@@ -149,6 +175,7 @@ test("react-server css", async ({ page }) => {
 
 test("react-server css @nojs", async ({ browser }) => {
   const page = await browser.newPage({ javaScriptEnabled: false });
+
   await page.goto("/test/css");
   await expect(page.getByText("css normal")).toHaveCSS(
     "background-color",
@@ -161,6 +188,8 @@ test("react-server css @nojs", async ({ browser }) => {
 });
 
 test("react-server css hmr @dev", async ({ page, browser }) => {
+  checkNoError(page);
+
   await page.goto("/test/css");
   await page.getByText("hydrated: true").click();
 
@@ -208,6 +237,8 @@ test("react-server css hmr @dev", async ({ page, browser }) => {
 });
 
 test("server action with js", async ({ page }) => {
+  checkNoError(page);
+
   await page.goto("/test/action");
   await page.getByText("hydrated: true").click();
 
--- packages/react-server/examples/basic/e2e/helper.ts
+++ packages/react-server/examples/basic/e2e/helper.ts
@@ -1,11 +1,28 @@
 import fs from "node:fs";
 import test, { type Page, expect } from "@playwright/test";
 
+//
+// page error check
+//
+
+const pageErrorsMap = new WeakMap<Page, Error[]>();
+
+test.afterEach(({ page }) => {
+  const errors = pageErrorsMap.get(page);
+  if (errors) {
+    expect(errors).toEqual([]);
+  }
+});
+
 export function checkNoError(page: Page) {
   const pageErrors: Error[] = [];
+  pageErrorsMap.set(page, pageErrors);
   page.on("pageerror", (e) => pageErrors.push(e));
-  page.on("close", () => expect(pageErrors).toEqual([]));
 }
+
+//
+// file edit utils
+//
 
 const originalFileMap = new Map<string, string>();
 
