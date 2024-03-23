be9d5c6cd055495f65fbf73978e79781891bcf04
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-23 17:49:49 +0900

    fix(react-server): intercept only simple click on Link

--- packages/react-server/src/lib/components/link.tsx
+++ packages/react-server/src/lib/components/link.tsx
@@ -2,13 +2,24 @@
 
 import { __history } from "../csr";
 
+// TODO: study prior art
+// https://github.com/TanStack/router/blame/a1030ef24de104eb32f7a781cda247458e0ec90a/packages/react-router/src/link.tsx
+// https://github.com/remix-run/react-router/blob/9e7486b89e712b765d947297f228650cdc0c488e/packages/react-router-dom/index.tsx#L1394
+
 export function Link(props: JSX.IntrinsicElements["a"] & { href: string }) {
   return (
     <a
       {...props}
       onClick={(e) => {
-        e.preventDefault();
-        __history.push(props.href!);
+        const target = e.currentTarget.target;
+        if (
+          e.button === 0 &&
+          !(e.metaKey || e.altKey || e.ctrlKey || e.shiftKey) &&
+          (!target || target === "_self")
+        ) {
+          e.preventDefault();
+          __history.push(props.href!);
+        }
       }}
     />
   );
