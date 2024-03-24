7aa33268e5799b8683e6e7b6d1cf0c4b673b2872
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-24 13:43:08 +0900

    test(react-server): test `ReactDom.useFormStatus`

--- packages/react-server/examples/basic/src/routes/test/action/_client.tsx
+++ packages/react-server/examples/basic/src/routes/test/action/_client.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import React from "react";
-import { addMessage, changeCounter, type getMessages } from "./_action";
+import ReactDom from "react-dom";
+import {
+  addMessage,
+  changeCounter,
+  type getMessages,
+  slowAction,
+} from "./_action";
 
 export function Counter(props: { value: number }) {
   return (
@@ -89,4 +95,73 @@ export function Chat(props: { messages: ReturnType<typeof getMessages> }) {
       </form>
     </div>
   );
+}
+
+// https://react.dev/reference/react-dom/hooks/useFormStatus
+export function FormStateTest() {
+  return (
+    <form
+      method="POST"
+      action={slowAction}
+      className="flex flex-col items-start gap-2"
+    >
+      <FormStateTestInner />
+    </form>
+  );
+}
+
+function FormStateTestInner() {
+  // useFormStatus works only inside <form />
+  const formStatus = ReactDom.useFormStatus();
+
+  return (
+    <>
+      <h4 className="font-bold">Form Status</h4>
+      <div className="flex gap-2">
+        <button
+          className="antd-btn antd-btn-default px-2"
+          name="sleep"
+          value={"200"}
+          disabled={formStatus.pending}
+        >
+          0.2 sec
+        </button>
+        <button
+          className="antd-btn antd-btn-default px-2"
+          name="sleep"
+          value={"1000"}
+          disabled={formStatus.pending}
+        >
+          1.0 sec
+        </button>
+        <button
+          className="antd-btn antd-btn-default px-2"
+          name="sleep"
+          value={"2000"}
+          disabled={formStatus.pending}
+        >
+          2.0 sec
+        </button>
+      </div>
+      <pre className="text-sm">
+        <div>pending: {String(formStatus.pending)}</div>
+        <div>method: {String(formStatus.method)}</div>
+        <div>
+          data:{" "}
+          {formStatus.data
+            ? JSON.stringify(formDataToJson(formStatus.data))
+            : String(formStatus.data)}
+        </div>
+        <div>action: {String(formStatus.action)}</div>
+      </pre>
+    </>
+  );
+}
+
+function formDataToJson(data: FormData) {
+  const result: any = {};
+  data.forEach((v, k) => {
+    result[k] = v;
+  });
+  return result;
 }
