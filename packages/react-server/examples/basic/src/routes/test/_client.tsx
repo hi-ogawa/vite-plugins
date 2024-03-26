b9d7b1059e0d01a5993f7b5b214ce699d444bd09
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-26 12:18:23 +0900

    test(react-server): test client render count

--- packages/react-server/examples/basic/src/routes/test/_client.tsx
+++ packages/react-server/examples/basic/src/routes/test/_client.tsx
@@ -8,5 +8,15 @@ export function Hydrated() {
     setHydrated(true);
   }, []);
 
-  return <div>hydrated: {String(hydrated)}</div>;
+  return <div>[hydrated: {String(hydrated)}]</div>;
+}
+
+// test client re-rendering
+// https://github.com/pmndrs/jotai/blob/419ab5cda3503e70463dc340076e21e438db89b6/tests/react/basic.test.tsx#L27-L33
+export function EffectCount() {
+  const commitCountRef = React.useRef(1);
+  React.useEffect(() => {
+    commitCountRef.current += 1;
+  });
+  return <div>[EffectCount: {commitCountRef.current}]</div>;
 }
--- packages/react-server/examples/basic/src/routes/test/layout.tsx
+++ packages/react-server/examples/basic/src/routes/test/layout.tsx
@@ -1,13 +1,13 @@
 import type { LayoutProps } from "@hiogawa/react-server/server";
 import { NavMenu } from "../../components/nav-menu";
-import { Hydrated } from "./_client";
+import { EffectCount, Hydrated } from "./_client";
 
 export default async function Layout(props: LayoutProps) {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 max-w-lg">
       <h2 className="text-lg">Test</h2>
       <NavMenu
-        className="grid grid-cols-3 w-lg gap-1"
+        className="grid grid-cols-3 gap-1"
         links={[
           "/test",
           "/test/other",
@@ -20,9 +20,10 @@ export default async function Layout(props: LayoutProps) {
           "/test/transition",
         ]}
       />
-      <div className="flex items-center gap-2 w-sm text-sm">
+      <div className="flex items-center gap-2 text-sm">
         <input className="antd-input px-2" placeholder="test-input" />
         <Hydrated />
+        <EffectCount />
       </div>
       {props.children}
     </div>
