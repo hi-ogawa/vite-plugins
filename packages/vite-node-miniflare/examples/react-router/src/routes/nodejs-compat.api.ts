009118f4892abb8efe794486e158f5afafcc3517
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-30 14:36:16 +0900

    chore: lint

--- packages/vite-node-miniflare/examples/react-router/src/routes/nodejs-compat.api.ts
+++ packages/vite-node-miniflare/examples/react-router/src/routes/nodejs-compat.api.ts
@@ -1,6 +1,6 @@
+import util from "node:util";
 import type { RequestHandler } from "@hattip/compose";
 import { json } from "react-router-dom";
-import util from "node:util";
 
 export const get: RequestHandler = () => {
   return json({ format: util.format("hello:%s", "world") });
