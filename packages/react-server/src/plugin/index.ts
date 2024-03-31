614a9995ef36484dbb1f4de1ab0c0d760e6ccbd7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-31 09:32:13 +0900

    chore: revert some

--- packages/react-server/src/plugin/index.ts
+++ packages/react-server/src/plugin/index.ts
@@ -18,8 +18,6 @@ import {
   createServer,
   parseAstAsync,
 } from "vite";
-import { createVirtualPlugin } from "../features/core/plugin";
-import { vitePluginSilenceUseClientBuildWarning } from "../features/use-client/plugin";
 import { __global } from "../lib/global";
 import { USE_CLIENT_RE, USE_SERVER_RE, getExportNames } from "./ast-utils";
 import { collectStyle, collectStyleUrls } from "./css";
@@ -697,4 +695,57 @@ function hashString(v: string) {
     .update(v)
     .digest()
     .toString("base64url");
+}
+
+function createVirtualPlugin(name: string, load: Plugin["load"]) {
+  name = "virtual:" + name;
+  return {
+    name: `virtual-${name}`,
+    resolveId(source, _importer, _options) {
+      return source === name ? "\0" + name : undefined;
+    },
+    load(id, options) {
+      if (id === "\0" + name) {
+        return (load as any)(id, options);
+      }
+    },
+  } satisfies Plugin;
+}
+
+// silence warning due to "use client"
+// https://github.com/vitejs/vite-plugin-react/blob/814ed8043d321f4b4679a9f4a781d1ed14f185e4/packages/plugin-react/src/index.ts#L303
+function vitePluginSilenceUseClientBuildWarning(): Plugin {
+  return {
+    name: vitePluginSilenceUseClientBuildWarning.name,
+    enforce: "post",
+    config(config, _env) {
+      return {
+        build: {
+          rollupOptions: {
+            onwarn(warning, defaultHandler) {
+              // https://github.com/vitejs/vite/issues/15012#issuecomment-1948550039
+              if (
+                warning.code === "SOURCEMAP_ERROR" &&
+                warning.message.includes("(1:0)")
+              ) {
+                return;
+              }
+              // https://github.com/TanStack/query/pull/5161#issuecomment-1506683450
+              if (
+                warning.code === "MODULE_LEVEL_DIRECTIVE" &&
+                warning.message.includes(`"use client"`)
+              ) {
+                return;
+              }
+              if (config.build?.rollupOptions?.onwarn) {
+                config.build.rollupOptions.onwarn(warning, defaultHandler);
+              } else {
+                defaultHandler(warning);
+              }
+            },
+          },
+        },
+      };
+    },
+  };
 }
