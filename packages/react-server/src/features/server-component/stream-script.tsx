7017555ef59f696a5f5510341d6068c21c6794d6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-31 13:43:04 +0900

    refactor: simplify stream script

--- packages/react-server/src/features/server-component/stream-script.tsx
+++ packages/react-server/src/features/server-component/stream-script.tsx
@@ -3,9 +3,6 @@
 // https://github.com/vercel/next.js/blob/1c5aa7fa09cc5503c621c534fc40065cbd2aefcb/packages/next/src/client/app-index.tsx#L110-L113
 // https://github.com/devongovett/rsc-html-stream/
 
-// TODO: handle cancel?
-// TODO: handle binary?
-
 export function injectStreamScript(stream: ReadableStream<string>) {
   const search = "</body>";
   return new TransformStream<string, string>({
@@ -18,21 +15,17 @@ export function injectStreamScript(stream: ReadableStream<string>) {
       const [pre, post] = chunk.split(search);
       controller.enqueue(pre);
 
+      // TODO: handle cancel?
       await stream.pipeTo(
         new WritableStream({
           start() {
             controller.enqueue(`<script>self.__stream_chunks||=[]</script>`);
           },
           write(chunk) {
-            // assume chunk is already encoded as raw js string e.g. by
+            // assume chunk is already encoded as javascript code e.g. by
             //   stream.pipeThrough(jsonStringifyTransform())
             controller.enqueue(
               `<script>__stream_chunks.push(${chunk})</script>`,
-            );
-          },
-          close() {
-            controller.enqueue(
-              `<script>__stream_chunks.push("__stream_close")</script>`,
             );
           },
         }),
@@ -44,26 +37,26 @@ export function injectStreamScript(stream: ReadableStream<string>) {
 }
 
 export function readStreamScript() {
-  const stream = new ReadableStream<unknown>({
+  return new ReadableStream<unknown>({
     start(controller) {
-      function handleChunk(chunk: string) {
-        if (chunk === "__stream_close") {
-          controller.close();
-          return;
-        }
+      const chunks: unknown[] = ((globalThis as any).__stream_chunks ||= []);
+
+      for (const chunk of chunks) {
         controller.enqueue(chunk);
       }
 
-      const chunks: string[] = ((globalThis as any).__stream_chunks ||= []);
-      for (const chunk of chunks) {
-        handleChunk(chunk);
-      }
-
       chunks.push = function (chunk) {
-        handleChunk(chunk);
+        controller.enqueue(chunk);
         return 0;
       };
+
+      if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", () => {
+          controller.close();
+        });
+      } else {
+        controller.close();
+      }
     },
   });
-  return stream;
 }
