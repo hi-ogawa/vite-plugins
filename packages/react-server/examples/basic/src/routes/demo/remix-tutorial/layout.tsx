0a7f7c4fdf8766f12c7ea295939edf3091d6d293
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-05 18:54:21 +0900

    wip

--- packages/react-server/examples/basic/src/routes/demo/remix-tutorial/layout.tsx
+++ packages/react-server/examples/basic/src/routes/demo/remix-tutorial/layout.tsx
@@ -1,7 +1,8 @@
-export default async function Layout(props: React.PropsWithChildren) {
+import type { LayoutProps } from "@hiogawa/react-server/server";
+
+export default async function Layout(props: LayoutProps) {
   return (
-    <div className="flex flex-col items-center gap-2 w-full">
-      {/* <link rel="styleshee" href={cssHref} /> */}
+    <div className="flex flex-col items-center gap-4">
       <h2 className="text-lg">
         Demo ported from{" "}
         <a
@@ -12,7 +13,48 @@ export default async function Layout(props: React.PropsWithChildren) {
           Remix
         </a>
       </h2>
-      <div className="border p-4">{props.children}</div>
+      <div className="border p-4 w-full">
+        <div className="flex gap-2">
+          <SideBar />
+          <div className="flex-1 p-2">{props.children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function SideBar() {
+  return (
+    <div className="flex flex-col p-2 gap-4">
+      <h1 className="font-bold">Remix Contacts</h1>
+      <div className="flex gap-2">
+        <form role="search">
+          <input
+            className="antd-input p-1"
+            aria-label="Search contacts"
+            name="q"
+            placeholder="Search..."
+            type="search"
+          />
+          <div aria-hidden hidden={true} id="search-spinner" />
+        </form>
+        <form method="post">
+          <button className="antd-btn antd-btn-default p-1 px-2" type="submit">
+            New
+          </button>
+        </form>
+      </div>
+      <div className="border-t"></div>
+      <nav>
+        <ul className="flex flex-col gap-2">
+          <li className="antd-btn antd-btn-default p-1">
+            <a href={`/demo/remix-tutorial/contacts/1`}>Your Name</a>
+          </li>
+          <li className="antd-btn antd-btn-default p-1">
+            <a href={`/demo/remix-tutorial/contacts/2`}>Your Friend</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
--- packages/react-server/examples/basic/src/routes/demo/remix-tutorial/page.tsx
+++ packages/react-server/examples/basic/src/routes/demo/remix-tutorial/page.tsx
@@ -1,36 +1,3 @@
 export default async function Page() {
-  return (
-    <>
-      <div id="sidebar" className="flex flex-col gap-4">
-        <h1 className="font-bold">Remix Contacts</h1>
-        <div className="flex gap-2">
-          <form role="search">
-            <input
-              className="antd-input px-2"
-              aria-label="Search contacts"
-              name="q"
-              placeholder="Search..."
-              type="search"
-            />
-            <div aria-hidden hidden={true} id="search-spinner" />
-          </form>
-          <form method="post">
-            <button className="antd-btn antd-btn-default px-2" type="submit">
-              New
-            </button>
-          </form>
-        </div>
-        <nav>
-          <ul className="flex flex-col gap-2">
-            <li className="antd-btn antd-btn-default p-1">
-              <a href={`/demo/remix-tutorial/contacts/1`}>Your Name</a>
-            </li>
-            <li className="antd-btn antd-btn-default p-1">
-              <a href={`/demo/remix-tutorial/contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
-  );
+  return <div>TODO</div>;
 }
