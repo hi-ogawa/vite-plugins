cb63a29b0050da6a5362c5bd4146df46218a9c91
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-06 18:55:50 +0900

    chore: comment

--- packages/react-server/examples/basic/src/entry-react-server.tsx
+++ packages/react-server/examples/basic/src/entry-react-server.tsx
@@ -3,12 +3,9 @@ import {
   handler as baseHandler,
 } from "@hiogawa/react-server/entry-react-server";
 
-console.log("[import]", import.meta.url);
-
 export const handler: ReactServerHandler = async (ctx) => {
-  const url = new URL(ctx.request.url);
-
   // custom handler example
+  const url = new URL(ctx.request.url);
   if (url.pathname === "/test/__rpc") {
     return new Response(JSON.stringify({ hello: "world" }), {
       headers: {
