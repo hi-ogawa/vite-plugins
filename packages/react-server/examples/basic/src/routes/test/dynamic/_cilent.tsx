03d2aeca46eb3f00e4dc05659424b74090d51548
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-08 14:45:55 +0900

    chore: cleanup

--- packages/react-server/examples/basic/src/routes/test/dynamic/_cilent.tsx
+++ packages/react-server/examples/basic/src/routes/test/dynamic/_cilent.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import { useRouter } from "@hiogawa/react-server/client";
-import React from "react";
 
 export function ClientLocation() {
   const location = useRouter((s) => s.location);
-
-  React.useEffect(() => {
-    0 && console.log("[ClientLocation]", location, window.location);
-  }, [location]);
-
   return <>{location.pathname}</>;
 }
