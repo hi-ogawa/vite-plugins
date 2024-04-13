c617aad49148ccb4b4b24a09952e60939364d0b1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-13 14:01:33 +0900

    wip

--- packages/error-overlay/src/index.ts
+++ packages/error-overlay/src/index.ts
@@ -1,10 +1,11 @@
-import { type Plugin } from "vite";
+import { type Plugin, type WebSocketClient } from "vite";
 import { name as packageName } from "../package.json";
+
+const virtualName = "virtual:runtime-error-overlay";
 
 export function vitePluginErrorOverlay(options?: {
   filter?: (error: Error) => boolean;
 }): Plugin {
-  options?.filter;
   return {
     name: packageName,
     apply: "serve",
@@ -12,31 +13,56 @@ export function vitePluginErrorOverlay(options?: {
       return [
         {
           tag: "script",
-          attrs: { type: "module" },
-          children: CLIENT_SCRIPT,
+          attrs: { type: "module", src: "/@id/__x00__" + virtualName },
         },
       ];
+    },
+    resolveId(source, _importer, _options) {
+      return source === virtualName ? "\0" + virtualName : undefined;
+    },
+    load(id, _options) {
+      if (id === "\0" + virtualName) {
+        return `(${clientScriptFn.toString()})()`;
+      }
+      return;
+    },
+    configureServer(server) {
+      server.hot.on("custom:runtime-error", (...args: any[]) => {
+        const [data, client] = args as [unknown, WebSocketClient];
+        const error = Object.assign(new Error(), data);
+        if (options?.filter?.(error) ?? true) {
+          client.send({
+            type: "error",
+            err: {
+              message: error.message,
+              stack: error.stack ?? "", // TODO: solve sourcemap
+            },
+          });
+        }
+      });
     },
   };
 }
 
-// cf. https://github.com/vitejs/vite/blob/f8e0791e3f7c7c39c041a563e77396eca706d05e/packages/vite/src/client/client.ts#L313
-const CLIENT_SCRIPT = /* js */ `
+function clientScriptFn() {
+  if (import.meta.hot) {
+    window.addEventListener("error", (evt) => {
+      sendError(evt.error);
+    });
 
-// TODO: base?
-import { ErrorOverlay } from "/@vite/client";
+    window.addEventListener("unhandledrejection", (evt) => {
+      sendError(evt.reason);
+    });
 
-function createErrorOverlay(err) {
-  document.querySelectorAll("vite-error-overlay").forEach((n) => n.close());
-  document.body.appendChild(new ErrorOverlay(err));
-};
-
-window.addEventListener("error", (evt) => {
-  createErrorOverlay(evt.error);
-});
-
-window.addEventListener("unhandledrejection", (evt) => {
-  createErrorOverlay(evt.reason);
-});
-
-`;
+    function sendError(e: unknown) {
+      const error =
+        e instanceof Error ? e : new Error("(unknown error)", { cause: e });
+      const serialized = {
+        message: error.message,
+        stack: error.stack,
+        cause: error.cause,
+      };
+      import.meta.hot?.send("custom:runtime-error", serialized);
+    }
+  }
+}
