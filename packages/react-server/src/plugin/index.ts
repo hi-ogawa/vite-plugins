82008d141d6f5ca17b85038285d39f6902f72d7a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-01 20:10:48 +0900

    fix: update async reference patch

--- packages/react-server/src/plugin/index.ts
+++ packages/react-server/src/plugin/index.ts
@@ -187,7 +187,7 @@ export function vitePluginReactServer(options?: {
             // make server reference async for simplicity (stale chunkCache, etc...)
             // see TODO in https://github.com/facebook/react/blob/33a32441e991e126e5e874f831bd3afc237a3ecf/packages/react-server-dom-webpack/src/ReactFlightClientConfigBundlerWebpack.js#L131-L132
             code = code.replaceAll("if (isAsyncImport(metadata))", "if (true)");
-            code = code.replaceAll("4===a.length", "true");
+            code = code.replaceAll("4 === metadata.length", "true");
 
             return code;
           }
