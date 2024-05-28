0aa6014ca3a740ce2d92f52ed00f9ed3e76660f0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-28 18:57:48 +0900

    chore: lockfile

--- packages/transforms/src/index.ts
+++ packages/transforms/src/index.ts
@@ -1,6 +1,5 @@
 import type {} from "./types/estree"; // for rollup dts?
 export * from "./server-action";
-export * from "./client-reference";
 export * from "./hoist";
 export * from "./wrap-export";
 export * from "./utils";
