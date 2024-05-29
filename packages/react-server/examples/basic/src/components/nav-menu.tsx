16326070d64268b8eda0e81bbe336adf6034a436
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-29 16:38:12 +0900

    chore: tweak NavMenu

--- packages/react-server/examples/basic/src/components/nav-menu.tsx
+++ packages/react-server/examples/basic/src/components/nav-menu.tsx
@@ -14,8 +14,7 @@ export function NavMenu(props: {
           className="antd-link self-start justify-self-start"
           activeProps={props.activeProps}
         >
-          <li className="flex items-center">
-            <span className="text-lg pr-2 select-none">•</span>
+          <li className="flex items-center pt-0.5 before:content-['•'] before:pr-2">
             {e}
           </li>
         </Link>
