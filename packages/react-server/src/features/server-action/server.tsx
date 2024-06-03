b87581532f8468eddc6fc81cdbaeed621bf53049
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-03 14:16:38 +0900

    fix: typo

--- packages/react-server/src/features/server-action/server.tsx
+++ packages/react-server/src/features/server-action/server.tsx
@@ -1,6 +1,6 @@
 import reactServerDomClient from "react-server-dom-webpack/client.edge";
 
-/* @__NO_SIDE_EFFECT */
+/* @__NO_SIDE_EFFECTS__ */
 export function createServerReference(id: string) {
   return reactServerDomClient.createServerReference(id, (...args) => {
     console.error(args);
