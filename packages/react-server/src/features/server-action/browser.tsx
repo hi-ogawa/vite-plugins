b9ec2759362dcd645808bb4ea93a66ebb1bab05e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-03 14:25:51 +0900

    fix(react-server): tree shake unused references by `__NO_SIDE_EFFECTS__` (#336)

--- packages/react-server/src/features/server-action/browser.tsx
+++ packages/react-server/src/features/server-action/browser.tsx
@@ -3,6 +3,7 @@ import { $__global } from "../../lib/global";
 
 // https://github.com/facebook/react/blob/c8a035036d0f257c514b3628e927dd9dd26e5a09/packages/react-client/src/ReactFlightReplyClient.js#L758
 
+/* @__NO_SIDE_EFFECTS__ */
 export function createServerReference(id: string) {
   return reactServerDomClient.createServerReference(id, (...args) =>
     $__global.callServer(...args),
