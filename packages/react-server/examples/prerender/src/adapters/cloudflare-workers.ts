dc40a2aee09549a4115b221477ae3a1288d3dafc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-06 17:31:55 +0900

    chore: add example

---
+++ packages/react-server/examples/prerender/src/adapters/cloudflare-workers.ts
@@ -0,0 +1,5 @@
+import { handler } from "../entry-server";
+
+export default {
+  fetch: handler,
+};
---
+++ packages/react-server/examples/prerender/src/entry-client.tsx
@@ -0,0 +1,4 @@
+import "./style.css";
+import { start } from "@hiogawa/react-server/entry-browser";
+
+start();
