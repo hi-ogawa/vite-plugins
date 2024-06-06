789c7e87c0b3a51f9cd160526dbb41da3225713e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-06 19:21:11 +0900

    chore: comment

--- packages/react-server/examples/prerender/src/routes/posts/layout.tsx
+++ packages/react-server/examples/prerender/src/routes/posts/layout.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@hiogawa/react-server/client";
 import type { LayoutProps } from "@hiogawa/react-server/server";
 
+// https://github.com/TanStack/router/blob/4a2bb0412d83bfe4527b2db211f516992927e9a2/examples/react/basic-ssr-file-based/src/routes/posts.tsx
 export type PostType = {
   id: string;
   title: string;
