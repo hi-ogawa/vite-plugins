75a5127f9a504b8d2c71da7549c8cf94e1d22121
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-06 18:06:31 +0900

    feat: preview-prerender middleware

--- packages/react-server/examples/prerender/vite.config.ts
+++ packages/react-server/examples/prerender/vite.config.ts
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import path from "node:path";
 import { vitePluginReactServer } from "@hiogawa/react-server/plugin";
 import {
@@ -17,7 +18,27 @@ export default defineConfig({
     vitePluginLogger(),
     vitePluginSsrMiddleware({
       entry: process.env["SSR_ENTRY"] || "/src/adapters/node.ts",
-      preview: path.resolve("./dist/server/index.js"),
     }),
+    // TODO: expose prerender middleware from plugin
+    {
+      name: "preview-prerender",
+      apply: (_config, env) => !!env.isPreview,
+      configurePreviewServer(server) {
+        const outDir = server.config.build.outDir;
+        server.middlewares.use((req, res, next) => {
+          // rewrite url if `index.html` exists
+          const url = new URL(req.url!, "https://test.local");
+          if (fs.existsSync(path.join(outDir, url.pathname, "index.html"))) {
+            if (url.searchParams.has("__rsc")) {
+              req.url = path.posix.join(url.pathname, "index.data");
+              res.setHeader("content-type", "text/x-component;charset=utf-8");
+            } else {
+              req.url = path.posix.join(url.pathname, "index.html");
+            }
+          }
+          next();
+        });
+      },
+    },
   ],
 });
