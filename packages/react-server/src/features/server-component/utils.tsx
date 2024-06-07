fd31e25a27101780b3ba653ecac1b0cd7658c17e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-07 13:25:30 +0900

    wip

--- packages/react-server/src/features/server-component/utils.tsx
+++ packages/react-server/src/features/server-component/utils.tsx
@@ -16,6 +16,7 @@ export function wrapStreamRequestUrl(
   const newUrl = new URL(url, window.location.href);
   newUrl.pathname = posixJoin(newUrl.pathname, RSC_PATH);
   // TODO: remove params on prerendered paths for better caching
+  // TODO: move it to request headers and ssr can return no-cache?
   newUrl.searchParams.set(RSC_PARAM, JSON.stringify(param));
   return newUrl.toString();
 }
@@ -24,7 +25,7 @@ export function unwrapStreamRequest(request: Request) {
   const url = new URL(request.url);
   const isStream = url.pathname.endsWith(RSC_PATH);
   if (!isStream) {
-    return { url, request };
+    return { url, request, isStream };
   }
   url.pathname = url.pathname.slice(0, -RSC_PATH.length) || "/";
   const rawParam = url.searchParams.get(RSC_PARAM);
