e6f2782dec0958d2b27ccdbfaabd57c4d4d0bd58
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-07 15:33:25 +0900

    chore: vercel hybrid deploy (forgot to push) (#365)

--- packages/react-server/examples/prerender/misc/vercel/build.sh
+++ packages/react-server/examples/prerender/misc/vercel/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -eu -o pipefail
 
-# .vercel/
-#   project.json
-#   output/
-#     config.json
-#     static/              = build/client
-
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # clean
@@ -19,3 +13,16 @@ cp config.json .vercel/output/config.json
 # static
 mkdir -p .vercel/output/static
 cp -r ../../dist/client/. .vercel/output/static
+
+# functions
+mkdir -p .vercel/output/functions/index.func
+cp .vc-config.json .vercel/output/functions/index.func/.vc-config.json
+npx esbuild ../../dist/server/index.js \
+  --outfile=.vercel/output/functions/index.func/index.js \
+  --metafile=dist/esbuild-metafile.json \
+  --log-override:ignored-bare-import=silent \
+  --define:process.env.NODE_ENV='"production"' \
+  --bundle \
+  --minify \
+  --format=esm \
+  --platform=browser
--- packages/react-server/examples/prerender/src/adapters/vercel-edge.ts
+++ packages/react-server/examples/prerender/src/adapters/vercel-edge.ts
@@ -1,5 +1,3 @@
 import { handler } from "../entry-server";
 
-export default {
-  fetch: handler,
-};
+export default handler;
