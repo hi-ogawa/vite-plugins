f904d54db499706efc41227bc8ca46f3a12530c4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-07 11:01:48 +0900

    refactor: simplify stream request params

--- packages/react-server/src/features/server-component/utils.tsx
+++ packages/react-server/src/features/server-component/utils.tsx
@@ -10,6 +10,7 @@ const RSC_PATH = "__f.data";
 const RSC_PARAM = "__f";
 
 type StreamRequestParam = {
+  actionId?: string;
   lastPathname?: string;
   // TODO: refine revalitating each layout layer
   revalidate?: boolean;
@@ -25,6 +26,32 @@ export function wrapStreamRequestUrl(
   return newUrl.toString();
 }
 
+export function unwrapStreamRequest2(request: Request) {
+  const url = new URL(request.url);
+  const isStream = url.pathname.endsWith(RSC_PATH);
+  if (!isStream) {
+    return { url, request };
+  }
+  url.pathname = url.pathname.slice(0, -RSC_PATH.length) || "/";
+  const rawParam = url.searchParams.get(RSC_PARAM);
+  url.searchParams.delete(RSC_PARAM);
+
+  return {
+    url,
+    request: new Request(url, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+      // @ts-ignore undici
+      ...("duplex" in request ? { duplex: "half" } : {}),
+    }),
+    isStream,
+    streamParam: rawParam
+      ? (JSON.parse(rawParam) as StreamRequestParam)
+      : undefined,
+  };
+}
+
 export function unwrapStreamRequest(
   request: Request,
   actionResult?: ActionResult,
@@ -38,6 +65,7 @@ export function unwrapStreamRequest(
   const rscParam = url.searchParams.get(RSC_PARAM);
   url.searchParams.delete(RSC_PARAM);
 
+  // TODO: separate this part
   let layoutRequest = createLayoutContentRequest(url.pathname);
   if (rscParam && !actionResult?.context.revalidate) {
     const param = JSON.parse(rscParam) as StreamRequestParam;
