4366ebf7e43db783edbdd04e74c77e5ba2d1b6e0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-14 10:41:02 +0900

    chore: cleanup demo

--- packages/react-server/examples/prerender/src/routes/ppr/page.tsx
+++ packages/react-server/examples/prerender/src/routes/ppr/page.tsx
@@ -5,42 +5,25 @@ export default async function Page() {
   return (
     <div
       style={{
-        border: "1px solid #00000030",
+        border: "1px solid #0004",
         padding: "1rem",
+        maxWidth: "30rem",
       }}
     >
-      <h2>Static</h2>
+      <div>Static</div>
       <pre style={{ whiteSpace: "wrap" }}>
         [rendered at {new Date().toISOString()}]
       </pre>
       <div
         style={{
-          background: "#00ff0010",
-          marginBottom: "1rem",
-          height: "8rem",
-          width: "100%",
-          display: "grid",
-          alignContent: "center",
+          background: "#f002",
+          padding: "1rem",
+          height: "5rem",
         }}
       >
         <React.Suspense fallback={<div>Sleeping 1 sec ...</div>}>
           <Postpone>
-            <Sleep id="1" ms={1000} />
-          </Postpone>
-        </React.Suspense>
-      </div>
-      <div
-        style={{
-          background: "#ff000010",
-          height: "8rem",
-          width: "100%",
-          display: "grid",
-          alignContent: "center",
-        }}
-      >
-        <React.Suspense fallback={<div>Sleeping 2 sec ...</div>}>
-          <Postpone>
-            <Sleep id="2" ms={2000} />
+            <Sleep />
           </Postpone>
         </React.Suspense>
       </div>
@@ -48,15 +31,15 @@ export default async function Page() {
   );
 }
 
-async function Sleep(props: { id: string; ms: number }) {
-  await new Promise((r) => setTimeout(r, props.ms));
+async function Sleep() {
+  await new Promise((r) => setTimeout(r, 1000));
 
   return (
-    <div>
-      <h2>Dynamic {props.id}</h2>
+    <>
+      <div>Dynamic</div>
       <pre style={{ whiteSpace: "wrap" }}>
         [rendered at {new Date().toISOString()}]
       </pre>
-    </div>
+    </>
   );
 }
