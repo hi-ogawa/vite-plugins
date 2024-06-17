b91674632604275e7e5173cb51b42084651fdb90
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-17 13:46:47 +0900

    chore: rewrite build scripts in js

---
+++ packages/react-server/examples/prerender/misc/vercel/build.js
@@ -0,0 +1,73 @@
+import { cp, mkdir, rm, writeFile } from "node:fs/promises";
+import { join } from "node:path";
+import * as esbuild from "esbuild";
+
+const distDir = join(import.meta.dirname, "../../dist");
+const outDir = join(import.meta.dirname, ".vercel/output");
+
+const configJson = {
+  version: 3,
+  trailingSlash: false,
+  routes: [
+    {
+      src: "^/assets/(.*)$",
+      headers: {
+        "cache-control": "public, immutable, max-age=31536000",
+      },
+    },
+    {
+      handle: "filesystem",
+    },
+    {
+      src: ".*",
+      dest: "/",
+    },
+  ],
+};
+
+const vcConfigJson = {
+  runtime: "edge",
+  entrypoint: "index.js",
+};
+
+async function main() {
+  // clean
+  await rm(outDir, { recursive: true, force: true });
+  await mkdir(outDir, { recursive: true });
+
+  // config
+  await writeFile(
+    join(outDir, "config.json"),
+    JSON.stringify(configJson, null, 2),
+  );
+
+  // static
+  await mkdir(join(outDir, "static"), { recursive: true });
+  await cp(join(distDir, "client"), join(outDir, "static"), {
+    recursive: true,
+  });
+
+  // function
+  await mkdir(join(outDir, "functions/index.func"), { recursive: true });
+  await writeFile(
+    join(outDir, "functions/index.func/.vc-config.json"),
+    JSON.stringify(vcConfigJson, null, 2),
+  );
+
+  // bundle function
+  await esbuild.build({
+    entryPoints: [join(distDir, "server/index.js")],
+    outfile: join(outDir, "functions/index.func/index.js"),
+    bundle: true,
+    format: "esm",
+    platform: "browser",
+    define: {
+      "process.env.NODE_ENV": `"production"`,
+    },
+    logOverride: {
+      "ignored-bare-import": "silent",
+    },
+  });
+}
+
+main();
