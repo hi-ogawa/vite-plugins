8d2db0c28774a85df6d8321e91eeb53d93a37823
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-17 14:53:12 +0900

    chore: replace more

---
+++ packages/react-server/examples/basic/misc/cloudflare-workers/build.js
@@ -0,0 +1,38 @@
+// @ts-check
+
+import { cp, mkdir, rm } from "node:fs/promises";
+import { join } from "node:path";
+import * as esbuild from "esbuild";
+
+const buildDir = join(import.meta.dirname, "../../dist");
+const outDir = join(import.meta.dirname, "dist");
+
+async function main() {
+  // clean
+  await rm(outDir, { recursive: true, force: true });
+
+  // assets
+  await mkdir(join(outDir, "assets"), { recursive: true });
+  await cp(join(buildDir, "client"), join(outDir, "assets"), {
+    recursive: true,
+  });
+
+  // worker
+  await esbuild.build({
+    entryPoints: [join(buildDir, "server/index.js")],
+    outfile: join(outDir, "main.js"),
+    bundle: true,
+    minify: true,
+    format: "esm",
+    platform: "browser",
+    external: ["node:async_hooks"],
+    define: {
+      "process.env.NODE_ENV": `"production"`,
+    },
+    logOverride: {
+      "ignored-bare-import": "silent",
+    },
+  });
+}
+
+main();
