25995372e0ff568282ff27efb9828ec353ab0a81
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-24 20:53:03 +0900

    test: update e2e

--- packages/react-server/examples/next/e2e/basic.test.ts
+++ packages/react-server/examples/next/e2e/basic.test.ts
@@ -3,5 +3,5 @@ import { expect, test } from "@playwright/test";
 test("basic", async ({ page }) => {
   const res = await page.goto("/");
   expect(res?.status()).toBe(200);
-  await page.getByText("Hello world!").click();
+  await page.getByRole("img", { name: "Next.js logo" }).click();
 });
