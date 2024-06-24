940ffe76a3f995003620efc7053168feaa400695
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-24 20:57:17 +0900

    refactor: setup next alias automatically

--- packages/react-server/examples/next/vite.config.ts
+++ packages/react-server/examples/next/vite.config.ts
@@ -1,34 +1,8 @@
 import { vitePluginReactServerNext } from "@hiogawa/react-server/next/plugin";
 import react from "@vitejs/plugin-react";
-import { type AliasOptions, defineConfig } from "vite";
-
-const nextAlias: AliasOptions = [
-  {
-    find: /^next(\/.*)?/,
-    replacement: "@hiogawa/react-server/next/compat$1",
-  },
-];
+import { defineConfig } from "vite";
 
 export default defineConfig({
   clearScreen: false,
-  resolve: {
-    alias: nextAlias,
-  },
-  plugins: [
-    react(),
-    vitePluginReactServerNext({
-      plugins: [
-        {
-          name: "next-compat-alias",
-          config(_config, _env) {
-            return {
-              resolve: {
-                alias: nextAlias,
-              },
-            };
-          },
-        },
-      ],
-    }),
-  ],
+  plugins: [react(), vitePluginReactServerNext()],
 });
--- packages/react-server/src/next/plugin.ts
+++ packages/react-server/src/next/plugin.ts
@@ -7,15 +7,32 @@ import {
   vitePluginLogger,
   vitePluginSsrMiddleware,
 } from "@hiogawa/vite-plugin-ssr-middleware";
-import type { PluginOption } from "vite";
+import type { AliasOptions, Plugin, PluginOption } from "vite";
+
+const nextAlias: AliasOptions = [
+  {
+    find: /^next(\/.*)?/,
+    replacement: "@hiogawa/react-server/next/compat$1",
+  },
+];
+
+const nextAliasPlugin: Plugin = {
+  name: "next-compat-alias",
+  config: () => ({
+    resolve: {
+      alias: nextAlias,
+    },
+  }),
+};
 
 export function vitePluginReactServerNext(options?: {
   plugins?: PluginOption[];
 }): PluginOption {
   return [
+    nextAliasPlugin,
     vitePluginReactServer({
       routeDir: "app",
-      plugins: options?.plugins,
+      plugins: [nextAliasPlugin, ...(options?.plugins ?? [])],
     }),
     vitePluginLogger(),
     vitePluginSsrMiddleware({
