02f9ace44e12b755a8eb5ffe3b62c75c4697cd92
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-24 21:55:27 +0900

    refactor: minor

--- packages/react-server/src/next/plugin.ts
+++ packages/react-server/src/next/plugin.ts
@@ -7,23 +7,7 @@ import {
   vitePluginLogger,
   vitePluginSsrMiddleware,
 } from "@hiogawa/vite-plugin-ssr-middleware";
-import type { AliasOptions, Plugin, PluginOption } from "vite";
-
-const nextAlias: AliasOptions = [
-  {
-    find: /^next(\/.*)?/,
-    replacement: "@hiogawa/react-server/next/compat$1",
-  },
-];
-
-const nextAliasPlugin: Plugin = {
-  name: "next-compat-alias",
-  config: () => ({
-    resolve: {
-      alias: nextAlias,
-    },
-  }),
-};
+import type { Plugin, PluginOption } from "vite";
 
 export function vitePluginReactServerNext(options?: {
   plugins?: PluginOption[];
@@ -41,3 +25,17 @@ export function vitePluginReactServerNext(options?: {
     }),
   ];
 }
+
+const nextAliasPlugin: Plugin = {
+  name: "next-compat-alias",
+  config: () => ({
+    resolve: {
+      alias: [
+        {
+          find: /^next(\/.*)?/,
+          replacement: "@hiogawa/react-server/next/compat$1",
+        },
+      ],
+    },
+  }),
+};
