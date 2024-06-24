c41ecbd7c6b6977c5b8bd87801ffd3a5c5d158c3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-24 19:24:21 +0900

    chore: update examples

--- packages/react-server/examples/next/src/adapters/node.ts
+++
@@ -1,4 +0,0 @@
-import { webToNodeHandler } from "@hiogawa/utils-node";
-import { handler } from "../entry-server";
-
-export default webToNodeHandler(handler);
--- packages/react-server/examples/next/vite.config.ts
+++ packages/react-server/examples/next/vite.config.ts
@@ -1,23 +1,8 @@
-import path from "node:path";
-import { vitePluginReactServer } from "@hiogawa/react-server/plugin";
-import {
-  vitePluginLogger,
-  vitePluginSsrMiddleware,
-} from "@hiogawa/vite-plugin-ssr-middleware";
+import { vitePluginReactServerNext } from "@hiogawa/react-server/next/plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   clearScreen: false,
-  plugins: [
-    react(),
-    vitePluginReactServer({
-      routeDir: "app",
-    }),
-    vitePluginLogger(),
-    vitePluginSsrMiddleware({
-      entry: process.env["SSR_ENTRY"] || "/src/adapters/node.ts",
-      preview: path.resolve("./dist/server/index.js"),
-    }),
-  ],
+  plugins: [react(), vitePluginReactServerNext()],
 });
