e77ff0e6ad05b0912961f0cfaaadba55dce8a897
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-25 11:38:26 +0900

    test: e2e

--- packages/react-server/examples/next/e2e/basic.test.ts
+++ packages/react-server/examples/next/e2e/basic.test.ts
@@ -5,3 +5,9 @@ test("basic", async ({ page }) => {
   expect(res?.status()).toBe(200);
   await page.getByRole("img", { name: "Next.js logo" }).click();
 });
+
+test("navigation", async ({ page }) => {
+  await page.goto("/navigation");
+  await page.getByRole("link", { name: "set Query" }).click();
+  await page.getByTestId("a=b&c=d").click();
+});
