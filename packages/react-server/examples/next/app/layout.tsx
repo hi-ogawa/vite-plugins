760b573bfbaa25568f28e7cbf2078181471e6dcb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-26 18:41:08 +0900

    chore: test tsconfig paths

--- packages/react-server/examples/next/app/layout.tsx
+++ packages/react-server/examples/next/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "@/app/globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
