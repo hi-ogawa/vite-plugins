a14bc15ede4b00cb7b339bf08bd98e62378e36f5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-26 18:23:02 +0900

    feat(next): auto setup vite.config.ts

--- packages/react-server-next/bin/cli.js
+++ packages/react-server-next/bin/cli.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import { spawn } from "node:child_process";
+import { existsSync, writeFileSync } from "node:fs";
 import { createRequire } from "node:module";
 import path from "node:path";
 import process from "node:process";
+import { fileURLToPath } from "node:url";
 
 const require = createRequire(import.meta.url);
 const viteBin = path.join(
@@ -29,6 +31,21 @@ if (!["dev", "build", "start"].includes(argv[0])) {
 // next start -> vite preview
 if (argv[0] === "start") {
   argv[0] = "preview";
+}
+
+// auto setup vite.config.ts
+if (!existsSync("vite.config.ts")) {
+  writeFileSync(
+    "vite.config.ts",
+    `\
+import next from "next/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [next()],
+});
+`,
+  );
 }
 
 spawn("node", [viteBin, ...argv], {
