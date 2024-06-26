2dc03046b6c0cb7394909793d2d7f24828e874b4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-26 17:33:24 +0900

    feat(next): ambient type via `next-env.d.ts`

---
+++ packages/react-server-next/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+/// <reference types="react" />
+/// <reference types="react/experimental" />
+/// <reference types="react-dom" />
+/// <reference types="react-dom/experimental" />
+
+export * from "./dist/compat";
