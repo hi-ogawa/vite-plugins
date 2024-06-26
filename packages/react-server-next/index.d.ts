1d14b8e0d9be76114faec8deca3a919795c40be5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-27 08:30:38 +0900

    chore: comment

--- packages/react-server-next/index.d.ts
+++ packages/react-server-next/index.d.ts
@@ -1,3 +1,5 @@
+// cf. https://github.com/vercel/next.js/blob/4b7924b15593322633fe2847f52bac8dbd5d9047/packages/next/index.d.ts
+
 /// <reference types="vite/client" />
 /// <reference types="react" />
 /// <reference types="react/experimental" />
