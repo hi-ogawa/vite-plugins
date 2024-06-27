97b89f4e41ab379a42787a124def3658b86ec74f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-27 16:26:45 +0900

    fix(next): relax `Metadata` types (#451)

--- packages/react-server/src/features/meta/server.tsx
+++ packages/react-server/src/features/meta/server.tsx
@@ -1,9 +1,16 @@
+import { objectHas } from "@hiogawa/utils";
 import type { Metadata } from "./utils";
 
 export function renderMetadata(m: Metadata) {
+  const title =
+    typeof m.title === "string"
+      ? m.title
+      : objectHas(m.title, "default")
+        ? m.title.default
+        : null;
   return (
     <>
-      {typeof m.title === "string" && <title>{m.title}</title>}
+      {typeof title === "string" && <title>{title}</title>}
       {typeof m.description === "string" && (
         <meta name="description" content={m.description} />
       )}
--- packages/react-server/src/features/meta/utils.ts
+++ packages/react-server/src/features/meta/utils.ts
@@ -1,5 +1,6 @@
 // cf. https://github.com/vercel/next.js/blob/4b7924b15593322633fe2847f52bac8dbd5d9047/packages/next/src/lib/metadata/types/metadata-interface.ts#L40
 export type Metadata = {
-  title?: null | string;
+  title?: null | string | { default: string; [k: string]: unknown };
   description?: null | string;
+  [k: string]: unknown;
 };
