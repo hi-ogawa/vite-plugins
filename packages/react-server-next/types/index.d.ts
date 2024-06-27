aaf8991e6946468f1143642395ba6f98b9db4195
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-27 09:09:38 +0900

    chore: move code

--- packages/react-server-next/types/index.d.ts
+++ packages/react-server-next/types/index.d.ts
@@ -1,4 +1,5 @@
-// cf. https://github.com/vercel/next.js/blob/4b7924b15593322633fe2847f52bac8dbd5d9047/packages/next/index.d.ts
+// cf.
+// https://github.com/vercel/next.js/blob/4b7924b15593322633fe2847f52bac8dbd5d9047/packages/next/index.d.ts
 
 /// <reference types="vite/client" />
 /// <reference types="react" />
@@ -6,4 +7,4 @@
 /// <reference types="react-dom" />
 /// <reference types="react-dom/experimental" />
 
-export * from "./dist/compat";
+export * from "../dist/compat";
