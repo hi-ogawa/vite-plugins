d76649ff679f16956077341a910b74282a453d12
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-27 14:44:53 +0900

    fix(next): support `app/favicon.ico` (#448)

--- packages/react-server-next/src/vite/index.ts
+++ packages/react-server-next/src/vite/index.ts
@@ -1,3 +1,4 @@
+import { existsSync, readFileSync } from "node:fs";
 import path from "node:path";
 import { vitePluginReactServer } from "@hiogawa/react-server/plugin";
 import {
@@ -5,7 +6,7 @@ import {
   vitePluginSsrMiddleware,
 } from "@hiogawa/vite-plugin-ssr-middleware";
 import react from "@vitejs/plugin-react";
-import type { PluginOption } from "vite";
+import type { Plugin, PluginOption } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default function vitePluginReactServerNext(options?: {
@@ -36,6 +37,7 @@ export default function vitePluginReactServerNext(options?: {
       entry: "next/vite/entry-ssr",
       preview: path.resolve("./dist/server/index.js"),
     }),
+    appFaviconPlugin(),
     {
       name: "next-exclude-optimize",
       config: () => ({
@@ -48,4 +50,34 @@ export default function vitePluginReactServerNext(options?: {
       }),
     },
   ];
+}
+
+/** @todo https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons */
+function appFaviconPlugin(): Plugin {
+  // not sure what exactly Next.js does.
+  // for now, let's do quick workaround for app/favicon.ico
+  return {
+    name: appFaviconPlugin.name,
+    apply: (_config, env) => env.command === "serve" || !env.isSsrBuild,
+    configureServer(server) {
+      if (existsSync("app/favicon.ico")) {
+        server.middlewares.use((req, _res, next) => {
+          const url = new URL(req.url || "", "https://tmp.local");
+          if (url.pathname === "/favicon.ico") {
+            req.url = "/app/favicon.ico";
+          }
+          next();
+        });
+      }
+    },
+    generateBundle() {
+      if (existsSync("app/favicon.ico")) {
+        this.emitFile({
+          type: "asset",
+          fileName: "favicon.ico",
+          source: readFileSync("app/favicon.ico"),
+        });
+      }
+    },
+  };
 }
