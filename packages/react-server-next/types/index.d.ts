70a4b03e6ce3ae257d9446dae89fbadb992cfe34
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-27 10:03:06 +0900

    fix: RequestInit.next

--- packages/react-server-next/types/index.d.ts
+++ packages/react-server-next/types/index.d.ts
@@ -1,10 +1,14 @@
-// cf.
-// https://github.com/vercel/next.js/blob/4b7924b15593322633fe2847f52bac8dbd5d9047/packages/next/index.d.ts
-
 /// <reference types="vite/client" />
 /// <reference types="react" />
 /// <reference types="react/experimental" />
 /// <reference types="react-dom" />
 /// <reference types="react-dom/experimental" />
+
+declare global {
+  interface RequestInit {
+    /** @todo */
+    next?: unknown;
+  }
+}
 
 export * from "../dist/compat";
