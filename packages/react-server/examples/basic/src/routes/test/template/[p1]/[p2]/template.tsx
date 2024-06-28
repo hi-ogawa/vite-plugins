2a28268306eebfa9db8fb79a47ce83b2f2c0f231
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-28 15:15:14 +0900

    chore: tweak demo

--- packages/react-server/examples/basic/src/routes/test/template/[p1]/[p2]/template.tsx
+++ packages/react-server/examples/basic/src/routes/test/template/[p1]/[p2]/template.tsx
@@ -1,7 +1,11 @@
+import { ClientTime } from "../../_client";
+
 export default function Template(props: React.PropsWithChildren) {
   return (
     <div className="border p-2">
-      <div>/template/[p1]/[p2]/template.tsx</div>
+      <div>
+        /template/[p1]/[p2]/template.tsx <ClientTime />
+      </div>
       {props.children}
     </div>
   );
