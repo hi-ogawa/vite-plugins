968ea78852f3809f197166bafd6ee28fa0466377
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-02 08:12:36 +0900

    fix(next): correctly setup ssg html on vercel adapter (#475)

--- packages/react-server/examples/prerender/misc/vercel/build.js
+++ packages/react-server/examples/prerender/misc/vercel/build.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-import { cp, mkdir, rm, writeFile } from "node:fs/promises";
+import { cp, mkdir, readFile, rm, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 import * as esbuild from "esbuild";
 
@@ -25,17 +25,37 @@ const configJson = {
       dest: "/",
     },
   ],
+  overrides: {},
 };
 
-const vcConfigJson = {
-  runtime: "edge",
-  entrypoint: "index.js",
-};
+const isNodeRuntime = process.env["VERCE_RUNTIME"];
+
+const vcConfigJson = isNodeRuntime
+  ? {
+      runtime: "nodejs20.x",
+      handler: "index.mjs",
+      launcherType: "Nodejs",
+      regions: ["hnd1"],
+    }
+  : {
+      runtime: "edge",
+      entrypoint: "index.mjs",
+    };
 
 async function main() {
   // clean
   await rm(outDir, { recursive: true, force: true });
   await mkdir(outDir, { recursive: true });
+
+  // overrides for ssg html
+  // https://vercel.com/docs/build-output-api/v3/configuration#overrides
+  /** @type {import("@hiogawa/react-server/server").PrerenderEntry[]} */
+  const entries = JSON.parse(
+    await readFile(join(buildDir, "client/__prerender.json"), "utf-8"),
+  );
+  configJson.overrides = Object.fromEntries(
+    entries.map((e) => [e.html.slice(1), { path: e.route }]),
+  );
 
   // config
   await writeFile(
@@ -58,12 +78,16 @@ async function main() {
 
   // bundle function
   await esbuild.build({
-    entryPoints: [join(buildDir, "server/index.js")],
-    outfile: join(outDir, "functions/index.func/index.js"),
+    entryPoints: [
+      isNodeRuntime
+        ? join(import.meta.dirname, "entry-node.js")
+        : join(buildDir, "server/index.js"),
+    ],
+    outfile: join(outDir, "functions/index.func/index.mjs"),
     bundle: true,
     minify: true,
     format: "esm",
-    platform: "browser",
+    platform: isNodeRuntime ? "node" : "browser",
     define: {
       "process.env.NODE_ENV": `"production"`,
     },
