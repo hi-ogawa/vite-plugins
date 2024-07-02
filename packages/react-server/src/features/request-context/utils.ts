51bb654d97b1ccae6c13b99a2b21a481e4297321
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-02 16:49:01 +0900

    chore: comment

--- packages/react-server/src/features/request-context/utils.ts
+++ packages/react-server/src/features/request-context/utils.ts
@@ -1,5 +1,6 @@
 // for now, we don't require async hooks and fallbacks to sync context
 // if it's not availalbe globally like React.cache.
+// https://github.com/facebook/react/blob/f14d7f0d2597ea25da12bcf97772e8803f2a394c/packages/react-server/src/forks/ReactFlightServerConfig.dom-edge.js#L16-L19
 
 type ContextStorage<T> = {
   run<R>(store: T, callback: () => R): R;
