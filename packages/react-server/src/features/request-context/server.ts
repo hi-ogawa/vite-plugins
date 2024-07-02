235a680df2a2e10b8eeb048a21dd32b50200430f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-02 18:42:51 +0900

    refactor: add createCookiesWrapper

--- packages/react-server/src/features/request-context/server.ts
+++ packages/react-server/src/features/request-context/server.ts
@@ -5,35 +5,16 @@ import { createContextStorage } from "./utils";
 const requestContextStorage = createContextStorage<RequestContext>();
 
 export class RequestContext {
-  public requestCookies: RequestCookies;
-  public responseCookies: ResponseCookies;
+  public cookiesWrapper: ReturnType<typeof createCookiesWrapper>;
 
   // TODO: multiple revlidation paths
   public revalidate?: RevalidationType;
 
-  constructor(public headers: Headers) {
-    // TODO: refactor
-    // it seems Next.js's cookies API has to track modified response cookies
-    // so that mutable `cookies()` allows reading current cookies as well.
-    // https://github.com/vercel/next.js/blob/6795597a50112c4f83bf61caf3681e95816da4c9/packages/next/src/server/web/spec-extension/adapters/request-cookies.ts#L25-L33
-    // for now, we just diff two cookies at the end to decide final set-cookie headers
-    this.requestCookies = new RequestCookies(headers);
-    this.responseCookies = new ResponseCookies(new Headers());
-    for (const cookie of this.requestCookies.getAll()) {
-      this.responseCookies.set(cookie);
-    }
+  constructor(public requestHeaders: Headers) {
+    this.cookiesWrapper = createCookiesWrapper(requestHeaders);
   }
 
-  getSetCookie() {
-    const result = new ResponseCookies(new Headers());
-    for (const cookie of this.responseCookies.getAll()) {
-      const reqCookie = this.requestCookies.get(cookie.name);
-      if (reqCookie?.value !== cookie.value) {
-        result.set(cookie);
-      }
-    }
-    return result.toString();
-  }
+  getSetCookie = () => this.cookiesWrapper.getSetCookie();
 
   run<T>(f: () => T): T {
     return requestContextStorage.run(this, f);
@@ -52,13 +33,38 @@ export class RequestContext {
 }
 
 export function headers() {
-  return RequestContext.get().headers;
+  return RequestContext.get().requestHeaders;
 }
 
 export function cookies() {
-  return RequestContext.get().responseCookies;
+  return RequestContext.get().cookiesWrapper.cookies;
 }
 
 export function revalidatePath(path: string) {
   RequestContext.get().revalidate = path;
+}
+
+// it seems Next.js's cookies API has to track modified response cookies
+// so that mutable `cookies()` allows reading current cookies as well.
+// https://github.com/vercel/next.js/blob/6795597a50112c4f83bf61caf3681e95816da4c9/packages/next/src/server/web/spec-extension/adapters/request-cookies.ts#L25-L33
+// for now, we just diff two cookies at the end to decide final set-cookie header
+function createCookiesWrapper(requestHeaders: Headers) {
+  const requestCookies = new RequestCookies(requestHeaders);
+  const cookies = new ResponseCookies(new Headers());
+  for (const cookie of requestCookies.getAll()) {
+    cookies.set(cookie);
+  }
+
+  function getSetCookie() {
+    const result = new ResponseCookies(new Headers());
+    for (const cookie of cookies.getAll()) {
+      const reqCookie = requestCookies.get(cookie.name);
+      if (reqCookie?.value !== cookie.value) {
+        result.set(cookie);
+      }
+    }
+    return result.toString();
+  }
+
+  return { cookies, getSetCookie };
 }
