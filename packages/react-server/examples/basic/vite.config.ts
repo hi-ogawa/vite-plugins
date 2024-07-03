0c884395e06fa1efb262f70180976b1951c479c9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-03 10:51:52 +0900

    fix: e2e

--- packages/react-server/examples/basic/vite.config.ts
+++ packages/react-server/examples/basic/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         patchConsoleError: true,
       }),
     vitePluginReactServer({
+      entryBrowser: "/src/entry-client",
       entryServer: "/src/entry-react-server",
       plugins: [
         testVitePluginVirtual(),
