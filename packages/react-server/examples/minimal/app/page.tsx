e5e9aff16ed85edfe824a33c00f00880b126f439
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-04 08:36:41 +0900

    test: tweak examples/minimal

--- packages/react-server/examples/minimal/app/page.tsx
+++ packages/react-server/examples/minimal/app/page.tsx
@@ -3,12 +3,12 @@ import { TestClient } from "./_client";
 
 export default function Page() {
   return (
-    <>
-      <div>[Page]</div>
+    <div>
+      <h3>[Home]</h3>
       <TestClient />
       <form action={changeCount}>
         <button>Action: {getCount()}</button>
       </form>
-    </>
+    </div>
   );
 }
