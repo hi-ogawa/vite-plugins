b0d86cc174d8c0347fb9b94dfe988bd510cef6a6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-03 11:08:31 +0900

    feat(next): support next/image preload

--- packages/react-server-next/src/compat/image.tsx
+++ packages/react-server-next/src/compat/image.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import ReactDOM from "react-dom";
+
 /** @todo */
 export default function Image({
   priority,
@@ -7,9 +9,22 @@ export default function Image({
   blurDataURL,
   ...props
 }: JSX.IntrinsicElements["img"] & {
-  priority?: unknown;
+  priority?: boolean;
   placeholder?: unknown;
   blurDataURL?: unknown;
 }) {
+  // support only preload for now
+  // https://github.com/vercel/next.js/blob/82639520d3b70d6b532ce5ec650c0c5b268706a4/packages/next/src/client/image-component.tsx#L329-L337
+  if (props.src && priority) {
+    ReactDOM.preload(props.src, {
+      as: "image",
+      fetchPriority: "high",
+      imageSrcSet: props.srcSet,
+      imageSizes: props.sizes,
+      crossOrigin: props.crossOrigin,
+      referrerPolicy: props.referrerPolicy,
+    });
+  }
+
   return <img {...props} />;
 }
