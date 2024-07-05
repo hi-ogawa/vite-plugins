9034927e75a4040ff66f92722e746be7b5e9be87
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-05 09:01:04 +0900

    chore: copy https://github.com/hi-ogawa/vite-plugins/pull/396

--- packages/react-server/examples/prerender/vite.config.ts
+++ packages/react-server/examples/prerender/vite.config.ts
@@ -15,11 +15,25 @@ export default defineConfig({
       noAsyncLocalStorage: true,
       prerender: async (_manifest, presets) => {
         return [
-          ...(await presets.static()),
+          ...(await presets.static()).filter((s) => s !== "/ppr"),
           ...(await presets.generateStaticParams()),
         ];
       },
+      ppr: async () => {
+        return ["/ppr"];
+      },
     }),
+    {
+      // disable compressions as it breaks html streaming
+      // https://github.com/vitejs/vite/blob/9f5c59f07aefb1756a37bcb1c0aff24d54288950/packages/vite/src/node/preview.ts#L178
+      name: "no-compression",
+      configurePreviewServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          delete req.headers["accept-encoding"];
+          next();
+        });
+      },
+    },
     vitePluginLogger(),
     vitePluginSsrMiddleware({
       entry: process.env["SSR_ENTRY"] || "/src/adapters/node.ts",
