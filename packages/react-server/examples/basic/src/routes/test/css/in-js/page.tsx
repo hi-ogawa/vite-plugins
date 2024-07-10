c8fde944aee52f372c1a1f534a5d465ad5ad9448
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-10 16:03:38 +0900

    chore: add styled-jsx demo

--- packages/react-server/examples/basic/src/routes/test/css/in-js/page.tsx
+++ packages/react-server/examples/basic/src/routes/test/css/in-js/page.tsx
@@ -1,9 +1,25 @@
 "use client";
 
 import { useServerInsertedHTML } from "@hiogawa/react-server/client";
+import React from "react";
+import { StyleRegistry, createStyleRegistry } from "styled-jsx";
+import JSXStyle from "styled-jsx/style";
+
+// https://github.com/vercel/styled-jsx#server-side-rendering
+// https://github.com/vercel/styled-jsx#how-it-works
 
 export default function Page() {
-  useServerInsertedHTML;
+  const [registry] = React.useState(() => createStyleRegistry());
+  useServerInsertedHTML(() => <>{registry.styles()}</>);
 
-  return <div>CSS in JS</div>;
+  return (
+    <StyleRegistry registry={registry}>
+      <button className="jsx-123">CSS in JS</button>
+      <JSXStyle id="123">{`
+        button.jsx-123 {
+          color: red;
+        }
+      `}</JSXStyle>
+    </StyleRegistry>
+  );
 }
