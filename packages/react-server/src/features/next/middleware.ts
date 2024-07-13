289abcbb9f5d659d5fdb5875a473bcd93fd06c08
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-13 09:55:22 +0900

    wip: eager module

--- packages/react-server/src/features/next/middleware.ts
+++ packages/react-server/src/features/next/middleware.ts
@@ -1,16 +1,27 @@
 import type { RequestContext } from "../request-context/server";
 import type { NextRequest, NextResponse } from "./request";
 
+// https://nextjs.org/docs/app/api-reference/file-conventions/middleware
+
+// first goal is to support next-auth
+// https://github.com/nextauthjs/next-auth/blob/a3d3d4bab3e037a5359ed22de8b1fff0b5557523/packages/next-auth/src/index.ts#L86
+// https://github.com/nextauthjs/next-auth/blob/a3d3d4bab3e037a5359ed22de8b1fff0b5557523/packages/next-auth/src/lib/index.ts#L3
+
 export type MiddlewareModule = {
   middleware: (request: NextRequest) => Promise<NextResponse>;
   config: { matcher: string };
 };
 
 export async function handleMiddleware(
-  module: MiddlewareModule,
+  { middleware, config }: MiddlewareModule,
   requestContext: RequestContext,
 ) {
-  module.config.matcher;
-  module.middleware;
+  // TODO: matcher
+  config.matcher;
+
+  // TODO
+  // NextRequest
+  // NextResponse (redirect, next)
+  middleware;
   requestContext;
 }
--- packages/react-server/src/features/next/request.ts
+++ packages/react-server/src/features/next/request.ts
@@ -1,9 +1,21 @@
-// initial target is to support next-auth
-// https://github.com/nextauthjs/next-auth/blob/a3d3d4bab3e037a5359ed22de8b1fff0b5557523/packages/next-auth/src/index.ts#L86
-// https://github.com/nextauthjs/next-auth/blob/a3d3d4bab3e037a5359ed22de8b1fff0b5557523/packages/next-auth/src/lib/index.ts#L3
+import { RequestCookies, ResponseCookies } from "@edge-runtime/cookies";
 
 // https://github.com/vercel/next.js/blob/48f62e059317f78f07d71541a20e81d68f78e298/packages/next/src/server/web/spec-extension/request.ts
-export class NextRequest extends Request {}
+export class NextRequest extends Request {
+  cookies: RequestCookies;
+
+  constructor(...args: ConstructorParameters<typeof Request>) {
+    super(...args);
+    this.cookies = new RequestCookies(this.headers);
+  }
+}
 
 // https://github.com/vercel/next.js/blob/48f62e059317f78f07d71541a20e81d68f78e298/packages/next/src/server/web/spec-extension/response.ts
-export class NextResponse extends Response {}
+export class NextResponse extends Response {
+  cookies: ResponseCookies;
+
+  constructor(...args: ConstructorParameters<typeof Response>) {
+    super(...args);
+    this.cookies = new ResponseCookies(this.headers);
+  }
+}
