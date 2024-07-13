9560dfabbbc9db2b1dce889b1088c4d85a04a38f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-13 10:39:41 +0900

    chore: todo

--- packages/react-server/src/features/next/middleware.ts
+++ packages/react-server/src/features/next/middleware.ts
@@ -1,14 +1,23 @@
 import type { RequestContext } from "../request-context/server";
-import { NEXT_HANDLER_KEY, NextRequest, NextResponse } from "./request";
+import {
+  NEXT_HANDLER_KEY,
+  type NextFetchEvent,
+  NextRequest,
+  NextResponse,
+} from "./request";
 
 // https://nextjs.org/docs/app/api-reference/file-conventions/middleware
+// https://nextjs.org/docs/app/building-your-application/routing/middleware
 
 // first goal is to support next-auth
 // https://github.com/nextauthjs/next-auth/blob/a3d3d4bab3e037a5359ed22de8b1fff0b5557523/packages/next-auth/src/index.ts#L86
 // https://github.com/nextauthjs/next-auth/blob/a3d3d4bab3e037a5359ed22de8b1fff0b5557523/packages/next-auth/src/lib/index.ts#L3
 
 export type MiddlewareModule = {
-  middleware: (request: NextRequest) => Promise<Response>;
+  middleware: (
+    request: NextRequest,
+    event: NextFetchEvent,
+  ) => Promise<Response | undefined>;
   config?: { matcher: string };
 };
 
@@ -26,14 +35,15 @@ export async function handleMiddleware(
     headers: request.headers,
   });
 
-  const response = await middleware(nextRequest);
+  const response = await middleware(nextRequest, { waitUntil: () => {} });
+  if (!response) return;
+
   if (response instanceof NextResponse) {
     response.headers.set("set-cookie", response.cookies.toString());
   }
 
-  // add up headers to context if `NextResponse.next()`
+  // TODO: add up headers to context if `NextResponse.next()`
   if (response.headers.has(NEXT_HANDLER_KEY)) {
-    // TODO
     requestContext;
     return;
   }
