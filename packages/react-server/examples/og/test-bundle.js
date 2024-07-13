9fec7c6fffbe48a11004443f4a4dfc06f0840be0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-13 13:36:26 +0900

    chore: experiment with esbuild

--- packages/react-server/examples/og/test-bundle.js
+++ packages/react-server/examples/og/test-bundle.js
@@ -1,18 +1,35 @@
 import path from "node:path";
 import { nodeFileTrace } from "@vercel/nft";
+import * as esbuild from "esbuild";
 
 async function main() {
-  const [arg = "esbuld"] = process.argv.slice(2);
+  const [arg = "esbuild"] = process.argv.slice(2);
+
+  const entry = path.join(import.meta.dirname, "dist/server/index.js");
 
   if (arg === "nft") {
-    const result = await nodeFileTrace(
-      [path.join(import.meta.dirname, "dist/server/index.js")],
-      {
-        // set pnpm project root to correctly traverse dependency
-        base: path.join(import.meta.dirname, "../../../.."),
-      },
-    );
+    const result = await nodeFileTrace([entry], {
+      // set pnpm project root to correctly traverse dependency
+      base: path.join(import.meta.dirname, "../../../.."),
+    });
     console.log(result);
+  }
+
+  if (arg === "esbuild") {
+    await esbuild.build({
+      entryPoints: [entry],
+      outfile: path.join(import.meta.dirname, "dist/esbuild"),
+      bundle: true,
+      format: "esm",
+      platform: "node",
+      define: {
+        "process.env.NODE_ENV": `"production"`,
+      },
+      logLevel: "info",
+      logOverride: {
+        "ignored-bare-import": "silent",
+      },
+    });
   }
 
   if (arg === "rolldown") {
