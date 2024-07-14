7b642426e4381ed462618e51235070e0fe713b23
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-14 16:29:48 +0900

    fix: NextURL.clone

--- packages/react-server/src/features/next/request.ts
+++ packages/react-server/src/features/next/request.ts
@@ -3,12 +3,12 @@ import { RequestCookies, ResponseCookies } from "@edge-runtime/cookies";
 // https://github.com/vercel/next.js/blob/48f62e059317f78f07d71541a20e81d68f78e298/packages/next/src/server/web/spec-extension/request.ts
 export class NextRequest extends Request {
   cookies: RequestCookies;
-  nextUrl: URL;
+  nextUrl: NextURL;
 
   constructor(...args: ConstructorParameters<typeof Request>) {
     super(...args);
     this.cookies = new RequestCookies(this.headers);
-    this.nextUrl = new URL(this.url);
+    this.nextUrl = new NextURL(this.url);
   }
 }
 
@@ -39,6 +39,12 @@ export class NextResponse extends Response {
 }
 
 export const MIDDLEWARE_NEXT_KEY = "x-middleware-next";
+
+class NextURL extends URL {
+  clone() {
+    return new NextURL(this);
+  }
+}
 
 /** @todo */
 export type NextFetchEvent = {
