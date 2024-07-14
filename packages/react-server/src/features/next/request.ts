22cf3c45616b6dbf95d97056111da4c681fd308a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-14 16:24:02 +0900

    fix: shim `NextRequest.nextUrl`

--- packages/react-server/src/features/next/request.ts
+++ packages/react-server/src/features/next/request.ts
@@ -3,10 +3,12 @@ import { RequestCookies, ResponseCookies } from "@edge-runtime/cookies";
 // https://github.com/vercel/next.js/blob/48f62e059317f78f07d71541a20e81d68f78e298/packages/next/src/server/web/spec-extension/request.ts
 export class NextRequest extends Request {
   cookies: RequestCookies;
+  nextUrl: URL;
 
   constructor(...args: ConstructorParameters<typeof Request>) {
     super(...args);
     this.cookies = new RequestCookies(this.headers);
+    this.nextUrl = new URL(this.url);
   }
 }
 
