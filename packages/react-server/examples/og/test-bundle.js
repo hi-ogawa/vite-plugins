b7ce126cb46ee570f8bada2035935d5094b25c59
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-16 13:28:42 +0900

    chore: fix og test bundle script (#574)

--- packages/react-server/examples/og/test-bundle.js
+++ packages/react-server/examples/og/test-bundle.js
@@ -1,15 +1,15 @@
 // @ts-check
 
-import { mkdir, writeFile } from "node:fs/promises";
+import { mkdir, rm, writeFile } from "node:fs/promises";
 import path from "node:path";
 
 async function main() {
   const [arg = "esbuild"] = process.argv.slice(2);
-
   const entry = path.join(import.meta.dirname, "dist/server/index.js");
 
   if (arg === "nft") {
     const { nodeFileTrace } = await import("@vercel/nft");
+
     const result = await nodeFileTrace([entry], {
       // set pnpm project root to correctly traverse dependency
       base: path.join(import.meta.dirname, "../../../.."),
@@ -25,6 +25,7 @@ async function main() {
   if (arg === "ncc") {
     /** @type {any} */
     const { default: ncc } = await import("@vercel/ncc");
+
     const result = await ncc(entry, {
       filterAssetBase: path.join(import.meta.dirname, "../../../.."),
     });
@@ -41,13 +42,15 @@ async function main() {
 
     await esbuild.build({
       entryPoints: [entry],
-      outfile: path.join(import.meta.dirname, "dist/esbuild"),
+      outdir: path.join(import.meta.dirname, "dist/esbuild"),
       bundle: true,
       format: "esm",
       platform: "node",
       define: {
         "process.env.NODE_ENV": `"production"`,
       },
+      // split dynamic import like rollup/down
+      // splitting: true,
       logLevel: "info",
       logOverride: {
         "ignored-bare-import": "silent",
@@ -57,11 +60,23 @@ async function main() {
 
   if (arg === "rolldown") {
     const rolldown = await import("rolldown");
+    const { default: replace } = await import("@rollup/plugin-replace");
+
     const bundle = await rolldown.rolldown({
       input: entry,
-      cwd: import.meta.dirname,
+      platform: "node",
+      plugins: [
+        // @ts-ignore rollup/rolldown plugin type
+        replace({
+          preventAssignment: true,
+          values: {
+            "process.env.NODE_ENV": `"production"`,
+          },
+        }),
+      ],
     });
     const outDir = path.join(import.meta.dirname, "dist/rolldown");
+    await rm(outDir, { recursive: true, force: true });
     await mkdir(outDir, { recursive: true });
     await bundle.write({
       dir: outDir,
@@ -70,13 +85,32 @@ async function main() {
 
   if (arg === "rollup") {
     const rollup = await import("rollup");
+    const { default: replace } = await import("@rollup/plugin-replace");
+    const { default: nodeResolve } = await import(
+      "@rollup/plugin-node-resolve"
+    );
+    const { default: commonjs } = await import("@rollup/plugin-commonjs");
+
     const bundle = await rollup.rollup({
       input: entry,
+      plugins: [
+        replace({
+          preventAssignment: true,
+          values: {
+            "process.env.NODE_ENV": `"production"`,
+          },
+        }),
+        nodeResolve(),
+        commonjs(),
+      ],
     });
     const outDir = path.join(import.meta.dirname, "dist/rollup");
+    await rm(outDir, { recursive: true, force: true });
     await mkdir(outDir, { recursive: true });
     await bundle.write({
       dir: outDir,
+      // bundle dynamic import like esbuild without splitting
+      // inlineDynamicImports: true,
     });
   }
 }
