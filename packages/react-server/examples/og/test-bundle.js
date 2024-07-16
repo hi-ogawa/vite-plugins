dab937296f232f44f8b20f23cb278e3ebc49835a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-16 13:19:37 +0900

    chore: tweak

--- packages/react-server/examples/og/test-bundle.js
+++ packages/react-server/examples/og/test-bundle.js
@@ -5,11 +5,11 @@ import path from "node:path";
 
 async function main() {
   const [arg = "esbuild"] = process.argv.slice(2);
-
   const entry = path.join(import.meta.dirname, "dist/server/index.js");
 
   if (arg === "nft") {
     const { nodeFileTrace } = await import("@vercel/nft");
+
     const result = await nodeFileTrace([entry], {
       // set pnpm project root to correctly traverse dependency
       base: path.join(import.meta.dirname, "../../../.."),
@@ -25,6 +25,7 @@ async function main() {
   if (arg === "ncc") {
     /** @type {any} */
     const { default: ncc } = await import("@vercel/ncc");
+
     const result = await ncc(entry, {
       filterAssetBase: path.join(import.meta.dirname, "../../../.."),
     });
@@ -59,9 +60,20 @@ async function main() {
 
   if (arg === "rolldown") {
     const rolldown = await import("rolldown");
+    const { default: replace } = await import("@rollup/plugin-replace");
+
     const bundle = await rolldown.rolldown({
       input: entry,
       platform: "node",
+      plugins: [
+        // @ts-ignore rollup/rolldown plugin type
+        replace({
+          preventAssignment: true,
+          values: {
+            "process.env.NODE_ENV": `"production"`,
+          },
+        }),
+      ],
     });
     const outDir = path.join(import.meta.dirname, "dist/rolldown");
     await rm(outDir, { recursive: true, force: true });
@@ -73,8 +85,24 @@ async function main() {
 
   if (arg === "rollup") {
     const rollup = await import("rollup");
+    const { default: replace } = await import("@rollup/plugin-replace");
+    const { default: nodeResolve } = await import(
+      "@rollup/plugin-node-resolve"
+    );
+    const { default: commonjs } = await import("@rollup/plugin-commonjs");
+
     const bundle = await rollup.rollup({
       input: entry,
+      plugins: [
+        replace({
+          preventAssignment: true,
+          values: {
+            "process.env.NODE_ENV": `"production"`,
+          },
+        }),
+        nodeResolve(),
+        commonjs(),
+      ],
     });
     const outDir = path.join(import.meta.dirname, "dist/rollup");
     await rm(outDir, { recursive: true, force: true });
