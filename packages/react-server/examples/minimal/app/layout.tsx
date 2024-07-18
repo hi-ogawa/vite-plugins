8de18724ef34f5b1f7c17e63a98800ea326f16c3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-19 08:38:41 +0900

    wip: fix head inline script

--- packages/react-server/examples/minimal/app/layout.tsx
+++ packages/react-server/examples/minimal/app/layout.tsx
@@ -3,6 +3,9 @@ import type React from "react";
 export default function Layout(props: React.PropsWithChildren) {
   return (
     <html>
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `/* boom! */` }}></script>
+      </head>
       <body>
         <div>[Layout]</div>
         {props.children}
