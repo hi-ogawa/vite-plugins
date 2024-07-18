c083bf42209b7df380ecdbe5f952d2f55b08eeef
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-19 08:50:18 +0900

    chore: workaround by SsrOnly head

--- packages/react-server/examples/minimal/app/layout.tsx
+++ packages/react-server/examples/minimal/app/layout.tsx
@@ -1,10 +1,14 @@
 import type React from "react";
+import { SsrOnly } from "./_client";
 
 export default function Layout(props: React.PropsWithChildren) {
   return (
     <html>
       <head>
-        <script dangerouslySetInnerHTML={{ __html: `/* boom! */` }}></script>
+        {/* it only needs to render during ssr. also browser cannot hydrate head script properly.  */}
+        <SsrOnly>
+          <script>console.log("ssr script!")</script>
+        </SsrOnly>
       </head>
       <body>
         <div>[Layout]</div>
