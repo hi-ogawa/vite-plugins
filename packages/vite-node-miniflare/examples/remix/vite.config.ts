3c13cffe81dae46669dc37dcfd4b4adb5906ef87
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-01 12:13:22 +0900

    wip: use serviceBindings to implement __remixGetCriticalCss

--- packages/vite-node-miniflare/examples/remix/vite.config.ts
+++ packages/vite-node-miniflare/examples/remix/vite.config.ts
@@ -1,37 +1,29 @@
 import { vitePluginWorkerd } from "@hiogawa/vite-node-miniflare";
 import { vitePlugin as remix } from "@remix-run/dev";
-import { Log } from "miniflare";
+import { Log, Response as MiniflareResponse } from "miniflare";
 import { defineConfig } from "vite";
 import { vitePluginVirtualIndexHtml } from "../basic/vite.config";
 
 export default defineConfig({
   clearScreen: false,
   plugins: [
-    // vitePluginViteNodeMiniflare({
-    //   debug: true,
-    //   entry: "/app/worker-entry.ts",
-    //   miniflareOptions(options) {
-    //     options.log = new Log();
-    //     options.kvNamespaces = { kv: "0".repeat(32) };
-    //     options.kvPersist = ".wrangler/state/v3/kv";
-    //   },
-    //   customRpc: {
-    //     // DevServerHook is implemented via custom rpc
-    //     // https://github.com/remix-run/remix/blob/db4471d2e32a175abdcb907b877f9a510c735d8b/packages/remix-server-runtime/dev.ts#L37-L48
-    //     __remixGetCriticalCss: (...args: any[]) => {
-    //       return (globalThis as any)["__remix_devServerHooks"].getCriticalCss(
-    //         ...args,
-    //       );
-    //     },
-    //   },
-    // }),
-
-    // TODO: need to avoid spawning this for remix child compiler
+    // TODO: how to avoid spawning this for remix child compiler
     vitePluginWorkerd({
       entry: "/app/worker-entry",
       miniflare: {
         log: new Log(),
         kvPersist: true,
+        serviceBindings: {
+          // DevServerHook is implemented via serviceBindings based rpc
+          // https://github.com/remix-run/remix/blob/db4471d2e32a175abdcb907b877f9a510c735d8b/packages/remix-server-runtime/dev.ts#L37-L48
+          __remixGetCriticalCss: async (request) => {
+            const args: any = await request.json();
+            const result = await (globalThis as any)[
+              "__remix_devServerHooks"
+            ].getCriticalCss(...args);
+            return new MiniflareResponse(JSON.stringify(result));
+          },
+        },
       },
       wrangler: {
         configPath: "./wrangler.toml",
