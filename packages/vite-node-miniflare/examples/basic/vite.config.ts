e420181830733d08f5dffb65bd3481e9f0d43991
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-01 11:03:47 +0900

    wip: examples/basic

--- packages/vite-node-miniflare/examples/basic/vite.config.ts
+++ packages/vite-node-miniflare/examples/basic/vite.config.ts
@@ -1,27 +1,70 @@
+import fs from "node:fs";
 import { tinyReactVitePlugin } from "@hiogawa/tiny-react/dist/plugins/vite";
-import { vitePluginViteNodeMiniflare } from "@hiogawa/vite-node-miniflare";
+// import { vitePluginViteNodeMiniflare } from "@hiogawa/vite-node-miniflare";
+import { vitePluginWorkerd } from "@hiogawa/vite-node-miniflare";
 import { vitePluginSimpleHmr } from "@hiogawa/vite-plugin-simple-hmr";
 import { Log } from "miniflare";
-import { defineConfig } from "vite";
+import { type Plugin, type ViteDevServer, defineConfig } from "vite";
 
 export default defineConfig({
   clearScreen: false,
   appType: "custom",
-  ssr: {
-    noExternal: true,
-  },
+  // ssr: {
+  //   noExternal: true,
+  // },
   plugins: [
     vitePluginSimpleHmr({
       include: new URL("./src/**/*.tsx", import.meta.url).pathname,
     }),
-    vitePluginViteNodeMiniflare({
-      debug: true,
-      hmr: true,
-      entry: "/src/worker-entry.ts",
-      miniflareOptions(options) {
-        options.log = new Log();
+    vitePluginWorkerd({
+      entry: "/src/worker-entry",
+      miniflare: {
+        log: new Log(),
       },
     }),
+    // vitePluginViteNodeMiniflare({
+    //   debug: true,
+    //   hmr: true,
+    //   entry: "/src/worker-entry.ts",
+    //   miniflareOptions(options) {
+    //     options.log = new Log();
+    //   },
+    // }),
     tinyReactVitePlugin(),
+    vitePluginVirtualIndexHtml(),
   ],
+  environments: {
+    workerd: {
+      resolve: {
+        noExternal: true,
+      },
+    },
+  },
 });
+
+export function vitePluginVirtualIndexHtml(): Plugin {
+  let server: ViteDevServer | undefined;
+  return {
+    name: vitePluginVirtualIndexHtml.name,
+    configureServer(server_) {
+      server = server_;
+    },
+    resolveId(source, _importer, _options) {
+      return source === "virtual:index-html" ? "\0" + source : undefined;
+    },
+    async load(id, _options) {
+      if (id === "\0" + "virtual:index-html") {
+        let html: string;
+        if (server) {
+          this.addWatchFile("index.html");
+          html = await fs.promises.readFile("index.html", "utf-8");
+          html = await server.transformIndexHtml("/", html);
+        } else {
+          html = await fs.promises.readFile("dist/client/index.html", "utf-8");
+        }
+        return `export default ${JSON.stringify(html)}`;
+      }
+      return;
+    },
+  };
+}
