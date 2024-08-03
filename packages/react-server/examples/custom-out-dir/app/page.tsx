99acc2ab06b4c0cb722742043f5fff5982d2d91c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-03 12:28:19 +0900

    test: test import.meta.url

--- packages/react-server/examples/custom-out-dir/app/page.tsx
+++ packages/react-server/examples/custom-out-dir/app/page.tsx
@@ -2,6 +2,9 @@ export default function Page() {
   return (
     <div>
       <h1>Hello from custom out dir!</h1>
+      <pre data-testid="import-meta-url">
+        [import.meta.url] {import.meta.url}
+      </pre>
     </div>
   );
 }
--- packages/react-server/examples/custom-out-dir/e2e/basic.test.ts
+++ packages/react-server/examples/custom-out-dir/e2e/basic.test.ts
@@ -5,12 +5,17 @@ test("custom outDir app can be visited", async ({ page }) => {
   const res = await page.goto("/");
   expect(res?.status()).toBe(200);
   expect(await page.textContent("h1")).toBe("Hello from custom out dir!");
+
+  if (!process.env.E2E_CF) {
+    await expect(page.getByTestId("import-meta-url")).toContainText(
+      process.env.E2E_PREVIEW
+        ? "examples/custom-out-dir/custom-out-dir/server"
+        : "examples/custom-out-dir/app/page.tsx",
+    );
+  }
 });
 
 test("custom outDir is created @build", async () => {
   expect(fs.existsSync("custom-out-dir")).toBe(true);
-});
-
-test("default outDir is not created @build", async () => {
   expect(fs.existsSync("dist")).toBe(false);
 });
