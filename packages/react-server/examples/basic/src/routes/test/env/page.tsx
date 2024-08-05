3e6a9110b34b741ddd52d2b6dd7b3d55ef91be1e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-05 15:00:17 +0900

    chore: tweak style

--- packages/react-server/examples/basic/src/routes/test/env/page.tsx
+++ packages/react-server/examples/basic/src/routes/test/env/page.tsx
@@ -3,16 +3,17 @@ import { getTestEnv } from "./_test";
 
 export default function Page() {
   return (
-    <div className="text-sm">
+    <div className="flex flex-col gap-2 p-2">
+      <h2 className="font-bold">Test Env</h2>
       <div>
         <h3>Server component</h3>
-        <pre data-testid="server-env">
+        <pre className="text-sm" data-testid="server-env">
           {JSON.stringify(getTestEnv(), null, 2)}
         </pre>
       </div>
       <div>
         <h3>Client component</h3>
-        <pre data-testid="client-env">
+        <pre className="text-sm" data-testid="client-env">
           <TestEnvClient />
         </pre>
       </div>
