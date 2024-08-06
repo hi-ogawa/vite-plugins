3f0aebc145b53a64f94c60fad77281aa40e21a94
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-06 15:06:26 +0900

    chore: cleanup

--- packages/pre-bundle-new-url/examples/basic/src/main.js
+++ packages/pre-bundle-new-url/examples/basic/src/main.js
@@ -2,33 +2,25 @@ import * as testDepImage from "test-dep-image";
 import * as testDepWorker from "test-dep-worker";
 
 async function main() {
-  document.body.appendChild(
-    Object.assign(document.createElement("div"), {
-      innerHTML: `
-        <pre>new URL("./vite.svg", import.meta.url)</pre>
-        <img width="40" src="${testDepImage.image1}" />
-      `,
-    }),
-  );
-  document.body.appendChild(
-    Object.assign(document.createElement("div"), {
-      innerHTML: `
-        <pre>new URL("vite.svg", import.meta.url)</pre>
-        <img width="40" src="${testDepImage.image2}" />
-      `,
-    }),
-  );
+  render(`
+    <pre>new URL("./vite.svg", import.meta.url)</pre>
+    <img width="40" src="${testDepImage.image1}" />
+  `);
 
-  // worker
+  render(`
+    <pre>new URL("vite.svg", import.meta.url)</pre>
+    <img width="40" src="${testDepImage.image2}" />
+  `);
+
   testDepWorker.startWorker((e) => {
-    document.body.appendChild(
-      Object.assign(document.createElement("div"), {
-        innerHTML: `
-          <pre>worker = ${e.data}</pre>
-        `,
-      }),
-    );
+    render(`<pre>worker = ${e.data}</pre>`);
   });
+}
+
+function render(innerHTML) {
+  const el = document.createElement("div");
+  el.innerHTML = innerHTML;
+  document.body.appendChild(el);
 }
 
 main();
