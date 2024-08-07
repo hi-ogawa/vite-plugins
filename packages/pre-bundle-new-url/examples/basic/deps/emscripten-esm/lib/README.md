4b7f1db1012646c8d11e011091ca170850eb6a3a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-07 17:36:54 +0900

    chore: test -sMODULARIZE build

--- packages/pre-bundle-new-url/examples/basic/deps/emscripten-esm/lib/README.md
+++ packages/pre-bundle-new-url/examples/basic/deps/emscripten-esm/lib/README.md
@@ -3,7 +3,7 @@ based on https://github.com/hi-ogawa/reproductions/tree/main/vitest-5704-emscrip
 ```sh
 emcc --version # 3.1.59
 emcc lib.cpp -o build-esm.js -sEXPORT_ES6 -sENVIRONMENT=web,worker --bind
-emcc lib.cpp -o build-modularize.js -sMODULARIZE -sENVIRONMENT=web,worker --bind
+emcc lib.cpp -o build-modularize.cjs -sMODULARIZE -sENVIRONMENT=web,worker --bind
 ```
 
 ## references
--- packages/pre-bundle-new-url/examples/basic/deps/emscripten-esm/worker.js
+++ packages/pre-bundle-new-url/examples/basic/deps/emscripten-esm/worker.js
@@ -1,8 +1,18 @@
-import Module from "./lib/build-esm.js";
+import ModuleEsm from "./lib/build-esm.js";
+import ModuleModularize from "./lib/build-modularize.cjs";
 
-let lib;
+let modEsm;
+let modModularize;
 
 self.onmessage = async () => {
-  lib ??= await Module();
-  self.postMessage(lib.hello("world"));
+  modEsm ??= await ModuleEsm();
+  modModularize ??= await ModuleModularize({
+    locateFile: () =>
+      new URL("./lib/build-modularize.wasm", import.meta.url).href,
+  });
+  const data = {
+    esm: modEsm.hello("EXPORT_ES6"),
+    modularize: modModularize.hello("MODULARIZE"),
+  };
+  self.postMessage(JSON.stringify(data, null, 2));
 };
