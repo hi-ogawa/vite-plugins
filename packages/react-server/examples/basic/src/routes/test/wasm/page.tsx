9239d73fad79af0f9f98de80363903c1fa854225
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-11 17:38:34 +0900

    wip: wasmModulePlugin

--- packages/react-server/examples/basic/src/routes/test/wasm/page.tsx
+++ packages/react-server/examples/basic/src/routes/test/wasm/page.tsx
@@ -1,10 +1,22 @@
-import { codeToHtml } from "shiki";
+import { once } from "@hiogawa/utils";
+import { createHighlighterCore } from "shiki/core";
+import js from "shiki/langs/javascript.mjs";
+import nord from "shiki/themes/nord.mjs";
+
+const getHighlither = once(async () => {
+  return createHighlighterCore({
+    themes: [nord],
+    langs: [js],
+    loadWasm: import("shiki/onig.wasm" as string),
+  });
+});
 
 export default async function Page() {
+  const highligher = await getHighlither();
   const code = "const a = 1";
-  const html = await codeToHtml(code, {
-    lang: "javascript",
-    theme: "vitesse-dark",
+  const html = highligher.codeToHtml(code, {
+    lang: "js",
+    theme: "nord",
   });
   return (
     <div>
