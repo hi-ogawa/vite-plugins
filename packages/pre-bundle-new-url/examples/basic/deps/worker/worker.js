cf7ec03a1e53dfda02300a6cc9fc79f9b86be5a1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-14 14:35:18 +0900

    chore: tweak demo

--- packages/pre-bundle-new-url/examples/basic/deps/worker/worker.js
+++ packages/pre-bundle-new-url/examples/basic/deps/worker/worker.js
@@ -3,9 +3,9 @@ import { workerDep } from "./worker-dep.js";
 self.onmessage = async () => {
   const { workerDepDynamic } = await import("./worker-dep-dynamic.js");
   const data = {
-    "self.location.href": self.location.href,
+    href: self.location.href,
     workerDep,
     workerDepDynamic,
   };
-  self.postMessage(JSON.stringify(data, null, 2));
+  self.postMessage(data);
 };
