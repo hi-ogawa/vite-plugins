73e0bcde1baab9c04f14731cffbd4a270243ffaa
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-19 14:19:15 +0900

    wip: support mode: "fs"

--- packages/react-server/examples/og/vite.config.ts
+++ packages/react-server/examples/og/vite.config.ts
@@ -20,19 +20,35 @@ export default defineConfig({
             };
           },
         },
-        nextEdgeWasmPlugin(),
-        nextEdgeAssetPlugin(),
+        wasmModulePlugin({ mode: process.env.CF_PAGES ? "import" : "fs" }),
+        fetchImportMetaUrlPlugin({
+          mode: process.env.CF_PAGES ? "import" : "fs",
+        }),
       ],
     }),
   ],
 });
 
 // cf. packages/react-server/examples/basic/vite-plugin-wasm-module.ts
-function nextEdgeWasmPlugin(): Plugin {
+//
+// input
+//   import wasm from "xxx.wasm?module"
+//
+// output (fs / dev)
+//   ??
+//
+// output (fs / build)
+//   ??
+//
+// output (import / build)
+//   ??
+//
+function wasmModulePlugin(options: { mode: "fs" | "import" }): Plugin {
   const MARKER = "\0:virtual:wasm-module";
   let env: ConfigEnv;
+
   return {
-    name: nextEdgeWasmPlugin.name,
+    name: wasmModulePlugin.name,
     config(_, env_) {
       env = env_;
     },
@@ -56,28 +72,43 @@ function nextEdgeWasmPlugin(): Plugin {
         const file = id.slice(MARKER.length);
 
         // readFile + instantiate wasm module
-        if (env.command === "serve") {
+        if (options.mode === "fs") {
+          let source: string;
+          if (env.command === "serve") {
+            source = JSON.stringify(file);
+          } else {
+            const referenceId = this.emitFile({
+              type: "asset",
+              name: path.basename(file),
+              source: fs.readFileSync(file),
+            });
+            source = `fileURLToPath(import.meta.ROLLUP_FILE_URL_${referenceId})`;
+          }
           return `
             import fs from "node:fs";
-            const buffer = fs.readFileSync(${JSON.stringify(file)});
+            import { fileURLToPath } from "node:url";
+            const buffer = fs.readFileSync(${source});
             export default new WebAssembly.Module(buffer);
           `;
         }
 
         // emit wasm asset + rewrite import
-        if (env.command === "build") {
+        if (options.mode === "import") {
+          if (env.command === "serve") {
+            throw new Error("unsupported");
+          }
           const referenceId = this.emitFile({
             type: "asset",
             name: path.basename(file),
             source: fs.readFileSync(file),
           });
           // temporary placeholder which we replace during `renderChunk`
-          return `export default "__WASM_IMPORT_URL_${referenceId}"`;
+          return `export default "__WASM_MODULE_IMPORT_${referenceId}"`;
         }
       }
     },
     renderChunk(code, chunk) {
-      const matches = code.matchAll(/"__WASM_IMPORT_URL_(\w+)"/dg);
+      const matches = code.matchAll(/"__WASM_MODULE_IMPORT_(\w+)"/dg);
       const output = new MagicString(code);
       for (const match of matches) {
         const referenceId = match[1];
@@ -102,22 +133,26 @@ function nextEdgeWasmPlugin(): Plugin {
 
 //
 // input
-//   fetch(new URL("./some-asset.bin", import.meta.url))
+//   fetch(new URL("./xxx", import.meta.url))
 //
-// output (dev)
-//   import("node:fs").then(fs => new Response(fs.readFileSync(...)))
+// output (fs / dev)
+//   new Response(fs.readFileSync("/absolute-path-to/xxx")
 //
-// output (build)
-//   import("./relocated-asset.bin").then(mod => new Response(mod.default))
+// output (fs / build)
+//   import("node:fs").then(fs => new Response(fs.readFileSync(new URL("./relocated-xxx", import.meta.url).href)))
 //
-function nextEdgeAssetPlugin(): Plugin {
+// output (import / build)
+//   import("./relocated-xxx.bin").then(mod => new Response(mod.default))
+//
+function fetchImportMetaUrlPlugin(options: { mode: "fs" | "import" }): Plugin {
   // cf. https://github.com/vitejs/vite/blob/0f56e1724162df76fffd5508148db118767ebe32/packages/vite/src/node/plugins/assetImportMetaUrl.ts#L51-L52
   const FETCH_ASSET_RE =
     /\bfetch\s*\(new\s+URL\s*\(\s*('[^']+'|"[^"]+"|`[^`]+`)\s*,\s*import\.meta\.url\s*(?:,\s*)?\)\s*(?:,\s*)?\)/dg;
 
   let env: ConfigEnv;
+
   return {
-    name: nextEdgeAssetPlugin.name,
+    name: fetchImportMetaUrlPlugin.name,
     config(_, env_) {
       env = env_;
     },
@@ -129,10 +164,30 @@ function nextEdgeAssetPlugin(): Plugin {
           const urlArg = match[1]!.slice(1, -1);
           const absFile = path.resolve(id, "..", urlArg);
           if (fs.existsSync(absFile)) {
-            let replacement: string;
-            if (env.command === "serve") {
-              replacement = `import("node:fs").then(fs => new Response(fs.readFileSync(${JSON.stringify(absFile)})))`;
-            } else {
+            let replacement!: string;
+            if (options.mode === "fs") {
+              if (env.command === "serve") {
+                replacement = `((async () => {
+                  const fs = await import("node:fs");
+                  return new Response(fs.readFileSync(${JSON.stringify(absFile)}));
+                })())`;
+              } else {
+                const referenceId = this.emitFile({
+                  type: "asset",
+                  name: path.basename(absFile),
+                  source: fs.readFileSync(absFile),
+                });
+                replacement = `((async () => {
+                  const fs = await import("node:fs");
+                  const { fileURLToPath } = await import("node:url");
+                  return new Response(fs.readFileSync(fileURLToPath(import.meta.ROLLUP_FILE_URL_${referenceId})));
+                })())`;
+              }
+            }
+            if (options.mode === "import") {
+              if (env.command === "serve") {
+                throw new Error("unsupported");
+              }
               const referenceId = this.emitFile({
                 type: "asset",
                 name: path.basename(absFile) + ".bin",
