5265ff979a550b83230557dacea421f5d5dfef02
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-29 12:44:47 +0900

    chore: squashed all

--- packages/vite-node-miniflare/examples/remix/vite.config.ts
+++ packages/vite-node-miniflare/examples/remix/vite.config.ts
@@ -1,33 +1,19 @@
-import { vitePluginViteNodeMiniflare } from "@hiogawa/vite-node-miniflare";
+import { vitePluginWorkerd } from "@hiogawa/vite-node-miniflare";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { Log, Response as MiniflareResponse } from "miniflare";
 import { defineConfig } from "vite";
+import { vitePluginVirtualIndexHtml } from "../basic/vite.config";
 
 export default defineConfig({
   clearScreen: false,
-  ssr: {
-    resolve: {
-      conditions: ["workerd"],
-    },
-    optimizeDeps: {
-      include: [
-        "react",
-        "react/jsx-dev-runtime",
-        "react-dom",
-        "react-dom/server",
-        "@remix-run/server-runtime",
-      ],
-    },
-  },
   plugins: [
-    vitePluginViteNodeMiniflare({
-      debug: true,
-      entry: "/app/worker-entry.ts",
-      miniflareOptions(options) {
-        options.log = new Log();
-        options.kvNamespaces = { kv: "0".repeat(32) };
-        options.kvPersist = ".wrangler/state/v3/kv";
-        options.serviceBindings = {
+    // TODO: how to avoid spawning this for remix child compiler
+    vitePluginWorkerd({
+      entry: "/app/worker-entry",
+      miniflare: {
+        log: new Log(),
+        kvPersist: true,
+        serviceBindings: {
           // DevServerHook is implemented via serviceBindings based rpc
           // https://github.com/remix-run/remix/blob/db4471d2e32a175abdcb907b877f9a510c735d8b/packages/remix-server-runtime/dev.ts#L37-L48
           __remixGetCriticalCss: async (request) => {
@@ -37,9 +23,33 @@ export default defineConfig({
             ].getCriticalCss(...args);
             return new MiniflareResponse(JSON.stringify({ result }));
           },
-        };
+        },
+      },
+      wrangler: {
+        configPath: "./wrangler.toml",
       },
     }),
+    vitePluginVirtualIndexHtml(),
     remix(),
   ],
+  environments: {
+    workerd: {
+      webCompatible: true,
+      resolve: {
+        noExternal: true,
+        conditions: ["workerd"],
+      },
+      dev: {
+        optimizeDeps: {
+          include: [
+            "react",
+            "react/jsx-dev-runtime",
+            "react-dom",
+            "react-dom/server",
+            "@remix-run/server-runtime",
+          ],
+        },
+      },
+    },
+  },
 });
