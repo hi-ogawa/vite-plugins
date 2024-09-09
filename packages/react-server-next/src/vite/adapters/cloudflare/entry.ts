3bf64aebf57ce18d981ab8da28f21572e26a94cd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-09-09 17:06:43 +0900

    chore: comment

--- packages/react-server-next/src/vite/adapters/cloudflare/entry.ts
+++ packages/react-server-next/src/vite/adapters/cloudflare/entry.ts
@@ -1,8 +1,8 @@
 import { handler } from "@hiogawa/react-server/entry/ssr";
 
 export default {
-  fetch(request: Request, env: any) {
-    // copy cf binding variable to process.env
+  fetch(request: Request, env: any, _context: any) {
+    // expose process.env
     for (const [k, v] of Object.entries(env)) {
       if (typeof v === "string") {
         process.env[k] = v;
