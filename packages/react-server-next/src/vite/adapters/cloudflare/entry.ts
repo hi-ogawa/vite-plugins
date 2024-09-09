4c63f6fb7b148aa60a00908800af635773d1c4a8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-09-09 17:53:39 +0900

    chore: cloudflare `nodejs_compat_v2` example (#639)

--- packages/react-server-next/src/vite/adapters/cloudflare/entry.ts
+++ packages/react-server-next/src/vite/adapters/cloudflare/entry.ts
@@ -1,3 +1,13 @@
 import { handler } from "@hiogawa/react-server/entry/ssr";
 
-export default { fetch: handler };
+export default {
+  fetch(request: Request, env: any, _context: any) {
+    // expose process.env
+    for (const [k, v] of Object.entries(env)) {
+      if (typeof v === "string") {
+        process.env[k] = v;
+      }
+    }
+    return handler(request);
+  },
+};
