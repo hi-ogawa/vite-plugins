2d809b4cb6ab0e90255bbdff3b6b28c64c1fcf8f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-02 14:59:38 +0900

    chore: workaround wrangler dev streaming issue

--- packages/react-server/examples/basic/src/adapters/cf.ts
+++ packages/react-server/examples/basic/src/adapters/cf.ts
@@ -1,5 +1,18 @@
 import { handler } from "@hiogawa/react-server/entry/ssr";
 
 export default {
-  fetch: handler,
+  async fetch(request: Request, env: any) {
+    const response = await handler(request);
+    // avoid compressoin on wrangler dev to enable streaming
+    // https://github.com/cloudflare/workers-sdk/issues/6577
+    if (env.LOCAL_PREVIEW) {
+      let headers = response.headers;
+      if (!headers.has("content-encoding")) {
+        headers = new Headers(headers);
+        headers.set("content-encoding", "identity");
+        return new Response(response.body, { ...response, headers });
+      }
+    }
+    return response;
+  },
 };
