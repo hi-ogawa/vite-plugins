16f970d13230799a54884bda171821fe7d462b15
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-05 16:03:19 +0900

    chore: typo

--- packages/react-server/examples/basic/src/adapters/cf.ts
+++ packages/react-server/examples/basic/src/adapters/cf.ts
@@ -3,7 +3,7 @@ import { handler } from "@hiogawa/react-server/entry/ssr";
 export default {
   async fetch(request: Request, env: any) {
     const response = await handler(request);
-    // avoid compressoin on wrangler dev to enable streaming
+    // avoid compression on wrangler dev to enable streaming
     // https://github.com/cloudflare/workers-sdk/issues/6577
     if (env.LOCAL_PREVIEW) {
       let headers = response.headers;
