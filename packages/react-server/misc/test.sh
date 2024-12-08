9df69bc677673abc4a7ebda463008db721b1f76f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-12-08 10:24:37 +0900

    test: fix test.sh

--- packages/react-server/misc/test.sh
+++ packages/react-server/misc/test.sh
@@ -14,7 +14,6 @@ cd "$test_dir"
 rm -rf dist node_modules
 
 node "$lib_dir/misc/overrides.mjs" "$test_dir/package.json"
-pnpm i {react,react-dom,react-server-dom-webpack}@19.0.0-rc-2d16326d-20240930
 
 if test "${CI:-}" = "true"; then
   npx playwright install chromium
