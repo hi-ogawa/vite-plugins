79eaa5460b23dac244be2d6f97a98517c076dbb9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-12-15 17:16:54 +0900

    chore: improve test-dep-cjs

--- packages/react-server/examples/basic/deps/cjs/client.js
+++ packages/react-server/examples/basic/deps/cjs/client.js
@@ -4,5 +4,13 @@ import React from "react";
 import dep from "./client-dep.cjs";
 
 export function TestClient() {
-  return React.createElement("span", null, dep.test);
+  return React.createElement("span", null, useHydrated() ? dep.test : "[...]");
+}
+
+function useHydrated() {
+	return React.useSyncExternalStore(
+		React.useCallback(() => () => {}, []),
+		() => true,
+		() => false,
+	);
 }
