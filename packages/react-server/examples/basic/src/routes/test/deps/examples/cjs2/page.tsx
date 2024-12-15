f965fe39c60b3929a1c1f5abe02e2c7ccfe095cc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-12-15 15:54:35 +0900

    chore: lint

--- packages/react-server/examples/basic/src/routes/test/deps/examples/cjs2/page.tsx
+++ packages/react-server/examples/basic/src/routes/test/deps/examples/cjs2/page.tsx
@@ -2,7 +2,9 @@
 import { TestServer } from "@hiogawa/test-dep-cjs/server2";
 
 export default function Page() {
-  return <div>
-    <TestServer />
-    </div>;
+  return (
+    <div>
+      <TestServer />
+    </div>
+  );
 }
