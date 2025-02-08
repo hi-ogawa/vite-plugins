911b9c5f0356a9750c2f3fa3ce9ae543dfc147ab
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-02-08 18:47:16 +0900

    fix: rename `virtual:dev-ssr-css`

--- packages/react-server/src/features/assets/shared.ts
+++ packages/react-server/src/features/assets/shared.ts
@@ -1,2 +1,4 @@
 export const SERVER_CSS_PROXY = "virtual:server-css-proxy.js";
-export const DEV_SSR_CSS = "virtual:dev-ssr-css.css";
+// avoid .css prefix to exclude the virtual from tailwind v4's waitForRequestsIdle
+// https://github.com/tailwindlabs/tailwindcss/blob/d684733d804a0b8951d13c94fe27350271e076b6/packages/%40tailwindcss-vite/src/index.ts#L313-L322
+export const DEV_SSR_CSS = "virtual:dev-ssr-css";
