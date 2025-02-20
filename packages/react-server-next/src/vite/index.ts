a791c2a456589e59d0a3b978e9b9d5373a87db35
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-02-20 12:16:33 +0900

    chore: revert

--- packages/react-server-next/src/vite/index.ts
+++ packages/react-server-next/src/vite/index.ts
@@ -145,6 +145,13 @@ function nextConfigPlugin(): Plugin {
 function nextJsxPlugin(): Plugin {
   return {
     name: nextJsxPlugin.name,
+    // need to override next.js's default tsconfig `jsx: preserve`
+    config: () => ({
+      esbuild: { jsx: "automatic" },
+      optimizeDeps: {
+        esbuildOptions: { jsx: "automatic", loader: { ".js": "jsx" } },
+      },
+    }),
     // manually use esbuild transform to support jsx in js
     // TODO: try using vite-plugin-react-swc and parserConfig to support HMR
     // https://github.com/vitejs/vite-plugin-react-swc?tab=readme-ov-file#parserconfig
