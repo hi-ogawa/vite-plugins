d173d762f0f805fbbe292376a23eba9c1b8cb1c9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-02-26 14:59:42 +0900

    fix: bundle inlined deps types

--- packages/react-server/tsup.config.ts
+++ packages/react-server/tsup.config.ts
@@ -1,3 +1,5 @@
+import fs from "node:fs";
+import path from "node:path";
 import { defineConfig } from "tsup";
 import { IsolatedDecl } from "unplugin-isolated-decl";
 
@@ -21,7 +23,23 @@ export default defineConfig([
       IsolatedDecl.esbuild({
         transformer: "oxc",
         sourceMap: true,
+        rewriteImports(id, importer) {
+          return rewriteTypeDeps(id, importer);
+        },
       }),
+      {
+        name: "inline-dep-types",
+        setup(build) {
+          build.onEnd(() => {
+            for (const id in typeDeps) {
+              const src = toTypeDepSrc(id);
+              const dest = toTypeDepDest(id);
+              fs.mkdirSync(path.dirname(dest), { recursive: true });
+              fs.copyFileSync(src, dest);
+            }
+          });
+        },
+      },
     ],
     esbuildOptions(options) {
       // TODO: workaround for .d.ts.map generation. currently both side don't support it unless `write: true`.
@@ -32,3 +50,30 @@ export default defineConfig([
     external: [/^virtual:/, /^@hiogawa\/react-server\//, /^\/dist\//],
   },
 ]) as any;
+
+const typeDeps: Record<string, string> = {
+  "@edge-runtime/cookies": "dist/index.d.ts",
+  "@tanstack/history": "dist/esm/index.d.ts",
+};
+
+function toTypeDepSrc(id: string) {
+  return path.join("node_modules", id, typeDeps[id]!);
+}
+
+function toTypeDepDest(id: string) {
+  return path.join("dist/types-deps", id, typeDeps[id]!);
+}
+
+function rewriteTypeDeps(id: string, importer: string): string | undefined {
+  if (id in typeDeps) {
+    // TODO: this assumes "src/..." and "dist/..." are at the same level
+    return path.relative(path.dirname(importer), stripExt(toTypeDepDest(id)));
+  }
+  return;
+}
+
+// avoid a following error when importing .d.ts
+// > A declaration file cannot be imported without 'import type'
+function stripExt(filename: string): string {
+  return filename.replace(/\.(.?)[jt]sx?$/, "");
+}
