bb1c6141fb6671c515597f39b67e3e28f537814b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-03-29 15:06:35 +0900

    chore: todo

--- packages/rsc/src/lib/plugin.ts
+++ packages/rsc/src/lib/plugin.ts
@@ -58,6 +58,7 @@ export default function vitePluginRsc(rscOptions: {
               },
             },
             rsc: {
+              // TODO: vitefu crawlFrameworkPkgs
               optimizeDeps: {
                 include: [
                   "react",
