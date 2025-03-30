50b6356c307b8e917291466e3e6ec6137aae179b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-03-30 17:32:29 +0900

    chore: spa on client

--- packages/rsc/examples/hono/src/client.tsx
+++ packages/rsc/examples/hono/src/client.tsx
@@ -1,3 +1,39 @@
-import { hydrate } from "@hiogawa/vite-rsc/browser";
+import { fetchRSC } from "@hiogawa/vite-rsc/browser";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-hydrate();
+function main() {
+  const dom = document.getElementById("root")!;
+  ReactDOM.createRoot(dom).render(<App />);
+}
+
+function App() {
+  return (
+    <div>
+      <h4>hello client</h4>
+      <FetchRsc />
+    </div>
+  );
+}
+
+function FetchRsc() {
+  const [rsc, setRsc] = React.useState<React.ReactNode>(null);
+
+  return (
+    <div>
+      <button
+        onClick={async () => {
+          // TODO: how to hide `?__rsc`
+          // TODO: hide .root
+          const result = await fetchRSC("/api/rsc?__rsc");
+          setRsc(result.root);
+        }}
+      >
+        fetchRsc
+      </button>
+      {rsc}
+    </div>
+  );
+}
+
+main();
--- packages/rsc/examples/hono/src/server.tsx
+++ packages/rsc/examples/hono/src/server.tsx
@@ -1,7 +1,24 @@
 import { renderRequest } from "@hiogawa/vite-rsc/server";
 import { Hono } from "hono";
-import { changeServerCounter, serverCounter } from "./action";
-import { ClientCounter, FetchRsc } from "./counter";
+import React from "react";
+
+const app = new Hono();
+
+app.get("/api/rsc", (c) => {
+  const el = (
+    <div>
+      <div>Hono!</div>
+      <div>random: ${Math.random().toString(36).slice(2)}</div>
+      <React.Suspense></React.Suspense>
+    </div>
+  );
+  // TODO: renderRsc to just stream rsc
+  return renderRequest(c.req.raw, el);
+});
+
+app.all("/", (c) => {
+  return renderRequest(c.req.raw, <Document />);
+});
 
 function Document() {
   return (
@@ -10,35 +27,10 @@ function Document() {
         <title>vite-rsc</title>
       </head>
       <body>
-        <h4>hello server</h4>
-        <form action={changeServerCounter}>
-          <input type="hidden" name="change" value="1" />
-          <button>Server Counter: {serverCounter}</button>
-        </form>
-        <ClientCounter />
-        <FetchRsc />
+        <div id="root"></div>
       </body>
     </html>
   );
 }
-
-const app = new Hono();
-
-app.get("/api/hono", (c) => c.text("Hono!"));
-
-app.get("/api/rsc", (c) => {
-  const el = (
-    <div>
-      <div>Hono!</div>
-      <div>random: ${Math.random().toString(36).slice(2)}</div>
-    </div>
-  );
-  return renderRequest(c.req.raw, el);
-});
-
-app.all("/", (c) => {
-  const el = <Document />;
-  return renderRequest(c.req.raw, el);
-});
 
 export default app.fetch;
