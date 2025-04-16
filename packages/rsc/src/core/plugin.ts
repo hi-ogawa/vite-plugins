3cfbbdd9fbb59b655d360fc0f5e04fc71dde5133
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-16 09:18:56 +0900

    refactor: remove react-server-dom-webpack patch

--- packages/rsc/src/core/plugin.ts
+++ packages/rsc/src/core/plugin.ts
@@ -66,16 +66,6 @@ export function vitePluginRscCore(rscOptions: {
           );
           return { code, map: null };
         }
-        if (
-          this.environment?.name === "client" &&
-          id.includes("react-server-dom-webpack") &&
-          code.includes("__webpack_require__")
-        ) {
-          // avoid accessing `__webpack_require__` on import side effect
-          // https://github.com/facebook/react/blob/a9bbe34622885ef5667d33236d580fe7321c0d8b/packages/react-server-dom-webpack/src/client/ReactFlightClientConfigBundlerWebpackBrowser.js#L16-L17
-          code = code.replaceAll("__webpack_require__.u", "({}).u");
-          return { code, map: null };
-        }
         return;
       },
     },
--- packages/rsc/src/core/shared.ts
+++ packages/rsc/src/core/shared.ts
@@ -1,3 +1,10 @@
 export function getBrowserPreamble(): string {
-  return `self.__viteRscImport = (id) => import(id);`;
+  let code = ``;
+  // avoid throwing when accessing `__webpack_require__` on import side effect
+  // (note that __webpack_require__ is later properly defined by packages/rsc/src/core/client-browser.ts)
+  // https://github.com/facebook/react/blob/a9bbe34622885ef5667d33236d580fe7321c0d8b/packages/react-server-dom-webpack/src/client/ReactFlightClientConfigBundlerWebpackBrowser.js#L16-L17
+  code += `self.__webpack_require__ = () => {};\n`;
+  // use __viteRscImport to avoid Vite adding `?import` query, which causes duplicate modules on browser.
+  code += `self.__viteRscImport = (id) => import(id);\n`;
+  return code;
 }
