915f54948d9d13492873a7cfc258b2227feb194c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-21 15:24:08 +0900

    chore: fix react-router example

--- packages/rsc/examples/react-router/src/entry.ssr.tsx
+++ packages/rsc/examples/react-router/src/entry.ssr.tsx
@@ -16,6 +16,9 @@ export default async function handler(
   callServer: (request: Request) => Promise<Response>,
 ) {
   const assets = await importAssets();
+  const css = assets.css.map((href) => (
+    <link key={href} rel="stylesheet" href={href} precedence="high" />
+  ));
 
   return routeRSCServerRequest(
     request,
@@ -26,7 +29,10 @@ export default async function handler(
       }),
     (payload) =>
       ReactDomServer.renderToReadableStream(
-        <RSCStaticRouter payload={payload} />,
+        <>
+          <RSCStaticRouter payload={payload} />
+          {css}
+        </>,
         {
           bootstrapModules: assets.js,
         },
