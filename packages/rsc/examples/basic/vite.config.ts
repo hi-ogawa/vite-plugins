554132ec5ca222d449e82435caeb6c044c58ba78
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-21 15:15:06 +0900

    feat(rsc): support tailwindcss

--- packages/rsc/examples/basic/vite.config.ts
+++ packages/rsc/examples/basic/vite.config.ts
@@ -1,10 +1,12 @@
 import rsc from "@hiogawa/vite-rsc/plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   clearScreen: false,
   plugins: [
+    tailwindcss(),
     react(),
     rsc({
       entries: {
@@ -13,7 +15,7 @@ export default defineConfig({
         ssr: "@hiogawa/vite-rsc/extra/ssr",
         css: "/src/styles.css",
       },
-    }),
+    }) as any,
   ],
   build: {
     minify: false,
