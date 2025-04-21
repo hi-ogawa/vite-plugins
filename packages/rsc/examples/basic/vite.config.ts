816e58506a949c8a9eb4333f33d591888d88fd17
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-21 15:33:35 +0900

    chore: update tw

--- packages/rsc/examples/basic/vite.config.ts
+++ packages/rsc/examples/basic/vite.config.ts
@@ -1,7 +1,7 @@
 import rsc from "@hiogawa/vite-rsc/plugin";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   clearScreen: false,
