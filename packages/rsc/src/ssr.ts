f83edf586c63181402a889382663b9caf26b776f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-24 08:31:48 +0900

    feat(rsc): always ssr modulepreload for build

--- packages/rsc/src/ssr.ts
+++ packages/rsc/src/ssr.ts
@@ -1,3 +1,5 @@
+import * as clientReferences from "virtual:vite-rsc/client-references";
+import * as ReactDOM from "react-dom";
 import { setRequireModule } from "./core/ssr";
 import type { ServerAssets } from "./types";
 
@@ -11,14 +13,27 @@ export function initialize(): void {
       if (import.meta.env.DEV) {
         return import(/* @vite-ignore */ id);
       } else {
-        const clientReferences = await import(
-          "virtual:vite-rsc/client-references" as any
-        );
         const import_ = clientReferences.default[id];
         if (!import_) {
           throw new Error(`client reference not found '${id}'`);
         }
         return import_();
+      }
+    },
+    prepareDestination(id) {
+      // we manually run `preloadModule` instead of react-server-dom-webpack's prepareDestination
+      // maybe we can have this logic baked in react-server-dom-vite.
+      // for unbundled dev, preventing waterfall is not practical so this is build only
+      // (need to traverse entire module graph and add entire import chains to modulepreload).
+      if (!import.meta.env.DEV) {
+        if (clientReferences.assetDeps) {
+          const deps = clientReferences.assetDeps[id];
+          if (deps) {
+            for (const js of deps.js) {
+              ReactDOM.preloadModule(js);
+            }
+          }
+        }
       }
     },
   });
