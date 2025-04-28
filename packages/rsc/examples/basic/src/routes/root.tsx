767ec9f08ff1b9efe704930ebc05875ffecc6108
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-28 10:10:46 +0900

    chore(rsc): move example code

---
+++ packages/rsc/examples/basic/src/routes/root.tsx
@@ -0,0 +1,50 @@
+import type React from "react";
+import {
+  changeServerCounter,
+  resetServerCounter,
+  serverCounter,
+} from "./action";
+import {
+  ClientCounter,
+  Hydrated,
+  TestStyleClient,
+  TestTailwindClient,
+  TestTemporaryReference,
+} from "./counter";
+
+export function Root() {
+  return (
+    <html>
+      <head>
+        <title>vite-rsc</title>
+      </head>
+      <body className="flex flex-col gap-2 items-start p-2">
+        <h4 className="text-xl">Test</h4>
+        <div>
+          <Hydrated />
+          <input data-testid="client-state" placeholder="client-state" />
+        </div>
+        <ClientCounter />
+        <form action={changeServerCounter}>
+          <input type="hidden" name="change" value="1" />
+          <button>Server Counter: {serverCounter}</button>
+          <button formAction={resetServerCounter}>Server Reset</button>
+        </form>
+        <TestStyleClient />
+        <div className="test-style-server">test-style-server</div>
+        <TestTailwindClient />
+        <div className="test-tw-server text-red-500">test-tw-server</div>
+        <TestTemporaryReference
+          action={async (node: React.ReactNode) => {
+            "use server";
+            return (
+              <span>
+                [server <span>{node}</span>]
+              </span>
+            );
+          }}
+        />
+      </body>
+    </html>
+  );
+}
