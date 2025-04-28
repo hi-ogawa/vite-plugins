568b441818ee299810778e454c1538d5ad6e45c6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-28 10:25:39 +0900

    chore(rsc): move example code (#778)

--- packages/rsc/examples/basic/src/server.tsx
+++ packages/rsc/examples/basic/src/server.tsx
@@ -1,56 +1,7 @@
 import { renderRequest } from "@hiogawa/vite-rsc/extra/rsc";
-import type React from "react";
-import {
-  changeServerCounter,
-  resetServerCounter,
-  serverCounter,
-} from "./action";
-import {
-  ClientCounter,
-  Hydrated,
-  TestStyleClient,
-  TestTailwindClient,
-  TestTemporaryReference,
-} from "./counter";
-
-function Document() {
-  return (
-    <html>
-      <head>
-        <title>vite-rsc</title>
-      </head>
-      <body className="flex flex-col gap-2 items-start p-2">
-        <h4 className="text-xl">Test</h4>
-        <div>
-          <Hydrated />
-          <input data-testid="client-state" placeholder="client-state" />
-        </div>
-        <ClientCounter />
-        <form action={changeServerCounter}>
-          <input type="hidden" name="change" value="1" />
-          <button>Server Counter: {serverCounter}</button>
-          <button formAction={resetServerCounter}>Server Reset</button>
-        </form>
-        <TestStyleClient />
-        <div className="test-style-server">test-style-server</div>
-        <TestTailwindClient />
-        <div className="test-tw-server text-red-500">test-tw-server</div>
-        <TestTemporaryReference
-          action={async (node: React.ReactNode) => {
-            "use server";
-            return (
-              <span>
-                [server <span>{node}</span>]
-              </span>
-            );
-          }}
-        />
-      </body>
-    </html>
-  );
-}
+import { Root } from "./routes/root";
 
 export default async function handler(request: Request): Promise<Response> {
-  const root = <Document />;
+  const root = <Root />;
   return renderRequest(request, root);
 }
