0ccf821fa174432d1a2c264a8b05ec349ee3681f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-02 16:26:40 +0900

    refactor(react-server): simplify `registerClientReference` calls (#804)

--- packages/react-server/src/features/client-component/server.tsx
+++ packages/react-server/src/features/client-component/server.tsx
@@ -1,13 +1,1 @@
-import * as ReactServer from "@hiogawa/vite-rsc/react/rsc";
-
-// https://github.com/facebook/react/blob/c8a035036d0f257c514b3628e927dd9dd26e5a09/packages/react-server-dom-webpack/src/ReactFlightWebpackReferences.js#L43
-
-// $$id: /src/components/counter.tsx#Counter
-//   ⇕
-// id: /src/components/counter.tsx
-// name: Counter
-
-/* @__NO_SIDE_EFFECTS__ */
-export function registerClientReference(id: string, name: string) {
-  return ReactServer.registerClientReference({}, id, name);
-}
+export { registerClientReference } from "@hiogawa/vite-rsc/react/rsc";
