b709e5df86afb486b94e0e23d2f83c80c5fbe6e8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-06 15:37:18 +0900

    test(rsc): test useActionState

--- packages/rsc/examples/basic/src/routes/action-from-client/client.tsx
+++ packages/rsc/examples/basic/src/routes/action-from-client/client.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import { testAction, testAction2 } from "./action";
+import React from "react";
+import { testAction, testAction2, testActionState } from "./action";
 
 export function TestActionFromClient() {
+  const [state, formAction] = React.useActionState(testActionState, 0);
+
   return (
     <form action={testAction}>
       <button>test-action-from-client</button>
       <button formAction={testAction2}>test-action-from-client-2</button>
+      <button formAction={formAction}>test-form-action: {state}</button>
     </form>
   );
 }
