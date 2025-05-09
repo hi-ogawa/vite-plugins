6ff6ef5145a861e50df832a425360fd9280be406
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-09 17:53:15 +0900

    chore: cleanup

--- packages/rsc/examples/basic/src/server.tsx
+++ packages/rsc/examples/basic/src/server.tsx
@@ -1,14 +1,8 @@
 import { renderRequest } from "@hiogawa/vite-rsc/extra/rsc";
 import { Root } from "./routes/root";
-import { Styles } from "./styles";
 
 export default async function handler(request: Request): Promise<Response> {
   const url = new URL(request.url);
-  const root = (
-    <>
-      <Styles />
-      <Root url={url} />
-    </>
-  );
+  const root = <Root url={url} />;
   return renderRequest(request, root);
 }
