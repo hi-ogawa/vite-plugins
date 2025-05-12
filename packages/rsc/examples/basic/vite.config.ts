9a46ab7fe9bf1d9e1bc44b093115d9a13bee7c00
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-12 16:11:54 +0900

    chore(rsc): deploy example (#840)
    I didn't know cf deployment became this easy with `wrangler.jsonc`.
    https://vite-rsc-basic.hiro18181.workers.dev

--- packages/rsc/examples/basic/vite.config.ts
+++ packages/rsc/examples/basic/vite.config.ts
@@ -47,6 +47,47 @@ export default defineConfig({
         }
       },
     },
+    {
+      name: "cf-build",
+      enforce: "post",
+      apply: () => !!process.env.CF_BUILD,
+      configEnvironment() {
+        return {
+          keepProcessEnv: false,
+          define: {
+            "process.env.NO_CSP": "false",
+          },
+          resolve: {
+            noExternal: true,
+          },
+        };
+      },
+      generateBundle() {
+        if (this.environment.name === "rsc") {
+          this.emitFile({
+            type: "asset",
+            fileName: "cloudflare.js",
+            source: `\
+import handler from './index.js';
+export default { fetch: handler };
+`,
+          });
+        }
+        if (this.environment.name === "client") {
+          // https://developers.cloudflare.com/workers/static-assets/headers/#custom-headers
+          this.emitFile({
+            type: "asset",
+            fileName: "_headers",
+            source: `\
+/favicon.ico
+  Cache-Control: public, max-age=3600, s-maxage=3600
+/assets/*
+  Cache-Control: public, max-age=31536000, immutable
+`,
+          });
+        }
+      },
+    },
   ],
   build: {
     minify: false,
