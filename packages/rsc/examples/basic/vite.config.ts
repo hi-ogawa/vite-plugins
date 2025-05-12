0e558b4ab337b6ca0f1aff72effe09f5845c3e16
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-12 15:14:38 +0900

    wip

--- packages/rsc/examples/basic/vite.config.ts
+++ packages/rsc/examples/basic/vite.config.ts
@@ -2,7 +2,7 @@ import assert from "node:assert";
 import rsc from "@hiogawa/vite-rsc/plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, createBuilder } from "vite";
 import Inspect from "vite-plugin-inspect";
 
 export default defineConfig({
@@ -47,6 +47,47 @@ export default defineConfig({
         }
       },
     },
+    {
+      name: "cf-build",
+      enforce: "post",
+      apply: () => !!process.env.CF_BUILD,
+      config(config) {
+        const buildApp = config.builder!.buildApp!;
+        return {
+          builder: {
+            async buildApp(builder) {
+              await buildApp(builder);
+
+              // bundle server again for cf deployment
+              // const cfBuilder = await createBuilder({
+              //   configFile: false,
+              //   envDir: false,
+              //   publicDir: false,
+              //   environments: {
+              //     ssr: {
+              //       resolve: {
+              //         noExternal: true,
+              //       },
+              //       build: {
+              //         outDir: 'dist/cf',
+              //         rollupOptions: {
+              //           input: {
+              //             index: 'dist/rsc/index.js'
+              //           },
+              //           // output: {
+              //           //   inlineDynamicImports: true,
+              //           // }
+              //         }
+              //       },
+              //     }
+              //   },
+              // });
+              // await cfBuilder.build(cfBuilder.environments.ssr);
+            },
+          }
+        }
+      },
+    }
   ],
   build: {
     minify: false,
