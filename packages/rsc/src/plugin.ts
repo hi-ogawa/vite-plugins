0fbeb976ef6240b2407c8bbe44db7e9f0bb25b2a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-14 11:31:38 +0900

    chore: vendor react-server-dom (2nd approach)

--- packages/rsc/src/plugin.ts
+++ packages/rsc/src/plugin.ts
@@ -107,6 +107,7 @@ export default function vitePluginRsc({
               },
               resolve: {
                 noExternal: [PKG_NAME],
+                external: ["react-server-dom-vite/client.edge"],
               },
               optimizeDeps: {
                 exclude: [PKG_NAME],
@@ -160,7 +161,21 @@ export default function vitePluginRsc({
         };
       },
       async configEnvironment(name, _config, env) {
-        if (name !== "rsc") return;
+        // commonjsOptions needs to be tweaked when this is a linked dep
+        // since otherwise vendored cjs doesn't work.
+        const isLinkedDep = !import.meta.url.includes("/node_modules/");
+        const commonjsOptions = isLinkedDep
+          ? {
+              include: [/\/node_modules\//, /\/react-server-dom-vite\//],
+            }
+          : {};
+        if (name !== "rsc") {
+          return {
+            build: {
+              commonjsOptions,
+            },
+          };
+        }
 
         // bundle deps with react-server condition
 
@@ -170,11 +185,7 @@ export default function vitePluginRsc({
           root: process.cwd(),
           isBuild: env.command === "build",
           isFrameworkPkgByJson(pkgJson) {
-            if (
-              [PKG_NAME, "react-dom", "react-server-dom-vite"].includes(
-                pkgJson.name,
-              )
-            ) {
+            if ([PKG_NAME, "react-dom"].includes(pkgJson.name)) {
               return;
             }
             const deps = pkgJson["peerDependencies"];
@@ -185,6 +196,9 @@ export default function vitePluginRsc({
         return {
           resolve: {
             noExternal: result.ssr.noExternal.sort(),
+          },
+          build: {
+            commonjsOptions,
           },
         };
       },
--- packages/rsc/tsdown.config.ts
+++ packages/rsc/tsdown.config.ts
@@ -1,4 +1,6 @@
+import fs from "node:fs";
 import { defineConfig } from "tsdown";
+import pkg from "./package.json" with { type: "json" };
 
 export default defineConfig({
   entry: [
@@ -19,9 +21,37 @@ export default defineConfig({
     "src/extra/rsc.tsx",
   ],
   format: ["esm"],
-  external: [/^virtual:/],
+  external: [/^virtual:/, new RegExp(`^${pkg.name}/`)],
   dts: {
     sourceMap: process.argv.slice(2).includes("--sourcemap"),
   },
   bundleDts: false,
+  plugins: [
+    {
+      name: "vendor",
+      buildStart() {
+        fs.rmSync("./dist/vendor/", { recursive: true, force: true });
+        fs.mkdirSync("./dist/vendor", { recursive: true });
+        fs.cpSync(
+          "./node_modules/react-server-dom-vite",
+          "./dist/vendor/react-server-dom-vite",
+          { recursive: true, dereference: true },
+        );
+        fs.rmSync("./dist/vendor/react-server-dom-vite/node_modules", {
+          recursive: true,
+          force: true,
+        });
+      },
+      transform(code) {
+        // TODO: don't be lazy and just update code?
+        if (code.includes(`"react-server-dom-vite/`)) {
+          code = code.replace(
+            /"react-server-dom-vite\/([^"]*)"/g,
+            (m) => `"@hiogawa/vite-rsc/vendor/${m.slice(1, -1)}"`,
+          );
+          return code;
+        }
+      },
+    },
+  ],
 }) as any;
