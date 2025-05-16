5f134b7d8514ee8a9e6623818772c17040b4cba0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-16 18:09:31 +0900

    chore(rsc): port react-router route module transform

--- packages/rsc/examples/react-router/react-router-vite/plugin.ts
+++ packages/rsc/examples/react-router/react-router-vite/plugin.ts
@@ -1,11 +1,15 @@
 import path from "node:path";
+import { type Plugin, runnerImport, transformWithEsbuild } from "vite";
+import { transformRouteModule } from "./transformer/transformer";
 import type { RouteConfigEntry } from "@react-router/dev/routes";
-import { type Plugin, runnerImport } from "vite";
 
 export function reactRouter(): Plugin[] {
   return [
     {
-      name: "react-router:routes",
+      name: "react-router",
+      //
+      // virtual routes.ts
+      //
       async load(id) {
         if (id.endsWith("?react-router-routes")) {
           const imported = await runnerImport<any>(id);
@@ -14,25 +18,91 @@ export function reactRouter(): Plugin[] {
             {
               id: "root",
               path: "",
-              file: path.resolve(appDirectory, "root.tsx"),
+              file: path.resolve(appDirectory, imported.module.root),
               children: imported.module.default,
             },
           ];
-          const code = generateRoutesCode({
+          const code = generateRoutes({
             appDirectory,
             routes,
           });
           return code;
         }
       },
+      //
+      // transform route module
+      //
+      async transform(code, id) {
+        // skip internal virtual module such as
+        // \0virtual:vite-rsc/css/dev-ssr//src/routes/about.tsx?react-router-client-route-module
+        if (id.startsWith("\0virtual")) return;
+
+        if (id.endsWith("?react-router-route-module")) {
+          const filePath = id.split("?")[0];
+          const result = await transformRouteModule({
+            code,
+            filePath,
+            clientHelper: "/plugin/transformer/client-route-component-props.ts",
+          });
+
+          routeModuleAssets[filePath] = {};
+          for (const asset of result.assets) {
+            if (
+              asset.uniqueKey === "client-route-module" ||
+              asset.uniqueKey === "server-route-module"
+            ) {
+              const result = await transformWithEsbuild(
+                asset.content,
+                filePath,
+                { loader: "jsx", jsx: "automatic" },
+              );
+              asset.content = replaceImport(result.code, filePath);
+            }
+            routeModuleAssets[filePath][asset.uniqueKey] = asset.content;
+          }
+
+          const resultCode = replaceImport(result.code, filePath);
+          return { code: resultCode };
+        }
+
+        if (id.endsWith("?react-router-client-route-module")) {
+          const filePath = id.split("?")[0];
+          return routeModuleAssets[filePath]["client-route-module"];
+        }
+
+        if (id.endsWith("?react-router-server-route-module")) {
+          const filePath = id.split("?")[0];
+          return routeModuleAssets[filePath]["server-route-module"];
+        }
+
+        if (id.endsWith("?react-router-client-route-module-source")) {
+          const filePath = id.split("?")[0];
+          return routeModuleAssets[filePath]["client-route-module-source"];
+        }
+      },
     },
   ];
 }
 
-// copied from
-// https://github.com/jacob-ebey/parcel-plugin-react-router/blob/9385be813534537dfb0fe640a3e5c5607be3b61d/packages/resolver/src/resolver.ts
+const routeModuleAssets: Record<string, Record<string, string>> = {};
 
-function generateRoutesCode(config: {
+function replaceImport(code: string, filePath: string) {
+  return code
+    .replaceAll(
+      `"client-route-module"`,
+      `"${filePath}?react-router-client-route-module"`,
+    )
+    .replaceAll(
+      `"server-route-module"`,
+      `"${filePath}?react-router-server-route-module"`,
+    )
+    .replaceAll(
+      `"client-route-module-source"`,
+      `"${filePath}?react-router-client-route-module-source"`,
+    );
+}
+
+function generateRoutes(config: {
   appDirectory: string;
   routes: RouteConfigEntry[];
 }) {
@@ -49,8 +119,7 @@ function generateRoutesCode(config: {
       continue;
     }
     code += "{";
-    // TODO: route-module transform
-    code += `lazy: () => import(${JSON.stringify(path.resolve(config.appDirectory, route.file))}),`;
+    code += `lazy: () => import(${JSON.stringify(path.resolve(config.appDirectory, route.file) + "?react-router-route-module")}),`;
     code += `id: ${JSON.stringify(route.id || createRouteId(route.file, config.appDirectory))},`;
     if (typeof route.path === "string") {
       code += `path: ${JSON.stringify(route.path)},`;
