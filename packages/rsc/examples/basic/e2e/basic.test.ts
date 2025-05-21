7660ac74d3bc38ef8dd470f378a5b636e9204b33
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-21 13:04:44 +0900

    test: tweak

--- packages/rsc/examples/basic/e2e/basic.test.ts
+++ packages/rsc/examples/basic/e2e/basic.test.ts
@@ -416,12 +416,27 @@ test("ssr rsc payload encoding", async ({ page }) => {
 
 test(`expose only "use server" functions @build`, async ({ request }) => {
   // curl -X POST -H "x-rsc-action: 783a248a7e18#Root" http://localhost:4173/
-  const res = await request.post("./", {
-    headers: {
-      "x-rsc-action": "783a248a7e18#Root",
-    },
-  });
-  expect(res.status()).toBe(404);
+  {
+    const res = await request.post("./", {
+      headers: {
+        "x-rsc-action": "783a248a7e18#Root",
+      },
+    });
+    expect(res.status()).toBe(404);
+    expect(await res.text()).toBe(
+      "Server function not found '783a248a7e18#Root'",
+    );
+  }
+
+  // curl -X POST -H "x-rsc-action: 12345678#foo" http://localhost:4173/
+  {
+    const res = await request.post("./", {
+      headers: {
+        "x-rsc-action": "12345678#foo",
+      },
+    });
+    expect(res.status()).toBe(500);
+  }
 
   // TODO: do the same for dev
   // curl -X POST -H "x-rsc-action: /src/routes/root.tsx#Root" http://localhost:5173/
