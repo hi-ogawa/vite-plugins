510125d7d1336e7617d63d278321e74ceb786ed7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-23 11:25:24 +0900

    fix(rsc): fix stale css import in non-boundary client module (#887)
    The issue is that `addWatchFile(<client-boundary-file>)` only allows invalidating css on client-boundary file change, but this virtual also needs to take into account any transitive css import inside the boundary.
    
    https://github.com/hi-ogawa/vite-plugins/blob/68d6823559e29738fa724c4e092cd0fa265e9640/packages/rsc/src/plugin.ts#L1083-L1084
    
    This fix is for ssr css virtual. The same fix for rsc css virtual will be needed in https://github.com/hi-ogawa/vite-plugins/pull/876.

--- packages/rsc/examples/basic/e2e/basic.test.ts
+++ packages/rsc/examples/basic/e2e/basic.test.ts
@@ -234,6 +234,53 @@ test("css hmr client @dev", async ({ page }) => {
   );
 });
 
+test("adding/removing css client @dev @js", async ({ page }) => {
+  await page.goto("./");
+  await waitForHydration(page);
+  await using _ = await expectNoReload(page);
+  await testAddRemoveCssClient(page, { js: true });
+});
+
+testNoJs("adding/removing css client @dev @nojs", async ({ page }) => {
+  await page.goto("./");
+  await testAddRemoveCssClient(page, { js: false });
+});
+
+async function testAddRemoveCssClient(page: Page, options: { js: boolean }) {
+  await expect(page.locator(".test-style-client-dep")).toHaveCSS(
+    "color",
+    "rgb(255, 165, 0)",
+  );
+
+  // remove css import
+  const editor = createEditor("src/routes/client-dep.tsx");
+  editor.edit((s) =>
+    s.replaceAll(
+      `import "./client-dep.css";`,
+      `/* import "./client-dep.css"; */`,
+    ),
+  );
+  if (!options.js) {
+    await page.waitForTimeout(100);
+    await page.reload();
+  }
+  await expect(page.locator(".test-style-client-dep")).toHaveCSS(
+    "color",
+    "rgb(0, 0, 0)",
+  );
+
+  // add back css import
+  editor.reset();
+  if (!options.js) {
+    await page.waitForTimeout(100);
+    await page.reload();
+  }
+  await expect(page.locator(".test-style-client-dep")).toHaveCSS(
+    "color",
+    "rgb(255, 165, 0)",
+  );
+}
+
 test("css hmr server @dev", async ({ page }) => {
   await page.goto("./");
   await waitForHydration(page);
