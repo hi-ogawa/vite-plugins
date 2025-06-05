6fd31315dcd1f1698129ca895f08af3fff60b4a4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-05 18:35:38 +0900

    chore: cleanup

--- packages/rsc/types/index.d.ts
+++ packages/rsc/types/index.d.ts
@@ -1,5 +1,3 @@
-import "./virtual.d.ts";
-
 declare global {
   interface ImportMeta {
     readonly viteRscCss: import("react").JSX.Element;
--- packages/rsc/types/virtual.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "virtual:vite-rsc/importer-resources" {
-  export const resources: { js: string[]; css: string[] };
-  export function Resources(props: {
-    nonce?: string;
-  }): import("react").JSX.Element;
-}
