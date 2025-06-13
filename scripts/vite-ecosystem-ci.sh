d1864b93e48af3872c2ae197bf42b756ba9cab46
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-13 15:37:03 +0900

    chore: add to ecossytem-ci

--- scripts/vite-ecosystem-ci.sh
+++ scripts/vite-ecosystem-ci.sh
@@ -2,6 +2,9 @@
 set -eu -o pipefail
 
 # a part of test-rsc-core from ci.yml
+pnpm -C packages/rsc/examples/create-vite-e2e test-e2e
+pnpm -C packages/rsc/examples/create-vite build
+pnpm -C packages/rsc/examples/create-vite-e2e test-e2e-preview
 pnpm -C packages/rsc/examples/basic test-e2e
 pnpm -C packages/rsc/examples/basic build
 pnpm -C packages/rsc/examples/basic test-e2e-preview
