e2d61c1bc78acc8acaa92c6ad82cd74cb25457a3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-13 11:52:26 +0900

    chore: add vite-plugin-inspect

--- packages/rsc/examples/create-vite/vite.config.ts
+++ packages/rsc/examples/create-vite/vite.config.ts
@@ -1,6 +1,7 @@
 import rsc from "@hiogawa/vite-rsc/plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import inspect from "vite-plugin-inspect";
 
 export default defineConfig({
   plugins: [
@@ -12,5 +13,6 @@ export default defineConfig({
         ssr: "./src/framework/entry.ssr.tsx",
       },
     }),
+    inspect({ build: true }),
   ],
 });
