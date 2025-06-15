cc945111a4be05e126053b27d2c630b4b9b613f5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-15 11:23:36 +0900

    test: test vite-rsc outside of monorepo

---
+++ packages/rsc/scripts/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu -o pipefail
+
+# run examples/basic e2e on fresh installation
+#   bash misc/test.sh
+
+test_dir="/tmp/test-vite-rsc"
+lib_dir="$PWD"
+
+rm -rf "$test_dir"
+cp -r examples/basic "$test_dir"
+
+cd "$test_dir"
+rm -rf dist node_modules
+
+node "$lib_dir/misc/overrides.mjs" "$test_dir/package.json"
+pnpm i
+
+if test "${CI:-}" = "true"; then
+  npx playwright install chromium
+fi
+
+pnpm test-e2e
+pnpm build
+pnpm test-e2e-preview
