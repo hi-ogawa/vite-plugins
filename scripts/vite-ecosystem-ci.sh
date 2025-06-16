322bae66391416d227252adde447ee74887f3422
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-17 08:31:36 +0900

    Revert "chore: tweak ecosystem-ci"
    This reverts commit 310f62ccae56216cac7c7437e8575ec53b7c8e1f.

--- scripts/vite-ecosystem-ci.sh
+++ scripts/vite-ecosystem-ci.sh
@@ -2,6 +2,9 @@
 set -eu -o pipefail
 
 # a part of test-rsc-core from ci.yml
+pnpm -C packages/rsc/examples/starter-e2e test-e2e
+pnpm -C packages/rsc/examples/starter build
+pnpm -C packages/rsc/examples/starter-e2e test-e2e-preview
 pnpm -C packages/rsc/examples/basic test-e2e
 pnpm -C packages/rsc/examples/basic build
 pnpm -C packages/rsc/examples/basic test-e2e-preview
