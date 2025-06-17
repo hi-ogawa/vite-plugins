e0f347e84bd4335ecba65fca1543ac6457e1344c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-17 12:50:00 +0900

    wip: dev

--- packages/rsc/examples/starter/cf/entry.rsc.tsx
+++ packages/rsc/examples/starter/cf/entry.rsc.tsx
@@ -1,16 +1,17 @@
-import handler from "../src/framework/entry.rsc";
+import handler from "../src/framework/entry.rsc.base";
 
 export default {
   fetch(request: Request, env: any) {
-    if (import.meta.env.DEV) {
-      (globalThis as any).__viteSsrRunner = {
-        import: async () => {
-          return {
-            renderHTML: (...args: any[]) => env.SSR.renderHTML(...args),
-          };
-        },
-      };
-    }
-    return handler(request);
+    // if (import.meta.env.DEV) {
+    //   (globalThis as any).__viteSsrRunner = {
+    //     import: async () => {
+    //       return {
+    //         renderHTML: (...args: any[]) => env.SSR.renderHTML(...args),
+    //       };
+    //     },
+    //   };
+    // }
+    // const loadSsrModule = import.meta.env.DEV
+    return handler(request, (arg) => env.SSR.renderHTML(arg));
   },
 };
--- packages/rsc/examples/starter/cf/entry.ssr.tsx
+++ packages/rsc/examples/starter/cf/entry.ssr.tsx
@@ -3,5 +3,7 @@ import { WorkerEntrypoint } from "cloudflare:workers";
 import { renderHTML } from "../src/framework/entry.ssr";
 
 export default class DevRenderHTML extends WorkerEntrypoint {
-  renderHTML = renderHTML;
+  renderHTML(arg: any) {
+    return renderHTML(arg);
+  }
 }
