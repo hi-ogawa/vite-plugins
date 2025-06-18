019c497c7e694fcb45641dca7c1911869669635b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-18 12:14:55 +0900

    wip: react-router

--- packages/rsc/examples/react-router/app/root.tsx
+++ packages/rsc/examples/react-router/app/root.tsx
@@ -12,7 +12,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>React Router Vite</title>
-        {import.meta.viteRsc.loadCss("/app/root")}
       </head>
       <body>
         <header className="container px-8 my-8 mx-auto">
