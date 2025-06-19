3eafed3673f7c845c58698fbe35ba3aef0381ade
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-19 13:33:51 +0900

    refactor: use workaroundCloudflarePlugin

--- packages/rsc/examples/react-router/cf/vite.config.ts
+++ packages/rsc/examples/react-router/cf/vite.config.ts
@@ -1,5 +1,5 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
-import rsc from "@hiogawa/vite-rsc/plugin";
+import rsc, { workaroundCloudflarePlugin } from "@hiogawa/vite-rsc/plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -21,6 +21,7 @@ export default defineConfig({
       },
       serverHandler: false,
     }),
+    workaroundCloudflarePlugin(),
     inspect(),
     cloudflare({
       configPath: "./cf/wrangler.ssr.jsonc",
@@ -36,32 +37,6 @@ export default defineConfig({
         },
       ],
     }),
-    {
-      name: "fix-up",
-      enforce: "post",
-      config(config) {
-        // TODO: cloudflare should allow optimizeDeps.exclude for esm packages?
-        // for now we patch out https://github.com/cloudflare/workers-sdk/blob/33830214ff76ec4738b3e998370eca7568240e12/packages/vite-plugin-cloudflare/src/index.ts#L197
-        const plugin = config
-          .plugins!.flat()
-          .find((p) => p && "name" in p && p.name === "vite-plugin-cloudflare");
-        const original = (plugin as any).configResolved;
-        (plugin as any).configResolved = function (this: any, ...args: any[]) {
-          try {
-            return original.apply(this, args);
-          } catch (e) {
-            console.log(
-              "[patched cloudflare plugin error]",
-              e instanceof Error ? e.message : e,
-            );
-          }
-        };
-
-        // workaround (fixed in Vite 7) https://github.com/vitejs/vite/pull/20077
-        (config.environments as any).ssr.resolve.noExternal = true;
-        (config.environments as any).rsc.resolve.noExternal = true;
-      },
-    },
   ],
   environments: {
     ssr: {
