770fe18ec51bc3e9773928c772f1c8d1f27c3269
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-19 18:06:52 +0900

    chore: remove `packages/vite-node-miniflare`

--- packages/vite-node-miniflare/examples/react-router/src/routes/nodejs-compat.api.ts
+++
@@ -1,7 +0,0 @@
-import util from "node:util";
-import type { RequestHandler } from "@hattip/compose";
-import { json } from "react-router-dom";
-
-export const get: RequestHandler = () => {
-  return json({ format: util.format("hello:%s", "world") });
-};
--- packages/vite-node-miniflare/examples/react-router/src/routes/redirect.page.server.tsx
+++
@@ -1,7 +0,0 @@
-import { type LoaderFunction, redirect } from "react-router-dom";
-
-// TODO: 302 response doesn't work for miniflare.dispatchFetch?
-
-export const loader: LoaderFunction = () => {
-  throw redirect("/");
-};
