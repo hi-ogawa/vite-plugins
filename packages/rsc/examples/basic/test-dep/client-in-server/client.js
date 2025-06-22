aff7276157644b0b5a486f1411b84cb08d381157
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-22 18:47:41 +0900

    chore: lint

--- packages/rsc/examples/basic/test-dep/client-in-server/client.js
+++ packages/rsc/examples/basic/test-dep/client-in-server/client.js
@@ -3,6 +3,6 @@
 import React from "react";
 
 export function TestClient() {
-  const [ok] = React.useState(() => true)
+  const [ok] = React.useState(() => true);
   return React.createElement("span", null, String(ok));
 }
