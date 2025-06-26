71d3f65e8113b93f817637c29e314ebca7048c54
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-27 08:27:52 +0900

    test: test closure

--- packages/rsc/examples/basic/src/routes/action-state/server.tsx
+++ packages/rsc/examples/basic/src/routes/action-state/server.tsx
@@ -1,13 +1,14 @@
 import { TestActionStateClient } from "./client";
 
 export function TestActionStateServer() {
+  const time = new Date().toISOString();
   return (
     <div>
       <TestActionStateClient
         fn={async () => {
           "use server";
           await new Promise((resolve) => setTimeout(resolve, 1000));
-          return <span>Action completed on server!</span>;
+          return <span>Action completed on server at {time}!</span>;
         }}
       />
     </div>
