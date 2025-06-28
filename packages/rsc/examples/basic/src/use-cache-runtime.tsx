bb59a35de11a043a4d2c47acdd24bea838eed688
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-28 13:22:43 +0900

    feat: cacheWrapper

--- packages/rsc/examples/basic/src/use-cache-runtime.tsx
+++ packages/rsc/examples/basic/src/use-cache-runtime.tsx
@@ -1,4 +1,4 @@
-import * as ReactServer from "@hiogawa/vite-rsc/rsc";
+import * as ReactRsc from "@hiogawa/vite-rsc/rsc";
 import React from "react";
 
 // based on
@@ -6,15 +6,76 @@ import React from "react";
 // https://github.com/vercel/next.js/blob/09a2167b0a970757606b7f91ff2d470f77f13f8c/packages/next/src/server/use-cache/use-cache-wrapper.ts
 
 export default function cacheWrapper(fn: (...args: any[]) => Promise<unknown>) {
-  let store: Record<string, Promise<ReadableStream>> = {};
+  const cacheEntries: Record<string, Promise<StreamCacher>> = {};
 
   async function cachedFn(...args: any[]) {
-    if (1) return fn(...args);
-    args;
-    store;
-    ReactServer.renderToReadableStream;
-    ReactServer.createFromReadableStream;
+    // serialize arguments to a cache key via `encodeReply` from `react-server-dom/client`
+    // TODO: why not use `renderToReadableStream` to serialize instead?
+    const clientTemporaryReferences =
+      ReactRsc.createClientTemporaryReferenceSet();
+    const encodedArguments = await ReactRsc.encodeReply(args, {
+      temporaryReferences: clientTemporaryReferences,
+    });
+    const serializedCacheKey =
+      typeof encodedArguments === "string"
+        ? "a:" + encodedArguments
+        : "b:" +
+          arrayBufferToString(
+            await new Response(encodedArguments).arrayBuffer(),
+          );
+
+    // cache `fn` result as stream
+    const entryPromise = (cacheEntries[serializedCacheKey] ??= (async () => {
+      // handle cache miss
+
+      // TODO: why not use `args` directly?
+      const temporaryReferences = ReactRsc.createTemporaryReferenceSet();
+      const decodedArgs = await ReactRsc.decodeReply(encodedArguments, {
+        temporaryReferences,
+      });
+
+      // run the original function
+      const result = await fn(...decodedArgs);
+
+      // serialize result to a ReadableStream
+      const stream = ReactRsc.renderToReadableStream(result, {
+        environmentName: "Cache",
+        temporaryReferences,
+      });
+      return new StreamCacher(stream);
+    })());
+
+    // deserialized cached stream
+    const stream = (await entryPromise).get();
+    const result = ReactRsc.createFromReadableStream(stream, {
+      environmentName: "Cache",
+      replayConsoleLogs: true,
+      temporaryReferences: clientTemporaryReferences,
+    });
+    return result;
   }
 
   return React.cache(cachedFn);
+}
+
+class StreamCacher {
+  constructor(private stream: ReadableStream<Uint8Array>) {}
+  get(): ReadableStream<Uint8Array> {
+    const [returnStream, savedStream] = this.stream.tee();
+    this.stream = savedStream;
+    return returnStream;
+  }
+}
+
+function arrayBufferToString(buffer: ArrayBuffer | Uint8Array): string {
+  const bytes = new Uint8Array(buffer);
+  const len = bytes.byteLength;
+  if (len < 65535) {
+    return String.fromCharCode.apply(null, bytes as unknown as number[]);
+  }
+  let binary = "";
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]!);
+  }
+  return binary;
 }
