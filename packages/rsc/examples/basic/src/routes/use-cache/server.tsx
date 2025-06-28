d89adb5be94d46189c2406bec0d963709120ba90
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-28 16:39:37 +0900

    test: test closure

--- packages/rsc/examples/basic/src/routes/use-cache/server.tsx
+++ packages/rsc/examples/basic/src/routes/use-cache/server.tsx
@@ -1,29 +1,29 @@
 export function TestUseCache() {
   return (
-    <div>
-      <form
-        data-testid="test-use-cache-fn"
-        action={async (formData) => {
-          "use server";
-          actionCount++;
-          await testFn(formData.get("argument"));
-        }}
-      >
-        <button>[test-use-cache-fn]</button>
-        <input className="w-25" name="argument" placeholder="argument" />
-        <span>
-          (actionCount: {actionCount}, cacheFnCount: {cacheFnCount})
-        </span>
-      </form>
-      <TestComponent>
-        {/*
-          NOTE: warpping with `span` is crucial because
-          raw string `children` would get included as cache key
-          and thus causes `TestComponent` to be evaluated in each render.
-        */}
-        <span>{new Date().toISOString()}</span>
-      </TestComponent>
-    </div>
+    <>
+      <TestUseCacheFn />
+      <TestUseCacheComponent />
+      <TestUseCacheClosure />
+    </>
+  );
+}
+
+function TestUseCacheFn() {
+  return (
+    <form
+      data-testid="test-use-cache-fn"
+      action={async (formData) => {
+        "use server";
+        actionCount++;
+        await testFn(formData.get("argument"));
+      }}
+    >
+      <button>[test-use-cache-fn]</button>
+      <input className="w-25" name="argument" placeholder="argument" />
+      <span>
+        (actionCount: {actionCount}, cacheFnCount: {cacheFnCount})
+      </span>
+    </form>
   );
 }
 
@@ -33,6 +33,17 @@ let cacheFnCount = 0;
 async function testFn(..._args: unknown[]) {
   "use cache";
   cacheFnCount++;
+}
+
+function TestUseCacheComponent() {
+  // NOTE: warpping with `span` (or any jsx) is crucial because
+  // raw string `children` would get included as cache key
+  // and thus causes `TestComponent` to be evaluated in each render.
+  return (
+    <TestComponent>
+      <span>{new Date().toISOString()}</span>
+    </TestComponent>
+  );
 }
 
 async function TestComponent(props: { children?: React.ReactNode }) {
@@ -49,3 +60,46 @@ async function TestComponent(props: { children?: React.ReactNode }) {
     </div>
   );
 }
+
+async function TestUseCacheClosure() {
+  function outerFn(outer: string) {
+    async function innerFn(inner: string) {
+      "use cache";
+      innerFnCount++;
+      return (
+        <span>
+          (outer: {outer}, inner: {inner})
+        </span>
+      );
+    }
+    return innerFn;
+  }
+
+  const result = await outerFn(outerFnArg)(innerFnArg);
+
+  return (
+    <div className="flex gap-1">
+      <form
+        action={async (formData) => {
+          "use server";
+          actionCount2++;
+          outerFnArg = formData.get("outer") as string;
+          innerFnArg = formData.get("inner") as string;
+        }}
+      >
+        <button>[test-use-cache-closure]</button>
+        <input className="w-15" name="outer" placeholder="outer" />
+        <input className="w-15" name="inner" placeholder="inner" />
+      </form>
+      <span>{result}</span>
+      <span>
+        (actionCount2: {actionCount2}, innerFnCount: {innerFnCount})
+      </span>
+    </div>
+  );
+}
+
+let outerFnArg = "";
+let innerFnArg = "";
+let innerFnCount = 0;
+let actionCount2 = 0;
