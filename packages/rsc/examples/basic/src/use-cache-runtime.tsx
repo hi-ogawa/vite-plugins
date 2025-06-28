c2a808e8ed4bead8556c1811219442cce57a28a9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-28 14:40:44 +0900

    chore: tweak

--- packages/rsc/examples/basic/src/use-cache-runtime.tsx
+++ packages/rsc/examples/basic/src/use-cache-runtime.tsx
@@ -16,8 +16,8 @@ export default function cacheWrapper(fn: (...args: any[]) => Promise<unknown>) {
   async function cachedFn(...args: any[]) {
     // Serialize arguments to a cache key via `encodeReply` from `react-server-dom/client`.
     // NOTE: using `renderToReadableStream` here for arguments serialization would end up
-    // serializing react elements arguments (e.g. children props), which causes
-    // those arguments to become a cache key and it doesn't achive
+    // serializing react elements (e.g. children props), which causes
+    // those arguments to be includes as a cache key and it doesn't achive
     // "use cache static shell + dynamic children props" pattern.
     // cf. https://nextjs.org/docs/app/api-reference/directives/use-cache#non-serializable-arguments
     const clientTemporaryReferences =
