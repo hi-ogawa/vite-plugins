7a3039961914b1c5299ae7946123ff66bb92fbef
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-01 15:18:52 +0900

    test: rework e2e

---
+++ packages/rsc/playwright.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "e2e",
+  use: {
+    trace: "on-first-retry",
+  },
+  expect: {
+    toPass: { timeout: 5000 },
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: null,
+        deviceScaleFactor: undefined,
+      },
+    },
+  ],
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: process.env.CI ? "github" : "list",
+}) as any;
