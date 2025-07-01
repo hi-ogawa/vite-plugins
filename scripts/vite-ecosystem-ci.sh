e3eb42b59caa18d9edac649224b774e5b2e2f91e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-01 18:38:49 +0900

    test: cleanup

--- scripts/vite-ecosystem-ci.sh
+++ scripts/vite-ecosystem-ci.sh
@@ -2,9 +2,7 @@
 set -eu -o pipefail
 
 # a part of test-rsc-core from ci.yml
-pnpm -C packages/rsc/examples/starter-e2e test-e2e
-pnpm -C packages/rsc/examples/starter build
-pnpm -C packages/rsc/examples/starter-e2e test-e2e-preview
+pnpm -C packages/rsc test-e2e
 pnpm -C packages/rsc/examples/basic test-e2e
 pnpm -C packages/rsc/examples/basic build
 pnpm -C packages/rsc/examples/basic test-e2e-preview
