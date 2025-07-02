eb2a451e847fdc658deecff1cf43e73c0b27ace0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-02 16:19:20 +0900

    test: test more browsers

--- packages/rsc/playwright.config.ts
+++ packages/rsc/playwright.config.ts
@@ -17,6 +17,14 @@ export default defineConfig({
         deviceScaleFactor: undefined,
       },
     },
+    {
+      name: "webkit",
+      use: devices["Desktop Safari"],
+    },
+    {
+      name: "firefox",
+      use: devices["Desktop Firefox"],
+    },
   ],
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
