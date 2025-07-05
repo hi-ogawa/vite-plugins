3bece254fc69d70cb911430aca342cdec7e9b874
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-05 15:55:23 +0900

    chore: tsdown

--- packages/import-attributes/tsdown.config.ts
+++ packages/import-attributes/tsdown.config.ts
@@ -1,17 +1,6 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: [
-    "src/plugin.ts",
-    "src/entry.browser.tsx",
-    "src/entry.rsc.tsx",
-    "src/entry.rsc.single.tsx",
-    "src/entry.ssr.tsx",
-    "src/client.tsx",
-  ],
+  entry: ["src/index.ts"],
   format: ["esm"],
-  external: [/^virtual:/],
-  dts: {
-    sourcemap: process.argv.slice(2).includes("--sourcemap"),
-  },
 }) as any;
