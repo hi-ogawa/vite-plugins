f8ccb85a532496cf1f07306206c479cb257a7c24
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-10 15:55:50 +0900

    ci: setup release (#1156)

---
+++ scripts/release.js
@@ -0,0 +1,25 @@
+// Example usage:
+//   node scripts/release.js nitro
+
+import { execSync } from "node:child_process";
+import fs from "node:fs";
+
+const $ = (command) => execSync(command, { stdio: "inherit" });
+
+async function main() {
+  const [name] = process.argv.slice(2);
+  if (!name) {
+    console.error("Package name is required");
+    process.exitCode = 1;
+    return;
+  }
+
+  const pkgDir = `packages/${name}`;
+  const pkg = JSON.parse(fs.readFileSync(`${pkgDir}/package.json`));
+  const pkgTag = `${name}@${pkg.version}`;
+
+  $(`git tag ${pkgTag}`);
+  $(`git push origin ${pkgTag}`);
+}
+
+main();
