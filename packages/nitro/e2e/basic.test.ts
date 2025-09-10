61b1fddc647e1881bf3095bbb2fc234dd3859636
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-10 12:31:39 +0900

    test: cache-control

--- packages/nitro/e2e/basic.test.ts
+++ packages/nitro/e2e/basic.test.ts
@@ -1,3 +1,5 @@
+import { globSync } from "node:fs";
+import path from "node:path";
 import { expect, test } from "@playwright/test";
 import { type Fixture, useFixture } from "./fixture";
 
@@ -9,6 +11,29 @@ test.describe("dev", () => {
 test.describe("build", () => {
   const f = useFixture({ root: "examples/basic", mode: "build" });
   defineTest(f);
+
+  test("cache-control", async ({ request }) => {
+    // not immutable asset
+    {
+      const res = await request.get(f.url("/favicon.ico"));
+      expect(res.headers()).toMatchObject({
+        "content-type": "image/vnd.microsoft.icon",
+      });
+      expect(res.headers()["cache-control"]).toBeFalsy();
+    }
+
+    // immutable asset
+    {
+      const files = globSync("assets/index-*.js", {
+        cwd: path.join(f.root, "dist/client"),
+      });
+      const res = await request.get(f.url("/" + files[0]));
+      expect(res.headers()).toMatchObject({
+        "content-type": "text/javascript; charset=utf-8",
+        "cache-control": "public, max-age=31536000, immutable",
+      });
+    }
+  });
 });
 
 function defineTest(f: Fixture) {
