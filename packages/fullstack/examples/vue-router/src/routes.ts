1cf977850d123f105afe3544d4717c046291d19e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-02 22:06:17 +0900

    feat: integrate assets

--- packages/fullstack/examples/vue-router/src/routes.ts
+++ packages/fullstack/examples/vue-router/src/routes.ts
@@ -1,93 +1,91 @@
+import {
+  type ImportAssetsResult,
+  mergeAssets,
+} from "@hiogawa/vite-plugin-fullstack/runtime";
+import { useHead } from "@unhead/vue";
 import type { RouteRecordRaw } from "vue-router";
 
-// TODO: how to handle assets like react-router?
+// custom framework may employ fs router convention and/or transform plugin
+// to reduce boilerplace
 export const routes: RouteRecordRaw[] = [
   {
     path: "/",
     name: "home",
     component: () => import("./routes/index.vue"),
+    beforeEnter: (_from, _to, next) => {
+      useAssets(
+        import.meta.vite.assets({
+          import: "./routes/index.vue",
+          environment: "client",
+        }),
+        import.meta.vite.assets({
+          import: "./routes/index.vue",
+          environment: "ssr",
+        }),
+      );
+      next();
+    },
   },
   {
     path: "/about",
     name: "client",
     component: () => import("./routes/about.vue"),
+    beforeEnter: (_from, _to, next) => {
+      useAssets(
+        import.meta.vite.assets({
+          import: "./routes/about.vue",
+          environment: "client",
+        }),
+        import.meta.vite.assets({
+          import: "./routes/about.vue",
+          environment: "ssr",
+        }),
+      );
+      next();
+    },
   },
   {
     path: "/:catchAll(.*)",
     name: "not-found",
     component: () => import("./routes/not-found.vue"),
+    beforeEnter: (_from, _to, next) => {
+      useAssets(
+        import.meta.vite.assets({
+          import: "./routes/not-found.vue",
+          environment: "client",
+        }),
+        import.meta.vite.assets({
+          import: "./routes/not-found.vue",
+          environment: "ssr",
+        }),
+      );
+      next();
+    },
   },
 ];
 
-// import {
-//   type ImportAssetsResult,
-//   mergeAssets,
-// } from "@hiogawa/vite-plugin-fullstack/runtime";
-// import type { RouteObject } from "react-router";
+const rootAssets = [
+  import.meta.vite.assets({
+    import: "./root.vue",
+    environment: "client",
+  }),
+  import.meta.vite.assets({
+    import: "./root.vue",
+    environment: "ssr",
+  }),
+  import.meta.vite.assets({
+    import: "./framework/entry.client.tsx",
+    environment: "client",
+  }),
+];
 
-// // custom framework may employ fs router convention and/or transform plugin
-// // to reduce boilerplace
-// export const routes: RouteObject[] = [
-//   {
-//     id: "root",
-//     path: "/",
-//     lazy: () => import("./root"),
-//     handle: {
-//       assets: mergeAssets(
-//         import.meta.vite.assets({
-//           import: "./root",
-//           environment: "client",
-//         }),
-//         import.meta.vite.assets({
-//           import: "./root",
-//           environment: "ssr",
-//         }),
-//         // include client entry for ssr modulepreload
-//         import.meta.vite.assets({
-//           import: "./framework/entry.client.tsx",
-//           environment: "client",
-//         }),
-//       ),
-//     } satisfies CustomHandle,
-//     children: [
-//       {
-//         id: "index",
-//         index: true,
-//         lazy: () => import("./routes/index"),
-//         handle: {
-//           assets: mergeAssets(
-//             import.meta.vite.assets({
-//               import: "./routes/index",
-//               environment: "client",
-//             }),
-//             import.meta.vite.assets({
-//               import: "./routes/index",
-//               environment: "ssr",
-//             }),
-//           ),
-//         } satisfies CustomHandle,
-//       },
-//       {
-//         id: "about",
-//         path: "about",
-//         lazy: () => import("./routes/about"),
-//         handle: {
-//           assets: mergeAssets(
-//             import.meta.vite.assets({
-//               import: "./routes/about",
-//               environment: "client",
-//             }),
-//             import.meta.vite.assets({
-//               import: "./routes/about",
-//               environment: "ssr",
-//             }),
-//           ),
-//         } satisfies CustomHandle,
-//       },
-//     ],
-//   },
-// ];
-
-// export type CustomHandle = {
-//   assets: ImportAssetsResult;
-// };
+function useAssets(...args: ImportAssetsResult[]) {
+  if (!import.meta.env.SSR) return;
+  const assets = mergeAssets(...args, ...rootAssets);
+  useHead({
+    link: [
+      ...assets.css.map((attrs) => ({ rel: "stylesheet", ...attrs })),
+      ...assets.js.map((attrs) => ({ rel: "modulepreload", ...attrs })),
+    ],
+  });
+}
