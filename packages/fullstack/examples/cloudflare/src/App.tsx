bd37b2ec1bce2a0bf4a563af41272ef7e1dc9945
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-03 13:42:12 +0900

    test(fullstack): cloudflare plugin

---
+++ packages/fullstack/examples/cloudflare/src/App.tsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+export function App() {
+  return (
+    <main>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <Card />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </main>
+  );
+}
+
+function Card() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </div>
+  );
+}
