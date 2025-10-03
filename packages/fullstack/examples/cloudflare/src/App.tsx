5562231fddbf028f13fb407ffb0d991e7743d9a6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-03 18:50:06 +0900

    test: update e2e

--- packages/fullstack/examples/cloudflare/src/App.tsx
+++ packages/fullstack/examples/cloudflare/src/App.tsx
@@ -28,7 +28,7 @@ function Card() {
   return (
     <div className="card">
       <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
+        count (edit) is {count}
       </button>
       <p>
         Edit <code>src/App.tsx</code> and save to test HMR
