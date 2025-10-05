ce771607ca8b5ea22fa426849751f9b3de9df441
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-05 17:10:41 +0900

    chore: tweak

--- packages/fullstack/examples/glob/src/routes/blog/page.tsx
+++ packages/fullstack/examples/glob/src/routes/blog/page.tsx
@@ -2,20 +2,17 @@ import { Link } from "react-router";
 
 export function Component() {
   const posts = [
-    { slug: "hello-world", title: "Hello World", date: "January 15, 2024" },
     {
       slug: "vite-tips",
       title: "Vite Tips & Tricks",
       date: "January 20, 2024",
     },
+    { slug: "hello-world", title: "Hello World", date: "January 15, 2024" },
   ];
 
   return (
     <main>
       <h1>Blog</h1>
-      <div className="card">
-        <p>Welcome to our blog!</p>
-      </div>
       {posts.map((post) => (
         <div key={post.slug} className="card">
           <h3>
