b49c4982db7e6f629a141a09e4e7793e6f655549
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-05 16:05:58 +0900

    chore(fullstack): tweak vue-router example (#1223)

--- packages/fullstack/examples/vue-router/src/pages/about.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <main>
-    <h1>About</h1>
-    <div class="card">
-      <p>This is a simple Vue Router demo app built with Vite Plugin Fullstack.</p>
-      <p>It demonstrates basic routing and server-side rendering.</p>
-    </div>
-  </main>
-</template>
