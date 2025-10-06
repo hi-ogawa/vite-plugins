844eee2c818d5332031a9edf83c81b4041917b4d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-06 22:04:28 +0900

    cleanup

--- packages/fullstack/examples/island/src/framework/island/runtime-client.ts
+++ packages/fullstack/examples/island/src/framework/island/runtime-client.ts
@@ -3,6 +3,7 @@
 
 import { h, render } from "preact";
 
+// same technique as https://github.com/vitejs/vite-plugin-react/blob/5e245aa2e2a3671c24ac57de80b5d5a690c3a704/packages/plugin-rsc/src/plugin.ts#L258-L260
 declare let __island_raw_import__: (file: string) => Promise<any>;
 
 export class DemoIsland extends HTMLElement {
