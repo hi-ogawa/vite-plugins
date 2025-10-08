5b5dd004c432e665ad6bef0d2f138d76ad1ae5eb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-08 14:56:19 +0900

    chore: fix release script (#1257)

--- scripts/release.js
+++ scripts/release.js
@@ -3,24 +3,25 @@
 
 import { execSync } from "node:child_process";
 import fs from "node:fs";
+import path from "node:path";
 
 const $ = (command) => execSync(command, { stdio: "inherit" });
 
 async function main() {
-  const [name] = process.argv.slice(2);
-  if (!name) {
+  const [pkgDir] = process.argv.slice(2);
+  if (!pkgDir) {
     console.error("Package directory input is required");
     process.exitCode = 1;
     return;
   }
 
-  const pkgDir = name;
   if (!fs.existsSync(pkgDir)) {
     console.error(`Package directory "${pkgDir}" doesn't exist`);
     process.exitCode = 1;
     return;
   }
   const pkg = JSON.parse(fs.readFileSync(`${pkgDir}/package.json`));
+  const name = path.basename(pkgDir);
   const pkgTag = `${name}@${pkg.version}`;
 
   $(`git tag ${pkgTag}`);
