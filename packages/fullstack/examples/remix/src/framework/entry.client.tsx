c6e1e27efb55091a19eaed3a4bbabbd17e360872
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-14 18:04:18 +0900

    comment

--- packages/fullstack/examples/remix/src/framework/entry.client.tsx
+++ packages/fullstack/examples/remix/src/framework/entry.client.tsx
@@ -3,6 +3,7 @@ import { createFrame } from "@remix-run/dom";
 // same technique as https://github.com/vitejs/vite-plugin-react/blob/5e245aa2e2a3671c24ac57de80b5d5a690c3a704/packages/plugin-rsc/src/plugin.ts#L258-L260
 declare let __island_raw_import__: (file: string) => Promise<any>;
 
+// based on https://github.com/remix-run/remix/blob/90d4fb75dfc14e70d12a903019aaab189b922ff7/demos/bookstore/app/assets/entry.tsx
 createFrame(document, {
   async loadModule(moduleUrl, name) {
     let mod = await __island_raw_import__(moduleUrl);
