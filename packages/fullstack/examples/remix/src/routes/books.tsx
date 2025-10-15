c3e6b8f80b6345ad0aca7a1e9ed294a824994410
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-15 11:55:55 +0900

    chore: tweak styles

--- packages/fullstack/examples/remix/src/routes/books.tsx
+++ packages/fullstack/examples/remix/src/routes/books.tsx
@@ -2,9 +2,9 @@ import { BookCard } from "../frames/book-card";
 
 export default function BooksPage() {
   return (
-    <div>
+    <main>
       <BookCard name="The Great Gatsby" />
       <BookCard name="To Kill a Mockingbird" />
-    </div>
+    </main>
   );
 }
