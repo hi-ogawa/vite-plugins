5fc73f89f1aed2bd86159f549374698077ae00d5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-16 22:50:38 +0900

    chore: lint

--- packages/fullstack/src/runtime.ts
+++ packages/fullstack/src/runtime.ts
@@ -16,7 +16,7 @@ export function mergeAssets(
     args.flatMap((h) => h.css),
     (a) => a.href,
   );
-  const entry = args.filter(arg => arg.entry)?.[0]?.entry
+  const entry = args.filter((arg) => arg.entry)?.[0]?.entry;
   const raw: ImportAssetsResultRaw = { entry, js, css };
   return { ...raw, merge: (...args) => mergeAssets(raw, ...args) };
 }
