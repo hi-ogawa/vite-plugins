4a7d0e335336f460583fcaa95abd91362fe47447
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-16 12:54:28 +0900

    chore: comment

--- packages/fullstack/examples/remix/src/framework/plugin.ts
+++ packages/fullstack/examples/remix/src/framework/plugin.ts
@@ -119,12 +119,16 @@ import * as __dom_jsx from "@remix-run/dom/jsx-runtime";
         },
       },
     },
-    //
+    // similar idea as RSC HMR
+    // https://github.com/vitejs/vite-plugin-react/blob/910cd41d7e56c54466cfb3de9ea42f5671e347fc/packages/plugin-rsc/src/plugin.ts#L568-L569
     {
       name: "framework:server:update",
       hotUpdate(ctx) {
         const ids = ctx.modules.map((mod) => mod.id!).filter(Boolean);
         if (ids.length === 0) return;
+
+        // TODO: if remix hydrated component supports proper HMR, server refetch should be skipped in this case
+        // https://github.com/vitejs/vite-plugin-react/blob/910cd41d7e56c54466cfb3de9ea42f5671e347fc/packages/plugin-rsc/src/plugin.ts#L614C14-L614C36
 
         // send server:update event to client to refetch ssr html
         if (this.environment.name === "ssr") {
