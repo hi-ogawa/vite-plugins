dfb347cc12cc5ea7b5b2c1829e4b533a631a39e7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-11-06 10:34:22 +0900

    chore(deps): update react plugins (#1303)

--- packages/fullstack/examples/basic/vite.config.ts
+++ packages/fullstack/examples/basic/vite.config.ts
@@ -1,13 +1,12 @@
 import fullstack from "@hiogawa/vite-plugin-fullstack";
 import react from "@vitejs/plugin-react";
-import { type Plugin, defineConfig } from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig((_env) => ({
   clearScreen: false,
   plugins: [
     // import("vite-plugin-inspect").then((m) => m.default()),
     react(),
-    reactHmrPreamblePlugin(),
     fullstack(),
   ],
   environments: {
@@ -40,18 +39,3 @@ export default defineConfig((_env) => ({
     },
   },
 }));
-
-// waiting for https://github.com/vitejs/vite-plugin-react/pull/890
-function reactHmrPreamblePlugin(): Plugin[] {
-  return [
-    {
-      name: "react-hmr-preamble",
-      resolveId: (id) =>
-        id === "virtual:react-hmr-preamble" ? "\0" + id : null,
-      load: (id) =>
-        id === "\0virtual:react-hmr-preamble"
-          ? react.preambleCode.replace("__BASE__", "/")
-          : null,
-    },
-  ];
-}
