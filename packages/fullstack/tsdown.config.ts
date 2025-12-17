5c0874a4381eb3d03c82acff0ab346ef60b50d39
Pooya Parsa <pooya@pi0.io>
2025-12-17 11:01:32 +0100

    build(fullstack): inline runtime

--- packages/fullstack/tsdown.config.ts
+++ packages/fullstack/tsdown.config.ts
@@ -1,4 +1,5 @@
 import fs from "node:fs";
+import { readFile, writeFile } from "node:fs/promises";
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
@@ -8,11 +9,13 @@ export default defineConfig({
     sourcemap: process.argv.slice(2).includes("--sourcemap"),
   },
   hooks: {
-    "build:done"() {
+    async "build:done"() {
       fs.appendFileSync(
         "./dist/index.d.ts",
         `\nimport type {} from "@hiogawa/vite-plugin-fullstack/types";\n`,
       );
+      let pluginBundle = await readFile("dist/index.js", "utf-8")
+      await writeFile("dist/index.js", pluginBundle.replace(`fs.readFileSync(path.join(import.meta.dirname, "runtime.js"), "utf-8")`, `\`${await readFile("dist/runtime.js", "utf-8")}\``))
     },
   },
 }) as any;
